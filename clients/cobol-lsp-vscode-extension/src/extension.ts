dbd593a468a29a05f5a3ea87d808be3defd22b6b
Ramy Abdalla <ramy.abdalla@broadcom.com>
2019-09-20 11:06:38 +0200

    adding java check before starting server #11
    Signed-off-by: Ramy Abdalla <ramy.abdalla@broadcom.com>

--- clients/cobol-lsp-vscode-extension/src/extension.ts
+++ clients/cobol-lsp-vscode-extension/src/extension.ts
@@ -12,15 +12,17 @@
  *   Broadcom, Inc. - initial API and implementation
  */
 
+import * as cp from "child_process";
 import * as net from "net";
 import {ExtensionContext, extensions, StatusBarAlignment, window} from "vscode";
 import {
     Executable,
     LanguageClient,
     LanguageClientOptions,
+    StreamInfo,
 } from "vscode-languageclient/lib/main";
 
-export function activate(context: ExtensionContext) {
+export async function activate(context: ExtensionContext) {
     const fs = require("fs");
 
     // path resolved to identify the location of the LSP server into the extension
@@ -30,6 +32,7 @@ export function activate(context: ExtensionContext) {
     let serverOptions: Executable;
 
     try {
+        await isJavaInstalled();
         if (fs.existsSync(LSPServerPath)) {
             serverOptions = {
                 args: ["-Dline.separator=\r\n", "-jar", LSPServerPath, "pipeEnabled"],
@@ -59,4 +62,27 @@ export function activate(context: ExtensionContext) {
 
     context.subscriptions.push(disposable);
     item.text = "Language Server is active";
+}
+
+async function isJavaInstalled() {
+    return new Promise<any>((resolve, reject) => {
+        const ls = cp.spawn("java", ["-version"]);
+        ls.stderr.on("data", (data: any) => {
+            if (!data.toString().includes('java version "1.8')) {
+                return reject("Java version 8 expected");
+            }
+            return resolve();
+        });
+        ls.on("error", (code: any) => {
+            if ("Error: spawn java ENOENT" === code) {
+                reject("Java 8 is not found");
+            }
+            return reject(code);
+        });
+        ls.on("close", (code: number) => {
+            if (code !== 0) {
+                return reject("An error occured when checking if Java was installed");
+            }
+        });
+    });
 }
