6c802563273ec4de837cd61230291c1cc7f33573
zacanbrcom <andrea.zaccaro@broadcom.com>
2019-10-18 11:12:52 +0200

    fix issue for missing list of copybook files used by the cobol pre processor

--- com.ca.lsp.cobol/lsp-service-cobol/src/main/java/com/ca/lsp/cobol/service/MyLanguageServerImpl.java
+++ com.ca.lsp.cobol/lsp-service-cobol/src/main/java/com/ca/lsp/cobol/service/MyLanguageServerImpl.java
@@ -53,9 +53,6 @@ public class MyLanguageServerImpl implements IMyLanguageServer {
     WorkspaceServerCapabilities workspaceServiceCapabilities =
         new WorkspaceServerCapabilities(workspaceFoldersOptions);
     capabilities.setWorkspace(workspaceServiceCapabilities);
-    //
-    //    // set up workspace folders
-    //    workspaceService.setWorkspaceFolders(params.getWorkspaceFolders());
 
     // workspaceService.setWorkspaceFolders(params.getWorkspaceFolders());
     workspaceService.scanWorkspaceForCopybooks(params.getWorkspaceFolders());
