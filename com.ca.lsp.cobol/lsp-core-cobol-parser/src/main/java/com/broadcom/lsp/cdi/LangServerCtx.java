f0e5da598de14f5edbefe1bd0182d8c6a8b634a2
Andrei Temnikov <andrei.temnikov@broadcom.com>
2019-10-25 12:28:40 +0200

    Added shutdown to the Server context

--- com.ca.lsp.cobol/lsp-core-cobol-parser/src/main/java/com/broadcom/lsp/cdi/LangServerCtx.java
+++ com.ca.lsp.cobol/lsp-core-cobol-parser/src/main/java/com/broadcom/lsp/cdi/LangServerCtx.java
@@ -14,6 +14,7 @@
  *
  */
 package com.broadcom.lsp.cdi;
+
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Module;
@@ -22,28 +23,30 @@ import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * Created on 2019-10-03
- * Quick workaround to have the backward compatibility
- * meanwhile the old stack doesn't support DI.
+ * Created on 2019-10-03 Quick workaround to have the backward compatibility meanwhile the old stack
+ * doesn't support DI.
  */
 @Slf4j
 public class LangServerCtx {
-    // static variable single_instance of type Singleton
-    private static LangServerCtx single_instance = null;
-    @Getter
-    private static Injector injector;
+  // static variable single_instance of type Singleton
+  private static LangServerCtx single_instance = null;
+  @Getter private static Injector injector;
 
-    private LangServerCtx(Module[] modules) {
-        injector = Guice.createInjector(modules);
+  private LangServerCtx(Module[] modules) {
+    injector = Guice.createInjector(modules);
+  }
+
+  public static LangServerCtx getGuiceCtx(@NonNull Module... modules) {
+    if (single_instance == null) {
+      single_instance = new LangServerCtx(modules);
+    } else {
+      injector.createChildInjector(modules);
     }
+    return single_instance;
+  }
 
-    public static LangServerCtx getGuiceCtx(@NonNull Module... modules) {
-        if (single_instance == null) {
-            single_instance = new LangServerCtx(modules);
-        } else {
-            injector.createChildInjector(modules);
-        }
-        return single_instance;
-    }
-
+  public static void shutdown() {
+    single_instance = null;
+    injector = null;
+  }
 }
