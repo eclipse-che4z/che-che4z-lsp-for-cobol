130da24d45c40dfbd231079720b453253339ab2e
La Mantia, Giuseppe <giuseppe.lamantia@broadcom.com>
2019-11-01 10:51:42 +0100

    Added Unit Tests

--- com.ca.lsp.cobol/lsp-core-cobol-parser/src/test/java/com/broadcom/impl/DataBusGetFromCacheBadTest.java
+++ com.ca.lsp.cobol/lsp-core-cobol-parser/src/test/java/com/broadcom/impl/DataBusGetFromCacheBadTest.java
@@ -29,13 +29,14 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 /**
  * Created on 2019-10-02
  */
 @Slf4j
-public class DataBusGetFormCacheHappyTest extends AbsDataBusImplTest {
+public class DataBusGetFromCacheBadTest extends AbsDataBusImplTest {
 
     private DefaultDataBusBroker databus;
 
@@ -85,18 +86,16 @@ public class DataBusGetFormCacheHappyTest extends AbsDataBusImplTest {
         LOG.debug(String.format("Received : %s", adaptedDataEvent.getEventType().getId()));
     }
 
-    @Test
+    @Test (expected = NoSuchElementException.class)
     @SneakyThrows
-    public void getData() {
+    public void getData() throws NoSuchElementException {
         Assert.assertTrue(databus.isStored(ICpyRepository.calculateUUID("COPY40")));
         LOG.debug(String.format("Cache content : %s", databus.printCache()));
         Optional<CpyStorable> leastRecentlyUsed = databus.lastRecentlyUsed();
         LOG.debug(String.format("Least Recently Used item : %s  ID : %d", leastRecentlyUsed.get().getName(), leastRecentlyUsed.get().getId()));
         //Cache is Full
         LOG.debug(String.format("Cache STATUS --> MaxCacheSize: %d  ActualCacheSize: %d", databus.getCacheMaxSize(), databus.cacheSize()));
-        LOG.debug(String.format("Retrieving item %s ", "COPY40"));
-        Assert.assertTrue(databus.getData(ICpyRepository.calculateUUID("COPY40")).getName().equalsIgnoreCase("COPY40"));
-        LOG.debug(String.format("Element Retrieved : %s",databus.getData(ICpyRepository.calculateUUID("COPY40"))));
-        LOG.debug(String.format("Cache content : %s", databus.printCache()));
+        LOG.debug(String.format("Retrieving not existent item %s will throw NoSuchElementException ", "COPY20"));
+        databus.getData(ICpyRepository.calculateUUID("COPY20")).getName().equalsIgnoreCase("COPY20");
     }
 }
