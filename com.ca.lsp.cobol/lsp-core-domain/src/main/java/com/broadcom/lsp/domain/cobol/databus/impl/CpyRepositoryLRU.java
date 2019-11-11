4634e21b2568f7009e52da44a76db21bcd9bea8f
zacanbrcom <andrea.zaccaro@broadcom.com>
2019-11-11 09:17:54 +0100

    store (paragraphs, occurrences) in serialized structure in cache

--- com.ca.lsp.cobol/lsp-core-domain/src/main/java/com/broadcom/lsp/domain/cobol/databus/impl/CpyRepositoryLRU.java
+++ com.ca.lsp.cobol/lsp-core-domain/src/main/java/com/broadcom/lsp/domain/cobol/databus/impl/CpyRepositoryLRU.java
@@ -28,129 +28,127 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.SerializationUtils;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-/**
- * Created on 25/10/2019
- */
-
+/** Created on 25/10/2019 */
 @Slf4j
 @Singleton
 public class CpyRepositoryLRU implements ICpyRepository {
-    @Getter
-    private int cacheMaxSize;
-    @Setter
-    @NonNull
-    private List<CpyStorable> cpyRepo;
-    private Comparator<CpyStorable> storableComparator = (e1, e2) -> e1.getHit() - e2.getHit();//TLRU
+  @Getter private final int cacheMaxSize;
+  @Setter @NonNull private List<CpyStorable> cpyRepo;
+  private final Comparator<CpyStorable> storableComparator =
+      (e1, e2) -> e1.getHit() - e2.getHit(); // TLRU
 
-    @NonNull
-    private AtomicBoolean isSort = new AtomicBoolean(false);
+  @NonNull private final AtomicBoolean isSort = new AtomicBoolean(false);
 
-    @Inject
-    public CpyRepositoryLRU(@Named("CACHE-MAX-SIZE") int cachesize) {
-        this.cacheMaxSize = cachesize;
-        cpyRepo = new ArrayList<>(cacheMaxSize);
+  @Inject
+  public CpyRepositoryLRU(@Named("CACHE-MAX-SIZE") int cachesize) {
+    cacheMaxSize = cachesize;
+    cpyRepo = new ArrayList<>(cacheMaxSize);
+  }
+
+  @Override
+  @SneakyThrows
+  public void sortCache() {
+    if (!isSort.get()) cpyRepo.sort(storableComparator.reversed());
+    isSort.set(true);
+  }
+
+  @SneakyThrows
+  public Optional<CpyStorable> getCpyStorableCache(@NonNull long uuid) {
+    Optional<CpyStorable> cpy = cpyRepo.stream().filter(copy -> uuid == copy.getId()).findAny();
+    if (cpy.isPresent()) return Optional.of(SerializationUtils.clone(cpy.get()));
+    return cpy;
+  }
+
+  @SneakyThrows
+  private Optional<CpyStorable> getCpyStorableInstance(@NonNull long uuid) {
+    Optional<CpyStorable> cpy = cpyRepo.stream().filter(copy -> uuid == copy.getId()).findAny();
+    return cpy;
+  }
+
+  @Override
+  @SneakyThrows
+  public void setSort(boolean isSort) {
+    this.isSort.set(isSort);
+  }
+
+  @Override
+  @SneakyThrows
+  public void persist(@NonNull CpyStorable deepcopy) {
+    if (!isStored(deepcopy.getId())) {
+      if (cpyRepo.size() < getCacheMaxSize()) {
+        cpyRepo.add(deepcopy);
+        return;
+      }
+      cpyRepo.remove(cpyRepo.size() - 1);
+      cpyRepo.add(deepcopy);
     }
+  }
 
-    @Override
-    @SneakyThrows
-    public void sortCache() {
-        if (!isSort.get())
-                cpyRepo.sort(storableComparator.reversed());
-        isSort.set(true);
+  @Override
+  @SneakyThrows
+  public String logContent() {
+    StringBuilder chars = new StringBuilder();
+    cpyRepo.stream()
+        .forEach(
+            l ->
+                chars
+                    .append(System.getProperty("line.separator"))
+                    .append(l)
+                    .append(System.getProperty("line.separator")));
+    return chars.toString();
+  }
+
+  @Override
+  @SneakyThrows
+  public int size() {
+    return cpyRepo.size();
+  }
+
+  @SneakyThrows
+  public Optional<CpyStorable> topItem() {
+    return (cpyRepo.isEmpty()) ? Optional.empty() : Optional.of(cpyRepo.get(0));
+  }
+
+  @SneakyThrows
+  public Optional<CpyStorable> lastItem() {
+    return (cpyRepo.isEmpty()) ? Optional.empty() : Optional.of(cpyRepo.get(size() - 1));
+  }
+
+  @Override
+  @SneakyThrows
+  public boolean isStored(@NonNull StringBuilder id) {
+    long uuid = ICpyRepository.calculateUUID(id);
+    return isStored(uuid);
+  }
+
+  @Override
+  @SneakyThrows
+  public boolean isStored(@NonNull String id) {
+    long uuid = ICpyRepository.calculateUUID(id);
+    return isStored(uuid);
+  }
+
+  @Override
+  @SneakyThrows
+  public boolean isStored(@NonNull long uuid) {
+    Optional<CpyStorable> cpy = getCpyStorableInstance(uuid);
+    if (cpy.isPresent()) {
+      cpy.get().match();
+      setSort(false);
     }
+    sortCache();
+    return cpy.isPresent();
+  }
 
-
-    @SneakyThrows
-    public Optional<CpyStorable> getCpyStorableCache(@NonNull long uuid) {
-        Optional<CpyStorable> cpy = cpyRepo.stream()
-                .filter(copy -> uuid == copy.getId())
-                .findAny();
-        if (cpy.isPresent())
-            return Optional.of(SerializationUtils.clone(cpy.get())) ;
-        return cpy ;
-    }
-
-    @SneakyThrows
-    private Optional<CpyStorable> getCpyStorableInstance(@NonNull long uuid) {
-        Optional<CpyStorable> cpy = cpyRepo.stream()
-                .filter(copy -> uuid == copy.getId())
-                .findAny();
-        return cpy ;
-    }
-
-    @Override
-    @SneakyThrows
-    public void setSort(boolean isSort) {
-        this.isSort.set(isSort);
-    }
-
-
-    @Override
-    @SneakyThrows
-    public void persist(@NonNull CpyStorable deepcopy) {
-        if (!isStored(deepcopy.getId())){
-        if (cpyRepo.size() < getCacheMaxSize()) {
-            cpyRepo.add(deepcopy);
-            return;
-        }
-        cpyRepo
-                .remove(cpyRepo.size() - 1);
-        cpyRepo
-                .add(deepcopy);
-        }
-    }
-
-    @Override
-    @SneakyThrows
-    public String logContent() {
-        StringBuilder chars = new StringBuilder();
-        cpyRepo.stream().forEach(l -> chars.append(System.getProperty("line.separator")).append(l).append(System.getProperty("line.separator")));
-        return chars.toString();
-    }
-
-    @Override
-    @SneakyThrows
-    public int size() {
-        return cpyRepo.size();
-    }
-
-    @SneakyThrows
-    public Optional<CpyStorable> topItem() {
-        return (cpyRepo.isEmpty()) ? Optional.empty() : Optional.of(cpyRepo.get(0));
-    }
-
-    @SneakyThrows
-    public Optional<CpyStorable> lastItem() {
-        return (cpyRepo.isEmpty()) ? Optional.empty() : Optional.of(cpyRepo.get(size() - 1));
-    }
-
-
-    @Override
-    @SneakyThrows
-    public boolean isStored(@NonNull StringBuilder id) {
-        long uuid = ICpyRepository.calculateUUID(id);
-        return isStored(uuid);
-    }
-
-    @Override
-    @SneakyThrows
-    public boolean isStored(@NonNull String id) {
-        long uuid = ICpyRepository.calculateUUID(id);
-        return isStored(uuid);
-    }
-
-    @Override
-    @SneakyThrows
-    public boolean isStored(@NonNull long uuid) {
-        Optional<CpyStorable> cpy = getCpyStorableInstance(uuid);
-        if (cpy.isPresent()) {
-            cpy.get().match();
-            setSort(false);
-        }
-        sortCache();
-        return cpy.isPresent();
-    }
+  /** method that remove all the elments from the cache */
+  @Override
+  public void invalidateCache() {
+    cpyRepo.clear();
+  }
 }
