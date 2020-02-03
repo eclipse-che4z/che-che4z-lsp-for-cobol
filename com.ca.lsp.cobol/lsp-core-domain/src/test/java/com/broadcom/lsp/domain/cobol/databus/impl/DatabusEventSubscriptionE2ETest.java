216d765929081f17bfd39326492eb0a2458caf68
sergiuilie <sergiu.ilie@broadcom.com>
2020-02-03 17:33:16 +0100

    Align positive test parameters

--- com.ca.lsp.cobol/lsp-core-domain/src/test/java/com/broadcom/lsp/domain/cobol/databus/impl/DatabusEventSubscriptionE2ETest.java
+++ com.ca.lsp.cobol/lsp-core-domain/src/test/java/com/broadcom/lsp/domain/cobol/databus/impl/DatabusEventSubscriptionE2ETest.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright (c) 2020 Broadcom.
+ * The term "Broadcom" refers to Broadcom Inc. and/or its subsidiaries.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Broadcom, Inc. - initial API and implementation
+ */
+
 package com.broadcom.lsp.domain.cobol.databus.impl;
 
 import com.broadcom.lsp.domain.cobol.event.model.DataEvent;
@@ -43,7 +57,7 @@ public class DatabusEventSubscriptionE2ETest extends DatabusConfigProvider {
 
     try {
       databusSubscriptionForPositiveScenario(
-          DataEventType.REQUIRED_COPYBOOK_EVENT, DataEventType.FETCHED_COPYBOOK_EVENT);
+          DataEventType.REQUIRED_COPYBOOK_EVENT, DataEventType.REQUIRED_COPYBOOK_EVENT);
       databusSubscriptionForPositiveScenario(
           DataEventType.FETCHED_COPYBOOK_EVENT, DataEventType.FETCHED_COPYBOOK_EVENT);
       databusSubscriptionForPositiveScenario(
