7347ade901c1a13163362ab3199cc770a0b340dd
Andrei Temnikov <andrei.temnikov@broadcom.com>
2020-02-25 11:21:50 +0100

    Add missing text sync type for quick fix #215

--- com.ca.lsp.cobol/lsp-service-cobol/src/main/java/com/ca/lsp/cobol/service/TextDocumentSyncType.java
+++ com.ca.lsp.cobol/lsp-service-cobol/src/main/java/com/ca/lsp/cobol/service/TextDocumentSyncType.java
@@ -1,6 +1,6 @@
 package com.ca.lsp.cobol.service;
 
-/** This enum class is used to track text syncronization type for the processed document. */
+/** This enum class used to track text synchronization type for the processed document. */
 public enum TextDocumentSyncType {
   DID_OPEN,
   DID_CHANGE
