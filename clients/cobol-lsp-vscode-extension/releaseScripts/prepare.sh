1ea8491880016291570f056ba36deb6d2bbb708e
Anton Grigorev <anton.grigorev@broadcom.com>
2020-03-05 14:08:32 +0100

    Add ability to change version inside release branch.
    Signed-off-by: Anton Grigorev <anton.grigorev@broadcom.com>

--- clients/cobol-lsp-vscode-extension/releaseScripts/prepare.sh
+++ clients/cobol-lsp-vscode-extension/releaseScripts/prepare.sh
@@ -17,25 +17,48 @@
 set -e
 
 RELEASE=$1
-RELEASE_NOTES=$2
+CURRENT_BRANCH=$2
+RELEASE_NOTES=$3
 
 echo "Prepare for release $RELEASE"
 
-sed -i "s/\"version\": \".*\"/\"version\": \"$RELEASE\"/g" package.json
-sed -i "s/<revision>.*</<revision>$RELEASE</g" ../../com.ca.lsp.cobol/pom.xml
-sed -i "s/lsp-service-cobol-.*\.jar/lsp-service-cobol-$RELEASE.jar/g" server/note.md
-sed -i "s/cobol-language-support_.*\.vsix'/cobol-language-support_$RELEASE.vsix'/g" ../../Jenkinsfile
-sed -i "s/lsp-service-cobol-.*\.jar/lsp-service-cobol-$RELEASE.jar/g" src/extension.ts
-echo "Versions updated"
+update_version () {
+    sed -i "s/\"version\": \".*\"/\"version\": \"$1\"/g" package.json
+    sed -i "s/<revision>.*</<revision>$1</g" ../../com.ca.lsp.cobol/pom.xml
+    sed -i "s/lsp-service-cobol-.*\.jar/lsp-service-cobol-$1.jar/g" server/note.md
+    sed -i "s/cobol-language-support_.*\.vsix'/cobol-language-support_$1.vsix'/g" ../../Jenkinsfile
+    sed -i "s/lsp-service-cobol-.*\.jar/lsp-service-cobol-$1.jar/g" src/extension.ts
+    echo "Versions updated to $1"
+}
+
+update_version $RELEASE
 
 cp CHANGELOG.md ../../
 echo "Changelog copied"
 
-git add package.json CHANGELOG.md ../../CHANGELOG.md ../../com.ca.lsp.cobol/pom.xml server/note.md ../../Jenkinsfile src/extension.ts
+if [ "master" == "$CURRENT_BRANCH" ]; then
+    git add CHANGELOG.md ../../CHANGELOG.md
+fi
+
+git add package.json ../../com.ca.lsp.cobol/pom.xml server/note.md ../../Jenkinsfile src/extension.ts
 git commit -s -m "chore(release): $RELEASE" -m "" -m "$RELEASE_NOTES"
 git push
-echo "Changes committed to develop"
+echo "Changes committed"
 
-git checkout -b "release-$RELEASE"
-git push --set-upstream origin "release-$RELEASE"
-echo "Branch 'release-$RELEASE' created and pushed"
+if [ "development" == "$CURRENT_BRANCH" ]; then
+    INDEX=`expr index $RELEASE -`
+    SHORT_RELEASE=${RELEASE:0:INDEX-1}
+
+    git checkout -b "release-$SHORT_RELEASE"
+    git push --set-upstream origin "release-$SHORT_RELEASE"
+    echo "Branch 'release-$SHORT_RELEASE' created and pushed"
+
+    git checkout development
+    update_version "$SHORT_RELEASE-NEXT"
+    git add package.json ../../com.ca.lsp.cobol/pom.xml server/note.md ../../Jenkinsfile src/extension.ts
+    git commit -s -m "chore(release): Switch to development version"
+    git push
+    echo "Changes with devel version committed"
+fi
+
+git checkout CHANGELOG.md ../../CHANGELOG.md
