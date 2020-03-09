07590009d7668c208c99c0b791e039ab0a84fb22
Aleksandr Sokolov <aleksandr.sokolov@broadcom.com>
2020-03-09 11:29:30 +0100

    readme modified, copyright set to 2020
    Signed-off-by: Aleksandr Sokolov <aleksandr.sokolov@broadcom.com>

--- tests/theia_automation_lsp/lib/file_explorer.py
+++ tests/theia_automation_lsp/lib/file_explorer.py
@@ -1,3 +1,15 @@
+# Copyright (c) 2020 Broadcom.
+# The term "Broadcom" refers to Broadcom Inc. and/or its subsidiaries.
+#
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Contributors:
+#   Broadcom, Inc. - initial API and implementation
+
 from robot.api.deco import keyword
 from selenium.common.exceptions import NoSuchElementException
 from inc.exceptions.general_exception import GeneralException
