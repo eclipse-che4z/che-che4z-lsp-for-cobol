91c90600a1d2528e47621847155b47db12bca0aa
grianbrcom <60617592+grianbrcom@users.noreply.github.com>
2020-03-23 16:04:18 +0100

    Update theiaPrepare.sh

--- tests/theiaPrepare.sh
+++ tests/theiaPrepare.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -x
 
+chmod +rw /home/project
+chmod +rw /home/theia
 cp -a theia_automation_lsp/test_files/cobol/. /home/project/
 mkdir /home/theia/extplugin
 cp ../clients/cobol-lsp-vscode-extension/*.vsix /home/theia/extplugin/
 wget https://marketplace.visualstudio.com/_apis/public/gallery/publishers/bitlang/vsextensions/cobol/6.0.23/vspackage
 mv vspackage /home/theia/extplugin/bitlang.cobol-6.0.23.vsix
 THEIA_DEFAULT_PLUGINS=local-dir:/home/theia/extplugins
-yarn theia start /home/project --hostname=0.0.0.0 &
+nohup yarn theia start /home/project --hostname=0.0.0.0 &
 
