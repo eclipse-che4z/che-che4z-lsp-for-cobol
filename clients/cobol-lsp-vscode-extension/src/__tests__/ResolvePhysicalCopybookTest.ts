859c3f53593741475f546ecfe2892bc7d92b39e7
zacanbrcom <andrea.zaccaro@broadcom.com>
2020-04-21 10:02:35 +0200

    refactor: Introduce URI validation in the Local Copybook Resolver GH-355

--- clients/cobol-lsp-vscode-extension/src/__tests__/ResolvePhysicalCopybookTest.ts
+++ clients/cobol-lsp-vscode-extension/src/__tests__/ResolvePhysicalCopybookTest.ts
@@ -12,11 +12,14 @@
  *   Broadcom, Inc. - initial API and implementation
  */
 
+import * as fs from "fs";
+import * as path from "path";
 import {LocalCopybookResolver} from "../services/settings/LocalCopybookResolver";
 
 const settingsParser: LocalCopybookResolver = new LocalCopybookResolver();
-const PHY_LOCATION = "src\folder\test";
 const STAR_LOCATION = "*";
+const FILENAME: string = "test.cbl";
+const FILENAME_URI: string = createFile();
 
 /* NOTE
 When the settings json contains paths, them should be escaped, but on the server those settings come already escaped
@@ -73,11 +76,11 @@ describe("test parse method against bad setting configurations", () => {
 
 describe("test parse method with allowed setting configuration", () => {
     test("parse a setting file with key 'LOCAL' return the values in an array", () => {
-        assertParseOf({"broadcom-cobol-lsp.cpy-manager.local": [PHY_LOCATION]}, 1);
+        assertParseOf({"broadcom-cobol-lsp.cpy-manager.local": [FILENAME_URI]}, 1);
     });
 
     test("parse a setting file with etherogeneous keys with the key 'LOCAL' return the value in an array", () => {
-        assertParseOf({"key": "value", "broadcom-cobol-lsp.cpy-manager.local": [PHY_LOCATION]}, 1);
+        assertParseOf({"key": "value", "broadcom-cobol-lsp.cpy-manager.local": [FILENAME_URI]}, 1);
     });
 
 });
@@ -90,7 +93,7 @@ describe("validate path resource with bad configuration", () => {
     test("an array of paths eith defined as '*' are not resolved in an etherogeneous array and are excluded from the result'", () => {
         assertParseOf({
             "key": "value",
-            "broadcom-cobol-lsp.cpy-manager.local": [PHY_LOCATION, STAR_LOCATION],
+            "broadcom-cobol-lsp.cpy-manager.local": [FILENAME_URI, STAR_LOCATION],
         }, 1);
     });
 
@@ -101,7 +104,6 @@ describe("validate path resource with bad configuration", () => {
         }, 0);
     });
 
-    //TODO: Question - what happen if a list contains one valid resource and one malformed?
     test("a not valid path is not resolved and and empty array of resolved paths is returned", () => {
         assertParseOf({
             "key": "value",
@@ -109,14 +111,66 @@ describe("validate path resource with bad configuration", () => {
         }, 0);
     });
 
-});
-
-describe("validate path resource with correct configuration", () => {
-    test("a correct escaped path is found and the list of valid URI is returned", () => {
-        expect(true).toBe(true);
+    test("only not valid URIs are not calculated and included in the result list returned", () => {
+        assertParseOf({
+            "key": "value",
+            "broadcom-cobol-lsp.cpy-manager.local": ["%", FILENAME_URI],
+        }, 1);
     });
 });
 
-function assertParseOf(value: any, expectedListSize: number) {
-    expect(settingsParser.resolveLocation(JSON.stringify(value)).length).toBe(expectedListSize);
+describe("validate path resource with correct configuration", () => {
+    it("a correct escaped path is found and the list of valid URI is returned", () => {
+        assertParseOf(prepareJson(), 1);
+        // deleteTempFile();
+    });
+
+    it("a not existent path is included in the json but is not included in the returned URI list", () => {
+        assertParseOf({
+            "key": "value",
+            "broadcom-cobol-lsp.cpy-manager.local": ["URI_UNDEFINED"],
+        }, 0);
+    });
+
+    it("a correct Windows-Like path is provided in the json and the list of valid URI is returned", () => {
+
+    });
+
+});
+afterAll(() => {
+    return deleteTempFile();
+});
+
+function createFile(): string {
+    //TODO: check if is possible improve it..
+    fs.writeFile(FILENAME, "Some dummy content", err => {
+        if (err) {
+            return null;
+        }
+    });
+    return path.resolve(FILENAME);
+}
+
+function deleteTempFile() {
+    const filePath: string = path.resolve(FILENAME);
+    if (fs.existsSync(filePath)) {
+        fs.unlinkSync(filePath);
+    }
+}
+
+function prepareJson() {
+    const testURI: string = createFile();
+
+    if (testURI === null) {
+        throw new Error("URI not found");
+    }
+
+    return {
+        "key": "value",
+        "broadcom-cobol-lsp.cpy-manager.local": [testURI],
+    };
+}
+
+function assertParseOf(value: any, expectedSizeList: number) {
+    expect(settingsParser.resolveLocation(JSON.stringify(value)).length).toBe(expectedSizeList);
 }
