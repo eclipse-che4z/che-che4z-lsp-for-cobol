6c126fedc8f9d6548aa4f168c27ef2d08d5afc39
zacanbrcom <andrea.zaccaro@broadcom.com>
2020-04-27 09:50:40 +0200

    Address merge conflicts with development

--- clients/cobol-lsp-vscode-extension/src/extension.ts
+++ clients/cobol-lsp-vscode-extension/src/extension.ts
@@ -28,6 +28,9 @@ import { LanguageClientService } from "./services/LanguageClientService";
 import { PathsService } from "./services/PathsService";
 import { ProfileService } from "./services/ProfileService";
 import { ProfilesMap, ZoweApi } from "./services/ZoweApi";
+import {resolveLocalCopybooks} from "./commands/ResolveLocalCopybooks";
+import {LocalCopybookResolver} from "./services/settings/LocalCopybookResolver";
+import {CopybookResolver} from "./services/settings/CopybookResolver";
 
 export async function activate(context: vscode.ExtensionContext) {
     initializeSettings();
@@ -39,6 +42,7 @@ export async function activate(context: vscode.ExtensionContext) {
     const copyBooksDownloader: CopybooksDownloader = new CopybooksDownloader(copybookFix, zoweApi, profileService, copybooksPathGenerator);
     const languageClientService: LanguageClientService = new LanguageClientService(copybooksPathGenerator);
     const pathsService: PathsService = new PathsService();
+    const copybookResolver: CopybookResolver = new LocalCopybookResolver();
 
     try {
         await languageClientService.checkPrerequisites();
@@ -71,6 +75,12 @@ export async function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(vscode.commands.registerCommand("broadcom-cobol-lsp.cpy-manager.edit-dataset-paths", () => {
         editDatasetPaths(pathsService);
     }));
+
+    //POC Command: Used just to quickly share the resolution - might be removed later on.
+    context.subscriptions.push(vscode.commands.registerCommand("broadcom-cobol-lsp.cpy-manager.resolve-local-copybooks", () => {
+        resolveLocalCopybooks(copybookResolver);
+    }));
+
 
     context.subscriptions.push(languageClientService.start());
     context.subscriptions.push(initWorkspaceTracker(copyBooksDownloader));
