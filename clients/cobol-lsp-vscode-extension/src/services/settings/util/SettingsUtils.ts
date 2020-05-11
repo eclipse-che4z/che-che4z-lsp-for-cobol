f4add7629a10cf0008a9674cf0f679cc701d4db8
Andrea Zaccaro <andrea.zaccaro@broadcom.com>
2020-05-11 15:48:01 +0200

    Resolve local URI from settings
    * Add test to parse json like settings
    
    * Add test related to path validation
    
    * Refactor function to parse paths and exclude not valid URIs
    
    * Introduce URI validation in the Local Copybook Resolver
    
    * Add unit test to avoid duplicates in URI list
    
    * Minor changes on API
    
    * Fix setting key property
    
    * Address merge conflicts with development
    
    * Address conflict with existent path key in json definition
    
    * Add URI normalize to avoid unexpected result - as double slashes
    
    * Fix key in order to support zowe and local scenario under common key paths
    
    * Fix workspace path
    
    * Update API to return URI in the result array
    
    * Apply minor enhancement
    
    * Address request change
    
    * Address request change to readability improvements

---
+++ clients/cobol-lsp-vscode-extension/src/services/settings/util/SettingsUtils.ts
@@ -0,0 +1,30 @@
+import * as vscode from "vscode";
+/*
+ * Copyright (c) 2020 Broadcom.
+ * The term "Broadcom" refers to Broadcom Inc. and/or its subsidiaries.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Broadcom, Inc. - initial API and implementation
+ */
+export class SettingsUtils {
+    public static isValidJSON(json: string): boolean {
+        return (json !== undefined) ? JSON.parse(json) : false;
+    }
+
+    /**From a configuration of single/multi workspace folders, their URIs are collected and returned in a util method.
+     * @returns list of resolved URI in FS style
+     */
+    public static getWorkspacesURI(): string[] {
+        const result: string[] = [];
+        vscode.workspace.workspaceFolders.forEach(workspace => {
+            result.push(workspace.uri.scheme + "://" + workspace.uri.path);
+        });
+        return result;
+    }
+}
