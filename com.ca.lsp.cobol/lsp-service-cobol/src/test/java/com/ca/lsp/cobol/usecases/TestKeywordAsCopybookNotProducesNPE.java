c5260076fb66b30bdef759d0e83f848ed3c8402c
Andrei Temnikov <andrei.temnikov@broadcom.com>
2020-08-06 14:23:41 +0200

    fix: Allow keyword as a copybook name #304

--- com.ca.lsp.cobol/lsp-service-cobol/src/test/java/com/ca/lsp/cobol/usecases/TestKeywordAsCopybookNotProducesNPE.java
+++ com.ca.lsp.cobol/lsp-service-cobol/src/test/java/com/ca/lsp/cobol/usecases/TestKeywordAsCopybookNotProducesNPE.java
@@ -17,28 +17,38 @@ package com.ca.lsp.cobol.usecases;
 
 import com.ca.lsp.cobol.positive.CobolText;
 import com.ca.lsp.cobol.usecases.engine.UseCaseEngine;
+import org.eclipse.lsp4j.Diagnostic;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.util.Map;
 
+import static org.eclipse.lsp4j.DiagnosticSeverity.Information;
+
 /**
  * This test checks that there is no syntax error shown on a copybook usage when the copybook name
- * is a keyword (DETAIL in this case).
+ * is a keyword.
  */
 class TestKeywordAsCopybookNotProducesNPE {
 
   private static final String TEXT =
-      "0      Identification Division. \n"
-          + "1      Program-id.    ProgramId.\n"
-          + "2      Data Division.\n"
-          + "3      Working-Storage Section.\n"
-          + "4      01   {$*PARENT1}. \n"
-          + "5       COPY {~DETAIL}. \n"
-          + "6      Procedure Division.\n"
-          + "7      {#*000-Main-Logic}.\n"
-          + "8          DISPLAY {$CHILD1} OF {$PARENT1}.\n"
-          + "9      End program ProgramId.";
+      "       Identification Division. \n"
+          + "       Program-id.    ProgramId.\n"
+          + "       Data Division.\n"
+          + "       Working-Storage Section.\n"
+          + "       01   {$*PARENT1}. \n"
+          + "        COPY {~DETAIL}. \n"
+          + "        COPY {~IDENTIFICATION|long1}. \n"
+          + "        COPY {~ADATA}. \n"
+          + "        COPY {~DOT}. \n"
+          + "        COPY {~DISPLAY}. \n"
+          + "        COPY {~End}. \n"
+          + "        COPY {~NUMERICLITERAL|long2}. \n"
+          + "        COPY {~FILENAME}. \n"
+          + "       Procedure Division.\n"
+          + "       {#*000-Main-Logic}.\n"
+          + "           DISPLAY {$CHILD1} OF {$PARENT1}.\n"
+          + "       End program ProgramId.";
 
   private static final String DETAIL =
       "           03  {$*CHILD1}         PIC 9   VALUE IS '0'.\n"
@@ -48,6 +58,29 @@ class TestKeywordAsCopybookNotProducesNPE {
 
   @Test
   void test() {
-    UseCaseEngine.runTest(TEXT, List.of(new CobolText(DETAIL_NAME, DETAIL)), Map.of());
+    UseCaseEngine.runTest(
+        TEXT,
+        List.of(
+            new CobolText(DETAIL_NAME, DETAIL),
+            new CobolText("IDENTIFICATION", ""),
+            new CobolText("ADATA", ""),
+            new CobolText("DOT", ""),
+            new CobolText("DISPLAY", ""),
+            new CobolText("END", ""),
+            new CobolText("NUMERICLITERAL", ""),
+            new CobolText("FILENAME", "")),
+        Map.of(
+            "long1",
+            new Diagnostic(
+                null,
+                "Copybook declaration has more than 8 characters for: IDENTIFICATION",
+                Information,
+                "COBOL Language Support - I"),
+            "long2",
+            new Diagnostic(
+                null,
+                "Copybook declaration has more than 8 characters for: NUMERICLITERAL",
+                Information,
+                "COBOL Language Support - I")));
   }
 }
