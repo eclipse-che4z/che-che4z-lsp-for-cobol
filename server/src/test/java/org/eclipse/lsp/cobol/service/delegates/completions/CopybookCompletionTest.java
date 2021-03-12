284ea9a193f2cf8d702bddf9891f11538995481d
Andrei Temnikov <andrei.temnikov@broadcom.com>
2021-03-12 11:33:21 +0100

    feat: Show variable definition as documentation
    Resolve documentation requests immediately

--- server/src/test/java/org/eclipse/lsp/cobol/service/delegates/completions/CopybookCompletionTest.java
+++ server/src/test/java/org/eclipse/lsp/cobol/service/delegates/completions/CopybookCompletionTest.java
@@ -15,68 +15,59 @@
 
 package org.eclipse.lsp.cobol.service.delegates.completions;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import org.eclipse.lsp.cobol.service.CobolDocumentModel;
 import org.eclipse.lsp.cobol.service.delegates.validations.AnalysisResult;
-import org.eclipse.lsp4j.*;
+import org.eclipse.lsp4j.CompletionItem;
+import org.eclipse.lsp4j.CompletionItemKind;
 import org.junit.jupiter.api.Test;
 
-import java.util.*;
+import java.util.List;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
- * Testing Copybook autocomplete class, to determine if copybook names will be correctly detected
- * and shown in the autocomplete list
+ * This test {@link CopybookCompletion} asserts that filtration and preparing the copybook completion
+ * suggestions works correctly
  */
 class CopybookCompletionTest {
 
-  private static final String TEXT =
-      "       IDENTIFICATION DIVISION.\n"
-          + "       PROGRAM-ID. ID1.\n"
-          + "       AUTHOR. Author."
-          + "       ENVIRONMENT DIVISION.\n"
-          + "       DATA DIVISION.\n"
-          + "       WORKING-STORAGE SECTION.\n"
-          + "       01 TBPARM1      PIC 99 VALUE 5.\n"
-          + "       01 TBPARM2      PIC 99 VALUE 0.\n"
-          + "       01 ATCDEM4      PIC X(7) VALUE 'ATCDEM4'.\n"
-          + "       01 P1PARM1      PIC 99 VALUE 0.\n"
-          + "       COPY BRDCMCP.\n"
-          + "       PROCEDURE DIVISION.\n"
-          + "       PROGB.\n"
-          + "           PERFORM WITH TEST BEFORE UNTIL TBPARM1 = 0\n"
-          + "             SUBTRACT 1 FROM T\n"
-          + "             CALL 'ATCDEM4' \n"
-          + "           END-PERFORM\n"
-          + "       END PROGRAM ID1.";
+  private Completion completion = new CopybookCompletion();
 
   @Test
-  void testCopybookCompletion() {
-    CobolDocumentModel document = createModel();
-    Set<Completion> completionSet = new HashSet<>();
-    completionSet.add(new CopybookCompletion());
-    Completions completions = new Completions(completionSet);
-    List<CompletionItem> completionItems =
-        completions.collectFor(document, createCompletionParams()).getItems();
-
-    assertEquals(1, completionItems.size());
-    assertEquals("BRDCMCP", completionItems.get(0).getLabel());
-    assertEquals(CompletionItemKind.Class, completionItems.get(0).getKind());
+  void testCompletionEmptyResult() {
+    assertThat(
+        completion.getCompletionItems(
+            "smth",
+            new CobolDocumentModel(
+                "", AnalysisResult.builder().copybookUsages(ImmutableMap.of()).build())),
+        is(empty()));
   }
 
-  private CompletionParams createCompletionParams() {
-    return new CompletionParams(new TextDocumentIdentifier("id"), new Position(12, 30));
+  @Test
+  void testCompletionNull() {
+    assertThat(completion.getCompletionItems("smth", null), is(empty()));
   }
 
-  private CobolDocumentModel createModel() {
-    Map<String, List<Location>> copybookUsages = new HashMap<>();
-    copybookUsages.put(
-        "BRDCMCP",
-        Collections.singletonList(
-            new Location(null, new Range(new Position(9, 11), new Position(9, 18)))));
+  @Test
+  void testCompletionMock() {
+    assertEquals(createExpected(), completion.getCompletionItems("cp", MockCompletionModel.MODEL));
+  }
 
-    AnalysisResult result = AnalysisResult.empty().toBuilder().copybookUsages(copybookUsages).build();
+  private List<CompletionItem> createExpected() {
+    return ImmutableList.of(createItem("cpyU1"), createItem("CpyU2"));
+  }
 
-    return new CobolDocumentModel(TEXT, result);
+  private CompletionItem createItem(String name) {
+    CompletionItem item = new CompletionItem(name);
+    item.setLabel(name);
+    item.setInsertText(name);
+    item.setKind(CompletionItemKind.Class);
+    item.setSortText("3" + name);
+    return item;
   }
 }
---
+++ server/src/test/java/org/eclipse/lsp/cobol/service/delegates/completions/MockCompletionModel.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2020 Broadcom.
+ * The term "Broadcom" refers to Broadcom Inc. and/or its subsidiaries.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Broadcom, Inc. - initial API and implementation
+ *
+ */
+
+package org.eclipse.lsp.cobol.service.delegates.completions;
+
+import com.google.common.collect.ImmutableList;
+import org.eclipse.lsp.cobol.core.model.variables.Variable;
+import org.eclipse.lsp.cobol.service.CobolDocumentModel;
+import org.eclipse.lsp.cobol.service.delegates.validations.AnalysisResult;
+import org.eclipse.lsp4j.Location;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.google.common.collect.ImmutableMap.of;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/** This class stores a model to assert the completion providers */
+class MockCompletionModel {
+  private static final List<Location> LOCATIONS = new ArrayList<>();
+  private static final Variable VAR1 = mock(Variable.class);
+  private static final Variable VAR2 = mock(Variable.class);
+  private static final Variable VAR_NOT_EXPECTED = mock(Variable.class);
+  static final AnalysisResult RESULT =
+      AnalysisResult.builder()
+          .variableDefinitions(of("varD1", LOCATIONS, "VarD2", LOCATIONS, "Not-varD", LOCATIONS))
+          .variableUsages(of("varU1", LOCATIONS, "VarU2", LOCATIONS, "Not-varU", LOCATIONS))
+          .paragraphDefinitions(of("parD1", LOCATIONS, "ParD2", LOCATIONS, "Not-parD", LOCATIONS))
+          .paragraphUsages(of("parU1", LOCATIONS, "ParU2", LOCATIONS, "Not-parU", LOCATIONS))
+          .sectionDefinitions(of("secD1", LOCATIONS, "SecD2", LOCATIONS, "Not-secD", LOCATIONS))
+          .sectionUsages(of("secU1", LOCATIONS, "SecU2", LOCATIONS, "Not-secU", LOCATIONS))
+          .constantDefinitions(
+              of("constD1", LOCATIONS, "ConstD2", LOCATIONS, "Not-constD", LOCATIONS))
+          .constantUsages(of("constU1", LOCATIONS, "ConstU2", LOCATIONS, "Not-constU", LOCATIONS))
+          .copybookDefinitions(of("cpyD1", LOCATIONS, "CpyD2", LOCATIONS, "Not-cpyD", LOCATIONS))
+          .copybookUsages(of("cpyU1", LOCATIONS, "CpyU2", LOCATIONS, "Not-cpyU", LOCATIONS))
+          .subroutineDefinitions(of("subD1", LOCATIONS, "SubD2", LOCATIONS, "Not-subD", LOCATIONS))
+          .subroutineUsages(of("subU1", LOCATIONS, "SubU2", LOCATIONS, "Not-subU", LOCATIONS))
+          .variables(ImmutableList.of(VAR1, VAR2, VAR_NOT_EXPECTED))
+          .build();
+  static final CobolDocumentModel MODEL = new CobolDocumentModel("some text", RESULT);
+
+  static {
+    when(VAR1.getName()).thenReturn("var1");
+    when(VAR1.getFormattedDisplayLine()).thenReturn("expected1");
+    when(VAR2.getName()).thenReturn("VAR2");
+    when(VAR2.getFormattedDisplayLine()).thenReturn("expected2");
+    when(VAR_NOT_EXPECTED.getName()).thenReturn("Not-var");
+    when(VAR_NOT_EXPECTED.getFormattedDisplayLine()).thenReturn("not-expected");
+  }
+}
