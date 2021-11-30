f6776b9452581845c98901966aac06ee2cbb3079
Sachin Shitole <90305904+sachin-shitole-paccar@users.noreply.github.com>
2021-11-30 17:58:43 +0530

    feat: Add grammar for DaCo R/W transaction statement #1126
    Add grammar for DaCo Read & Write transaction statement
    Added snippet for DaCo statements for suggestion

---
+++ server/src/test/java/org/eclipse/lsp/cobol/usecases/TestDAFWriteTransactionToDoesNotProduceNPE.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2021 DAF Trucks NV.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ * DAF Trucks NV – implementation of DaCo COBOL statements
+ * and DAF development standards
+ */
+package org.eclipse.lsp.cobol.usecases;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import org.eclipse.lsp.cobol.service.delegates.validations.SourceInfoLevels;
+import org.eclipse.lsp.cobol.usecases.engine.UseCaseEngine;
+import org.eclipse.lsp4j.Diagnostic;
+import org.eclipse.lsp4j.DiagnosticSeverity;
+import org.junit.jupiter.api.Test;
+
+/** This test for DAF WRITE TRANSACTION TO statement for Null Pointer Exception */
+class TestDAFWriteTransactionToDoesNotProduceNPE {
+
+  private static final String TEXT =
+      "        IDENTIFICATION DIVISION. \r\n"
+          + "        PROGRAM-ID. test1. \r\n"
+          + "        DATA DIVISION. \r\n"
+          + "        WORKING-STORAGE SECTION. \r\n"
+          + "        PROCEDURE DIVISION. \r\n"
+          + "            WRITE TRANSACTION 3167 LENGTH 25 TO{|1} \r\n";
+
+  @Test
+  void test() {
+
+    UseCaseEngine.runTest(
+        TEXT,
+        ImmutableList.of(),
+        ImmutableMap.of(
+            "1",
+            new Diagnostic(
+                null,
+                "Unexpected end of file",
+                DiagnosticSeverity.Error,
+                SourceInfoLevels.ERROR.getText())));
+  }
+}
