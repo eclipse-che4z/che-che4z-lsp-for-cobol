7ebdfd808dd59b0681b521a11f582931fe94f171
Sachin Shitole <sachin.shitole@paccar.com>
2022-01-20 18:41:13 +0530

    feat: Add DaCo DML Table Row retrieval statements support #1126

--- server/src/test/java/org/eclipse/lsp/cobol/usecases/TestDAFTableRowGetStatement.java
+++ server/src/test/java/org/eclipse/lsp/cobol/usecases/TestDAFTableRowGetStatement.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 DAF Trucks NV.
+ * Copyright (c) 2022 DAF Trucks NV.
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
