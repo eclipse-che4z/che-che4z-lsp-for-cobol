10cfe944f1b304a15f2e423e3944aa10e19c70ae
Andrei Temnikov <andrei.temnikov@broadcom.com>
2022-01-28 17:58:11 +0100

    refactor: Encapsulate range utils

--- server/src/main/java/org/eclipse/lsp/cobol/core/preprocessor/delegates/util/RangeUtils.java
+++ server/src/main/java/org/eclipse/lsp/cobol/core/preprocessor/delegates/util/RangeUtils.java
@@ -18,9 +18,7 @@ package org.eclipse.lsp.cobol.core.preprocessor.delegates.util;
 import lombok.experimental.UtilityClass;
 import org.eclipse.lsp.cobol.core.model.Locality;
 import org.eclipse.lsp.cobol.core.model.tree.Node;
-import org.eclipse.lsp4j.DocumentSymbol;
 import org.eclipse.lsp4j.Position;
-import org.eclipse.lsp4j.Range;
 
 import java.util.Optional;
 
@@ -37,57 +35,15 @@ public class RangeUtils {
    * @return the found node
    */
   public static Optional<Node> findNodeByPosition(Node node, String uri, Position position) {
-    if (!RangeUtils.isInside(uri, position, node.getLocality())) return Optional.empty();
+    if (!isInside(uri, position, node.getLocality())) return Optional.empty();
     if (node.getChildren().isEmpty()) return Optional.of(node);
 
     return node.getChildren().stream()
-        .filter(it -> RangeUtils.isInside(uri, position, it.getLocality()))
+        .filter(it -> isInside(uri, position, it.getLocality()))
         .map(it -> findNodeByPosition(it, uri, position))
         .filter(Optional::isPresent)
         .findFirst()
         .orElse(Optional.of(node));
-  }
-
-  /**
-   * Tests if innerSymbol programming construct is placed inside the outerSymbol programming
-   * construct, by comparing its ranges.
-   *
-   * @param outerSymbol the possible wrapping DocumentSymbol.
-   * @param innerSymbol the possible nested DocumentSymbol.
-   * @return true if and only if the innerSymbol's range is inside the outerSymbol's range.
-   */
-  public boolean isInsideRange(DocumentSymbol outerSymbol, DocumentSymbol innerSymbol) {
-    return isInsideRange(outerSymbol.getRange(), innerSymbol.getRange());
-  }
-
-  /**
-   * Tests if inner range is nested into the outer range.
-   *
-   * @param outerRange an outline range.
-   * @param innerRange an outline range.
-   * @return true if and only if the start of inner range is after the outer range and the end of
-   *     inner range is before the outer range.
-   */
-  public boolean isInsideRange(Range outerRange, Range innerRange) {
-    return isBefore(outerRange.getStart(), innerRange.getStart())
-        && isAfter(outerRange.getEnd(), innerRange.getEnd());
-  }
-
-  /**
-   * Compare two document positions. The line is compared first. The character will be compared if
-   * lines are the same.
-   *
-   * @param first the first position to be compared.
-   * @param second the second position to be compared.
-   * @return the value 0 if the argument position is equal to this position; a value less than 0 if
-   *     this position is located before the argument position; and a value greater than 0 if this
-   *     position is located after the argument position.
-   */
-  public int compareTo(Position first, Position second) {
-    int lineComparison = Integer.compare(first.getLine(), second.getLine());
-    return lineComparison == 0
-        ? Integer.compare(first.getCharacter(), second.getCharacter())
-        : lineComparison;
   }
 
   /**
@@ -114,17 +70,16 @@ public class RangeUtils {
     return compareTo(after, before) < 0;
   }
 
-  /**
-   * Tests if text document position resides inside the location.
-   *
-   * @param uri the uri of the node
-   * @param position the position of the node to add
-   * @param location the locality of the wrapping node
-   * @return true if position is inside the location.
-   */
-  public boolean isInside(String uri, Position position, Locality location) {
+  private boolean isInside(String uri, Position position, Locality location) {
     return uri.equals(location.getUri())
         && !isBefore(position, location.getRange().getStart())
         && !isAfter(position, location.getRange().getEnd());
+  }
+
+  private int compareTo(Position first, Position second) {
+    int lineComparison = Integer.compare(first.getLine(), second.getLine());
+    return lineComparison == 0
+        ? Integer.compare(first.getCharacter(), second.getCharacter())
+        : lineComparison;
   }
 }
