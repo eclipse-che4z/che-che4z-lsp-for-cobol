86462bb9833ab319ad55f7e39554c710e30bd238
Leonid Baranov <leonid.baranov@broadcom.com>
2022-07-04 13:27:08 +0200

    refactor: Fix variable location

--- server/engine/src/main/java/org/eclipse/lsp/cobol/core/engine/TextTransformations.java
+++ server/engine/src/main/java/org/eclipse/lsp/cobol/core/engine/TextTransformations.java
@@ -17,7 +17,6 @@ package org.eclipse.lsp.cobol.core.engine;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.eclipse.lsp.cobol.core.model.tree.CopyNode;
-import org.eclipse.lsp4j.Position;
 import org.eclipse.lsp4j.Range;
 
 import java.util.*;
@@ -76,8 +75,6 @@ public class TextTransformations {
   public void extend(CopyNode copyNode, TextTransformations textTransformations) {
     copyNodes.add(copyNode);
     Range range = copyNode.getLocality().getRange();
-    range = new Range(new Position(range.getStart().getLine(), 0),
-        new Position(range.getEnd().getLine(), range.getEnd().getCharacter()));
     extensions.put(range, textTransformations);
   }
 
