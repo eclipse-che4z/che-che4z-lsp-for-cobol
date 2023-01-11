993d743e29cf85b12738e4db5ff64c60bad40148
Iurii Shchekochikhin <iurii.shchekochikhin@broadcom.com>
2023-01-11 12:03:47 +0100

    Fix intergation test

--- clients/cobol-lsp-vscode-extension/src/test/suite/testHelper.ts
+++ clients/cobol-lsp-vscode-extension/src/test/suite/testHelper.ts
@@ -100,13 +100,13 @@ export function deleteFile(path: string){
 export function recursiveCopySync(origin, dest) {
     if (fs.existsSync(origin)) {
         if (fs.statSync(origin).isDirectory()) {
-            fs.mkdirSync(dest);
+            fs.mkdirSync(dest, {recursive: true});
             fs.readdirSync(origin).forEach(file =>
                 recursiveCopySync(path.join(origin, file), path.join(dest, file)));
         }
         else {
             const destFolder = path.dirname(dest);
-            if(!fs.existsSync(destFolder)) fs.mkdirSync(destFolder);
+            if(!fs.existsSync(destFolder)) fs.mkdirSync(destFolder, {recursive: true});
             fs.copyFileSync(origin, dest);
         }
     }
