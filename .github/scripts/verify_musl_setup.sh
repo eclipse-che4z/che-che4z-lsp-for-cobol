2bc1b74a31cbb9269a8dc90fb6d844871d61fa3f
Aman Prashant <aman.prashant@broadcom.com>
2023-03-31 16:40:30 +0200

    build: Statically link libc and zlib in native build for LINUX
    Signed-off-by: Aman Prashant <aman.prashant@broadcom.com>

--- .github/scripts/verify_musl_setup.sh
+++ .github/scripts/verify_musl_setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set +e
 #Ref - https://www.graalvm.org/22.0/reference-manual/native-image/StaticImages/
 x86_64-linux-musl-gcc
 if [[ ($? -ne 0) && ($1 == *"x86_64-linux-musl-gcc: fatal error: no input files"*) ]]; then
