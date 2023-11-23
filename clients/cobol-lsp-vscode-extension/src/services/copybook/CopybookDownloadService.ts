377902a66a8a910dec17486e1304d8859e77a498
hizir-bc <86234686+hizir-bc@users.noreply.github.com>
2023-11-23 16:08:42 +0100

    fixes

--- clients/cobol-lsp-vscode-extension/src/services/copybook/CopybookDownloadService.ts
+++ clients/cobol-lsp-vscode-extension/src/services/copybook/CopybookDownloadService.ts
@@ -38,6 +38,7 @@ import { Utils } from "../util/Utils";
 import { CopybookURI } from "./CopybookURI";
 import { CopybookProfile, DownloadQueue } from "./DownloadQueue";
 import { getWorkspaceFolders } from "../../Helper";
+import { TelemetryEventMeasurements } from "../reporter/model/TelemetryEvent";
 
 export class CopybookName {
   public constructor(public name: string, public dialect: string) {}
@@ -351,16 +352,16 @@ export class CopybookDownloadService implements vscode.Disposable {
       dialectsDsn.push(...dialectDsn);
     });
 
-    const ussPath: string[] =
-      vscode.workspace.getConfiguration(SETTINGS_CPY_SECTION).get(PATHS_USS) ||
-      [];
-    const providedProfile: string =
-      vscode.workspace.getConfiguration(SETTINGS_CPY_SECTION).get("profiles") ||
-      "";
+    const ussPath: string[] | undefined = vscode.workspace
+      .getConfiguration(SETTINGS_CPY_SECTION)
+      .get(PATHS_USS);
+    const providedProfile: string | undefined = vscode.workspace
+      .getConfiguration(SETTINGS_CPY_SECTION)
+      .get("profiles");
     return (
       dsnPath?.length > 0 ||
-      ussPath?.length > 0 ||
-      providedProfile?.length > 0 ||
+      (ussPath && ussPath?.length > 0) ||
+      (providedProfile && providedProfile.length > 0) ||
       dialectsDsn?.length > 0
     );
   }
@@ -494,12 +495,10 @@ export class CopybookDownloadService implements vscode.Disposable {
           "Download copybooks from MF",
           ["copybook", "COBOL", experimentTag],
           "total time to search copybooks on MF",
-          {
-            ["time elapsed"]: TelemetryService.calculateTimeElapsed(
-              startTime,
-              Date.now(),
-            ),
-          },
+          new Map().set(
+            "time elapsed",
+            TelemetryService.calculateTimeElapsed(startTime, Date.now()),
+          ) as unknown as TelemetryEventMeasurements,
         );
       }
     }
