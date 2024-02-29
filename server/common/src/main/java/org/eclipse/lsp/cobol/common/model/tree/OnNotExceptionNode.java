243c0a337ae62543dd25e5fb9482c0c356eb842f
Leonid Baranov <leonid.baranov@broadcom.com>
2024-02-29 14:32:07 +0100

    chore: Add two nodes for handling exceptions in COBOL statements

---
+++ server/common/src/main/java/org/eclipse/lsp/cobol/common/model/tree/OnNotExceptionNode.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2024 Broadcom.
+ * The term "Broadcom" refers to Broadcom Inc. and/or its subsidiaries.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Broadcom, Inc. - initial API and implementation
+ *
+ */
+package org.eclipse.lsp.cobol.common.model.tree;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+import org.eclipse.lsp.cobol.common.model.Locality;
+import org.eclipse.lsp.cobol.common.model.NodeType;
+
+/** The class represents an ON NOT EXCEPTION statement in Cobol */
+@ToString(callSuper = true)
+@Getter
+@EqualsAndHashCode(callSuper = true)
+public class OnNotExceptionNode extends Node {
+
+  public OnNotExceptionNode(Locality location) {
+    super(location, NodeType.ON_NOT_EXCEPTION);
+  }
+}
