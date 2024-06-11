65e4e800fd6acd086848a54f87c40616417449ae
Michael Messer <michael.messer@broadcom.com>
2024-06-11 08:48:04 -0400

    Ran Prettier
    Ran prettier to fix the linting issues.

--- clients/cobol-lsp-vscode-extension/esbuild.js
+++ clients/cobol-lsp-vscode-extension/esbuild.js
@@ -1,9 +1,9 @@
-const esbuild = require('esbuild');
-const glob = require('glob');
-const path = require('path');
+const esbuild = require("esbuild");
+const glob = require("glob");
+const path = require("path");
 
-const production = process.argv.includes('--production');
-const watch = process.argv.includes('--watch');
+const production = process.argv.includes("--production");
+const watch = process.argv.includes("--watch");
 
 /**
  * This plugin hooks into the build process to print errors in a format that the problem matcher in
@@ -11,22 +11,23 @@ const watch = process.argv.includes('--watch');
  * @type {import('esbuild').Plugin}
  */
 const esbuildProblemMatcherPlugin = {
-    name: 'esbuild-problem-matcher',
+  name: "esbuild-problem-matcher",
 
-    setup(build) {
-        build.onStart(() => {
-            console.log('[watch] build started');
-        });
-        build.onEnd((result) => {
-            result.errors.forEach(({ text, location }) => {
-                console.error(`✘ [ERROR] ${text}`);
-                console.error(`    ${location.file}:${location.line}:${location.column}:`);
-            });
-            console.log('[watch] build finished');
-        });
-    },
+  setup(build) {
+    build.onStart(() => {
+      console.log("[watch] build started");
+    });
+    build.onEnd((result) => {
+      result.errors.forEach(({ text, location }) => {
+        console.error(`✘ [ERROR] ${text}`);
+        console.error(
+          `    ${location.file}:${location.line}:${location.column}:`,
+        );
+      });
+      console.log("[watch] build finished");
+    });
+  },
 };
-
 
 /**
  * For web extension, all tests, including the test runner, need to be bundled into
@@ -35,61 +36,64 @@ const esbuildProblemMatcherPlugin = {
  * @type {import('esbuild').Plugin}
  */
 const testBundlePlugin = {
-    name: 'testBundlePlugin',
-    setup(build) {
-        build.onResolve({ filter: /[\/\\]extensionTests\.ts$/ }, args => {
-            if (args.kind === 'entry-point') {
-                return { path: path.resolve(args.path) };
-            }
-        });
-        build.onLoad({ filter: /[\/\\]extensionTests\.ts$/ }, async args => {
-            const testsRoot = path.join(__dirname, 'src/web/test/suite');
-            const files = await glob.glob('*.test.{ts,tsx}', { cwd: testsRoot, posix: true });
-            return {
-                contents:
-                    `export { run } from './mochaTestRunner.ts';` +
-                    files.map(f => `import('./${f}');`).join(''),
-                watchDirs: files.map(f => path.dirname(path.resolve(testsRoot, f))),
-                watchFiles: files.map(f => path.resolve(testsRoot, f))
-            };
-        });
-    }
+  name: "testBundlePlugin",
+  setup(build) {
+    build.onResolve({ filter: /[\/\\]extensionTests\.ts$/ }, (args) => {
+      if (args.kind === "entry-point") {
+        return { path: path.resolve(args.path) };
+      }
+    });
+    build.onLoad({ filter: /[\/\\]extensionTests\.ts$/ }, async (args) => {
+      const testsRoot = path.join(__dirname, "src/web/test/suite");
+      const files = await glob.glob("*.test.{ts,tsx}", {
+        cwd: testsRoot,
+        posix: true,
+      });
+      return {
+        contents:
+          `export { run } from './mochaTestRunner.ts';` +
+          files.map((f) => `import('./${f}');`).join(""),
+        watchDirs: files.map((f) => path.dirname(path.resolve(testsRoot, f))),
+        watchFiles: files.map((f) => path.resolve(testsRoot, f)),
+      };
+    });
+  },
 };
 
 async function main() {
-    const ctx = await esbuild.context({
-        entryPoints: [
-            'src/web/extension.ts',
-            'src/web/test/suite/extensionTests.ts'
-        ],
-        bundle: true,
-        format: 'cjs',
-        minify: production,
-        sourcemap: !production,
-        sourcesContent: false,
-        platform: 'browser',
-        outdir: 'dist/web',
-        external: ['vscode'],
-        logLevel: 'silent',
-        // Node.js global to browser globalThis
-        define: {
-            global: 'globalThis',
-        },
+  const ctx = await esbuild.context({
+    entryPoints: [
+      "src/web/extension.ts",
+      "src/web/test/suite/extensionTests.ts",
+    ],
+    bundle: true,
+    format: "cjs",
+    minify: production,
+    sourcemap: !production,
+    sourcesContent: false,
+    platform: "browser",
+    outdir: "dist/web",
+    external: ["vscode"],
+    logLevel: "silent",
+    // Node.js global to browser globalThis
+    define: {
+      global: "globalThis",
+    },
 
-        plugins: [
-            testBundlePlugin,
-            esbuildProblemMatcherPlugin, /* add to the end of plugins array */
-        ],
-    });
-    if (watch) {
-        await ctx.watch();
-    } else {
-        await ctx.rebuild();
-        await ctx.dispose();
-    }
+    plugins: [
+      testBundlePlugin,
+      esbuildProblemMatcherPlugin /* add to the end of plugins array */,
+    ],
+  });
+  if (watch) {
+    await ctx.watch();
+  } else {
+    await ctx.rebuild();
+    await ctx.dispose();
+  }
 }
 
-main().catch(e => {
-    console.error(e);
-    process.exit(1);
+main().catch((e) => {
+  console.error(e);
+  process.exit(1);
 });
--- clients/cobol-lsp-vscode-extension/src/web/extension.ts
+++ clients/cobol-lsp-vscode-extension/src/web/extension.ts
@@ -1,6 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
