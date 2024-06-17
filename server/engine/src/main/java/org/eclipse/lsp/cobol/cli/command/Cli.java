af0acd234513cfa025dd786bdbc9ec7e269ff963
Iurii Shchekochikhin <iurii.shchekochikhin@broadcom.com>
2024-06-17 17:28:18 +0200

    feat: add GC stats and memory to CLI runner

--- server/engine/src/main/java/org/eclipse/lsp/cobol/cli/command/Cli.java
+++ server/engine/src/main/java/org/eclipse/lsp/cobol/cli/command/Cli.java
@@ -16,52 +16,39 @@ package org.eclipse.lsp.cobol.cli.command;
 
 import com.google.gson.*;
 import com.google.inject.Injector;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.*;
 import java.util.concurrent.Callable;
+
 import lombok.extern.slf4j.Slf4j;
-import org.antlr.v4.runtime.tree.ParseTreeListener;
 import org.eclipse.lsp.cobol.cli.processorgroups.ProcessorGroupsResolver;
 import org.eclipse.lsp.cobol.common.AnalysisConfig;
 import org.eclipse.lsp.cobol.common.CleanerPreprocessor;
 import org.eclipse.lsp.cobol.common.ResultWithErrors;
-import org.eclipse.lsp.cobol.common.SubroutineService;
 import org.eclipse.lsp.cobol.common.benchmark.BenchmarkService;
 import org.eclipse.lsp.cobol.common.benchmark.BenchmarkSession;
 import org.eclipse.lsp.cobol.common.benchmark.Measurement;
 import org.eclipse.lsp.cobol.common.copybook.CopybookProcessingMode;
-import org.eclipse.lsp.cobol.common.copybook.CopybookService;
 import org.eclipse.lsp.cobol.common.dialects.CobolLanguageId;
-import org.eclipse.lsp.cobol.common.error.SyntaxError;
+
 import org.eclipse.lsp.cobol.common.mapping.ExtendedDocument;
 import org.eclipse.lsp.cobol.common.mapping.ExtendedText;
-import org.eclipse.lsp.cobol.common.message.MessageService;
 import org.eclipse.lsp.cobol.common.pipeline.Pipeline;
 import org.eclipse.lsp.cobol.common.pipeline.PipelineResult;
 import org.eclipse.lsp.cobol.core.engine.analysis.AnalysisContext;
-import org.eclipse.lsp.cobol.core.engine.dialects.DialectService;
-import org.eclipse.lsp.cobol.core.engine.processor.AstProcessor;
-import org.eclipse.lsp.cobol.core.engine.symbols.SymbolsRepository;
-import org.eclipse.lsp.cobol.core.preprocessor.delegates.GrammarPreprocessor;
 import org.eclipse.lsp.cobol.dialects.TrueDialectServiceImpl;
-import org.eclipse.lsp.cobol.dialects.hp.HpCleanupStage;
-import org.eclipse.lsp.cobol.dialects.hp.HpCopybookProcessingStage;
-import org.eclipse.lsp.cobol.dialects.ibm.*;
-import org.eclipse.lsp.cobol.dialects.ibm.experimental.ExperimentalParserStage;
-import org.eclipse.lsp.cobol.service.settings.CachingConfigurationService;
-import org.eclipse.lsp.cobol.service.settings.layout.CodeLayoutStore;
 import picocli.CommandLine;
 
-/** The Cli class represents a Command Line Interface (CLI) for interacting with the application. */
-@CommandLine.Command(
-    description = "COBOL Analysis CLI tools.",
-    mixinStandardHelpOptions = true,
-    scope = CommandLine.ScopeType.INHERIT,
-    subcommands = {ListSources.class, ListCopybooks.class, CliAnalysis.class})
+import static org.eclipse.lsp.cobol.cli.command.CliUtils.setupPipeline;
+
+/**
+ * The Cli class represents a Command Line Interface (CLI) for interacting with the application.
+ */
+@CommandLine.Command(description = "COBOL Analysis CLI tools.", mixinStandardHelpOptions = true, scope = CommandLine.ScopeType.INHERIT, subcommands = {ListSources.class, ListCopybooks.class, CliAnalysis.class})
 @Slf4j
 public class Cli implements Callable<Integer> {
   ProcessorGroupsResolver processorGroupsResolver;
@@ -77,32 +64,25 @@ public class Cli implements Callable<Integer> {
     return 0;
   }
 
-  Result runAnalysis(File src, CobolLanguageId dialect, Injector diCtx, boolean isAnalysisRequired)
-      throws IOException {
+  Result runAnalysis(File src, CobolLanguageId dialect, Injector diCtx, boolean isAnalysisRequired) throws IOException {
     String documentUri = src.toURI().toString();
     Pipeline<AnalysisContext> pipeline = setupPipeline(diCtx, isAnalysisRequired, dialect);
 
     // Cleaning up
-    CleanerPreprocessor preprocessor =
-        diCtx.getInstance(TrueDialectServiceImpl.class).getPreprocessor(dialect);
+    CleanerPreprocessor preprocessor = diCtx.getInstance(TrueDialectServiceImpl.class).getPreprocessor(dialect);
     BenchmarkService benchmarkService = diCtx.getInstance(BenchmarkService.class);
 
     String text = new String(Files.readAllBytes(src.toPath()));
     ResultWithErrors<ExtendedText> resultWithErrors = preprocessor.cleanUpCode(documentUri, text);
-    AnalysisContext ctx =
-        new AnalysisContext(
-            new ExtendedDocument(resultWithErrors.getResult(), text),
-            createAnalysisConfiguration(),
-            benchmarkService.startSession(),
-            documentUri,
-            text,
-            dialect);
+    AnalysisContext ctx = new AnalysisContext(new ExtendedDocument(resultWithErrors.getResult(), text), createAnalysisConfiguration(), benchmarkService.startSession(), documentUri, text, dialect);
     ctx.getAccumulatedErrors().addAll(resultWithErrors.getErrors());
     PipelineResult pipelineResult = pipeline.run(ctx);
     return new Result(ctx, pipelineResult);
   }
 
-  /** Result of analysis */
+  /**
+   * Result of analysis
+   */
   static class Result {
     final AnalysisContext ctx;
     final PipelineResult pipelineResult;
@@ -121,10 +101,7 @@ public class Cli implements Callable<Integer> {
     Path groupsConfig = workspace.resolve(Paths.get(".cobolplugin", "proc_grps.json"));
     if (Files.exists(programConfig) && Files.exists(groupsConfig)) {
       try {
-        processorGroupsResolver =
-            new ProcessorGroupsResolver(
-                new String(Files.readAllBytes(programConfig)),
-                new String(Files.readAllBytes(groupsConfig)));
+        processorGroupsResolver = new ProcessorGroupsResolver(new String(Files.readAllBytes(programConfig)), new String(Files.readAllBytes(groupsConfig)));
       } catch (IOException e) {
         LOG.error("Processor group configuration read error", e);
       }
@@ -133,202 +110,14 @@ public class Cli implements Callable<Integer> {
     }
   }
 
-  JsonObject toJson(SyntaxError syntaxError, Gson gson) {
-    JsonObject diagnostic = new JsonObject();
-    Optional.ofNullable(syntaxError.getErrorCode())
-        .ifPresent(code -> diagnostic.add("code", new JsonPrimitive(code.getLabel())));
-    Optional.ofNullable(syntaxError.getErrorSource())
-        .ifPresent(es -> diagnostic.add("source", new JsonPrimitive(es.getText())));
-    Optional.ofNullable(syntaxError.getLocation())
-        .ifPresent(l -> diagnostic.add("location", gson.toJsonTree(l)));
-    Optional.ofNullable(syntaxError.getSeverity())
-        .ifPresent(s -> diagnostic.add("severity", new JsonPrimitive(s.name())));
-    Optional.ofNullable(syntaxError.getSuggestion())
-        .ifPresent(s -> diagnostic.add("suggestion", new JsonPrimitive(s)));
-    Optional.ofNullable(syntaxError.getRelatedInformation())
-        .ifPresent(ri -> diagnostic.add("related", gson.toJsonTree(ri)));
-    return diagnostic;
-  }
-
   void addTiming(JsonObject result, BenchmarkSession benchmarkSession) {
     JsonObject tObj = new JsonObject();
-    benchmarkSession
-        .getMeasurements()
-        .forEach(m -> tObj.add(m.getId(), new JsonPrimitive(m.getTime() / 1_000_000_000.0)));
+    benchmarkSession.getMeasurements().forEach(m -> tObj.add(m.getId(), new JsonPrimitive(m.getTime() / 1_000_000_000.0)));
     result.add("timings", tObj);
-    benchmarkSession.getMeasurements().stream()
-        .map(Measurement::getTime)
-        .reduce(Long::sum)
-        .ifPresent(totalTime -> tObj.add("total", new JsonPrimitive(totalTime / 1_000_000_000.0)));
+    benchmarkSession.getMeasurements().stream().map(Measurement::getTime).reduce(Long::sum).ifPresent(totalTime -> tObj.add("total", new JsonPrimitive(totalTime / 1_000_000_000.0)));
   }
 
   private static AnalysisConfig createAnalysisConfiguration() {
     return AnalysisConfig.defaultConfig(CopybookProcessingMode.ENABLED);
-  }
-
-  private static Pipeline<AnalysisContext> setupPipeline(
-      Injector diCtx, boolean isAnalysisRequired, CobolLanguageId dialect) {
-    DialectService dialectService = diCtx.getInstance(DialectService.class);
-    MessageService messageService = diCtx.getInstance(MessageService.class);
-    GrammarPreprocessor grammarPreprocessor = diCtx.getInstance(GrammarPreprocessor.class);
-    ParseTreeListener parseTreeListener = diCtx.getInstance(ParseTreeListener.class);
-    SymbolsRepository symbolsRepository = diCtx.getInstance(SymbolsRepository.class);
-    SubroutineService subroutineService = diCtx.getInstance(SubroutineService.class);
-    CleanerPreprocessor preprocessor =
-        diCtx.getInstance(TrueDialectServiceImpl.class).getPreprocessor(dialect);
-    CachingConfigurationService cachingConfigurationService =
-        diCtx.getInstance(CachingConfigurationService.class);
-    AstProcessor astProcessor = diCtx.getInstance(AstProcessor.class);
-    CodeLayoutStore layoutStore = diCtx.getInstance(CodeLayoutStore.class);
-    CopybookService copybookService = diCtx.getInstance(CopybookService.class);
-
-    Pipeline<AnalysisContext> pipeline = new Pipeline<>();
-    switch (dialect) {
-      case COBOL:
-        return getPipelineForCobolDialect(
-            isAnalysisRequired,
-            pipeline,
-            preprocessor,
-            messageService,
-            dialectService,
-            grammarPreprocessor,
-            parseTreeListener,
-            symbolsRepository,
-            subroutineService,
-            cachingConfigurationService,
-            astProcessor,
-            layoutStore);
-      case EXPERIMENTAL_COBOL:
-        return getPipelineForExpCobol(
-            isAnalysisRequired,
-            pipeline,
-            preprocessor,
-            messageService,
-            dialectService,
-            grammarPreprocessor,
-            parseTreeListener,
-            symbolsRepository,
-            subroutineService,
-            cachingConfigurationService,
-            astProcessor,
-            layoutStore);
-      case HP_COBOL:
-        return getPipelineForHpCobol(
-            isAnalysisRequired,
-            pipeline,
-            preprocessor,
-            messageService,
-            copybookService,
-            grammarPreprocessor,
-            dialectService,
-            parseTreeListener,
-            symbolsRepository,
-            subroutineService,
-            cachingConfigurationService,
-            astProcessor,
-            layoutStore);
-      default:
-        return pipeline;
-    }
-  }
-
-  private static Pipeline<AnalysisContext> getPipelineForHpCobol(
-      boolean isAnalysisRequired,
-      Pipeline<AnalysisContext> pipeline,
-      CleanerPreprocessor preprocessor,
-      MessageService messageService,
-      CopybookService copybookService,
-      GrammarPreprocessor grammarPreprocessor,
-      DialectService dialectService,
-      ParseTreeListener parseTreeListener,
-      SymbolsRepository symbolsRepository,
-      SubroutineService subroutineService,
-      CachingConfigurationService cachingConfigurationService,
-      AstProcessor astProcessor,
-      CodeLayoutStore layoutStore) {
-    pipeline.add(new HpCleanupStage(preprocessor));
-    pipeline.add(new CompilerDirectivesStage(messageService));
-    pipeline.add(new HpCopybookProcessingStage(messageService, copybookService));
-    pipeline.add(new PreprocessorStage(grammarPreprocessor, preprocessor));
-    if (isAnalysisRequired) {
-      pipeline.add(new ImplicitDialectProcessingStage(dialectService));
-      pipeline.add(new ParserStage(messageService, parseTreeListener));
-      pipeline.add(
-          new TransformTreeStage(
-              symbolsRepository,
-              messageService,
-              subroutineService,
-              cachingConfigurationService,
-              dialectService,
-              astProcessor,
-              layoutStore));
-    }
-    return pipeline;
-  }
-
-  private static Pipeline<AnalysisContext> getPipelineForExpCobol(
-      boolean isAnalysisRequired,
-      Pipeline<AnalysisContext> pipeline,
-      CleanerPreprocessor preprocessor,
-      MessageService messageService,
-      DialectService dialectService,
-      GrammarPreprocessor grammarPreprocessor,
-      ParseTreeListener parseTreeListener,
-      SymbolsRepository symbolsRepository,
-      SubroutineService subroutineService,
-      CachingConfigurationService cachingConfigurationService,
-      AstProcessor astProcessor,
-      CodeLayoutStore layoutStore) {
-    pipeline.add(new IbmCleanupStage(preprocessor));
-    pipeline.add(new CompilerDirectivesStage(messageService));
-    pipeline.add(new DialectProcessingStage(dialectService, preprocessor));
-    pipeline.add(new PreprocessorStage(grammarPreprocessor, preprocessor));
-    if (isAnalysisRequired) {
-      pipeline.add(new ImplicitDialectProcessingStage(dialectService));
-      pipeline.add(new ExperimentalParserStage(messageService, parseTreeListener));
-      pipeline.add(
-          new TransformTreeStage(
-              symbolsRepository,
-              messageService,
-              subroutineService,
-              cachingConfigurationService,
-              dialectService,
-              astProcessor,
-              layoutStore));
-    }
-    return pipeline;
-  }
-
-  private static Pipeline<AnalysisContext> getPipelineForCobolDialect(
-      boolean isAnalysisRequired,
-      Pipeline<AnalysisContext> pipeline,
-      CleanerPreprocessor preprocessor,
-      MessageService messageService,
-      DialectService dialectService,
-      GrammarPreprocessor grammarPreprocessor,
-      ParseTreeListener parseTreeListener,
-      SymbolsRepository symbolsRepository,
-      SubroutineService subroutineService,
-      CachingConfigurationService cachingConfigurationService,
-      AstProcessor astProcessor,
-      CodeLayoutStore layoutStore) {
-    pipeline.add(new IbmCleanupStage(preprocessor));
-    pipeline.add(new CompilerDirectivesStage(messageService));
-    pipeline.add(new DialectProcessingStage(dialectService, preprocessor));
-    pipeline.add(new PreprocessorStage(grammarPreprocessor, preprocessor));
-    if (isAnalysisRequired) {
-      pipeline.add(new ImplicitDialectProcessingStage(dialectService));
-      pipeline.add(new ParserStage(messageService, parseTreeListener));
-      pipeline.add(
-          new TransformTreeStage(
-              symbolsRepository,
-              messageService,
-              subroutineService,
-              cachingConfigurationService,
-              dialectService,
-              astProcessor,
-              layoutStore));
-    }
-    return pipeline;
   }
 }
--- server/engine/src/main/java/org/eclipse/lsp/cobol/cli/command/CliAnalysis.java
+++ server/engine/src/main/java/org/eclipse/lsp/cobol/cli/command/CliAnalysis.java
@@ -14,41 +14,48 @@
  */
 package org.eclipse.lsp.cobol.cli.command;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+
 import java.io.File;
+import java.lang.management.GarbageCollectorMXBean;
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryPoolMXBean;
+import java.lang.management.MemoryType;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.stream.Collectors;
+
 import org.eclipse.lsp.cobol.cli.di.CliModule;
 import org.eclipse.lsp.cobol.cli.modules.CliClientProvider;
 import org.eclipse.lsp.cobol.common.dialects.CobolLanguageId;
 import picocli.CommandLine;
 
-/** Analysis cli command */
+/**
+ * Analysis cli command
+ */
 @CommandLine.Command(name = "analysis", description = "analyse cobol source")
 public class CliAnalysis implements Callable<Integer> {
-  @CommandLine.ParentCommand private Cli parent;
+  @CommandLine.ParentCommand
+  private Cli parent;
 
   @CommandLine.Option(
-      names = {"-s", "--source"},
-      required = true,
-      description = "The COBOL program file.")
+          names = {"-s", "--source"},
+          required = true,
+          description = "The COBOL program file.")
   private File src;
 
   @CommandLine.ArgGroup(multiplicity = "1")
   private Args args;
 
   @CommandLine.Option(
-      description = "Supported dialect values: ${COMPLETION-CANDIDATES}",
-      names = {"-d", "--dialect"},
-      defaultValue = "COBOL")
+          description = "Supported dialect values: ${COMPLETION-CANDIDATES}",
+          names = {"-d", "--dialect"},
+          defaultValue = "COBOL")
   private CobolLanguageId dialect;
 
   @Override
@@ -61,47 +68,86 @@ public class CliAnalysis implements Callable<Integer> {
 
     cliClientProvider.setCpyPaths(createCopybooksPaths());
     cliClientProvider.setCpyExt(createCopybooksExtensions());
-    Gson gson = new GsonBuilder().setPrettyPrinting().create();
     JsonObject result = new JsonObject();
 
     Cli.Result analysisResult = parent.runAnalysis(src, dialect, diCtx, true);
     parent.addTiming(result, analysisResult.ctx.getBenchmarkSession());
     JsonArray diagnostics = new JsonArray();
     analysisResult
-        .ctx
-        .getAccumulatedErrors()
-        .forEach(
-            err -> {
-              JsonObject diagnostic = parent.toJson(err, gson);
-              diagnostics.add(diagnostic);
-            });
+            .ctx
+            .getAccumulatedErrors()
+            .forEach(
+                    err -> {
+                      JsonObject diagnostic = CliUtils.diagnosticToJson(err);
+                      diagnostics.add(diagnostic);
+                    });
     result.add("diagnostics", diagnostics);
-    System.out.println(gson.toJson(result));
+    result.addProperty("uri", analysisResult.ctx.getExtendedDocument().getUri());
+    result.addProperty("language", analysisResult.ctx.getLanguageId().getId());
+    result.addProperty("lines", String.valueOf(analysisResult.ctx.getExtendedDocument().toString().split("\n").length));
+    result.addProperty("size", String.valueOf(analysisResult.ctx.getExtendedDocument().toString().length()));
+    collectGcAndMemoryStats(result);
+    System.out.println(CliUtils.GSON.toJson(result));
     return 0;
   }
 
-  /** WorkspaceConfig options */
+  private void collectGcAndMemoryStats(JsonObject result) {
+    long totalGarbageCollections = 0;
+    long garbageCollectionTime = 0;
+
+    for (GarbageCollectorMXBean gc : ManagementFactory.getGarbageCollectorMXBeans()) {
+      long count = gc.getCollectionCount();
+      if (count >= 0) {
+        totalGarbageCollections += count;
+      }
+      long time = gc.getCollectionTime();
+      if (time >= 0) {
+        garbageCollectionTime += time;
+      }
+    }
+    result.addProperty("gc.count", totalGarbageCollections);
+    // milliseconds to seconds
+    result.addProperty("gc.time", garbageCollectionTime * 0.001);
+
+    List<MemoryPoolMXBean> pools = ManagementFactory.getMemoryPoolMXBeans();
+    long total = 0;
+    for (MemoryPoolMXBean memoryPoolMXBean : pools) {
+      if (memoryPoolMXBean.getType() == MemoryType.HEAP) {
+        long peakUsed = memoryPoolMXBean.getPeakUsage().getUsed();
+        total = total + peakUsed;
+      }
+    }
+    result.addProperty("memory.heap_peak", total / 1024 / 1024);
+  }
+
+  /**
+   * WorkspaceConfig options
+   */
   static class WorkspaceConfig {
     @CommandLine.Option(
-        description = "Path to workspace folder.",
-        names = {"-ws", "--workspace"})
+            description = "Path to workspace folder.",
+            names = {"-ws", "--workspace"})
     private Path workspace;
   }
 
-  /** explicit config options */
+  /**
+   * explicit config options
+   */
   static class ExplicitConfig {
     @CommandLine.Option(
-        names = {"-cf", "--copybook-folder"},
-        description = "Path to the copybook folder.")
+            names = {"-cf", "--copybook-folder"},
+            description = "Path to the copybook folder.")
     private File[] cpyPaths = {};
 
     @CommandLine.Option(
-        names = {"-ce", "--copybook-extension"},
-        description = "List of copybook paths.")
+            names = {"-ce", "--copybook-extension"},
+            description = "List of copybook paths.")
     private String[] cpyExt = {"", ".cpy"};
   }
 
-  /** options for analysis command */
+  /**
+   * options for analysis command
+   */
   static class Args {
     @CommandLine.ArgGroup(exclusive = false)
     ExplicitConfig explicitConfig;
@@ -113,11 +159,11 @@ public class CliAnalysis implements Callable<Integer> {
   private List<File> createCopybooksPaths() {
     if (args.workspaceConfig != null && parent.processorGroupsResolver != null) {
       return parent
-          .processorGroupsResolver
-          .resolveCopybooksPaths(src.toPath(), args.workspaceConfig.workspace)
-          .stream()
-          .map(Path::toFile)
-          .collect(Collectors.toList());
+              .processorGroupsResolver
+              .resolveCopybooksPaths(src.toPath(), args.workspaceConfig.workspace)
+              .stream()
+              .map(Path::toFile)
+              .collect(Collectors.toList());
     }
     return Arrays.asList(args.explicitConfig.cpyPaths);
   }
@@ -125,7 +171,7 @@ public class CliAnalysis implements Callable<Integer> {
   private List<String> createCopybooksExtensions() {
     if (args.workspaceConfig != null && parent.processorGroupsResolver != null) {
       return parent.processorGroupsResolver.resolveCopybooksExtensions(
-          src.toPath(), args.workspaceConfig.workspace);
+              src.toPath(), args.workspaceConfig.workspace);
     }
     return Arrays.asList(args.explicitConfig.cpyExt);
   }
