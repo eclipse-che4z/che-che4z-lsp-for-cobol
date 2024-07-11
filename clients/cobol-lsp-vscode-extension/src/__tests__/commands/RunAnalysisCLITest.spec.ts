eb2ae645521b7c39ab9f1908e312ffe1fefd472e
mm-broadcom <michael.messer@broadcom.com>
2024-07-11 12:31:34 -0400

    feat: Create command to run analysis vscode command via CLI/terminal (#2368)

---
+++ clients/cobol-lsp-vscode-extension/src/__tests__/commands/RunAnalysisCLITest.spec.ts
@@ -0,0 +1,281 @@
+/*
+ * Copyright (c) 2024 Broadcom.
+ * The term "Broadcom" refers to Broadcom Inc. and/or its subsidiaries.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Broadcom, Inc. - initial API and implementation
+ */
+
+import { RunAnalysis } from "../../commands/RunAnalysisCLI";
+import * as vscode from "vscode";
+
+jest.mock("vscode", () => ({
+  commands: {
+    registerCommand: jest
+      .fn()
+      .mockImplementation((command, callback) => callback()),
+    executeCommand: jest.fn(),
+  },
+  extensions: {
+    getExtension: jest.fn().mockReturnValue({ extensionPath: "/test" }),
+  },
+  FileType: {
+    Directory: 2,
+  },
+  languages: {
+    registerCodeActionsProvider: jest.fn(),
+    registerCompletionItemProvider: jest.fn(),
+  },
+  window: {
+    activeTextEditor: {
+      document: {
+        uri: {
+          path: "/storagePath",
+          fsPath: "/storagePath",
+          scheme: "file",
+        },
+        getText: jest.fn(),
+      },
+    },
+    setStatusBarMessage: jest
+      .fn()
+      .mockImplementation(
+        async (_text: string, _hideWhenDone: Thenable<any>) => {
+          return Promise.resolve(true);
+        },
+      ),
+    showErrorMessage: jest.fn().mockReturnValue("test"),
+    showInformationMessage: jest
+      .fn()
+      .mockImplementation((message: string) => Promise.resolve(message)),
+    showQuickPick: jest.fn(),
+    onDidChangeActiveTextEditor: jest.fn(),
+    createQuickPick: jest
+      .fn()
+      .mockReturnValue({ onDidChangeSelection: jest.fn(), show: jest.fn() }),
+    createOutputChannel: jest.fn().mockReturnValue({
+      appendLine: jest.fn(),
+    }),
+    terminals: {
+      find: jest.fn(),
+    },
+    createTerminal: jest.fn().mockReturnValue({
+      sendText: jest.fn(),
+      show: jest.fn(),
+    }),
+  },
+  workspace: {
+    getConfiguration: jest.fn().mockReturnValue({
+      get: jest.fn().mockReturnValue(9),
+    }),
+    getWorkspaceFolder: jest.fn().mockReturnValue({ name: "workspaceFolder1" }),
+    onDidChangeConfiguration: jest
+      .fn()
+      .mockReturnValue("onDidChangeConfiguration"),
+    workspaceFolders: [{ uri: { fsPath: "ws-path" } } as any],
+    fs: {
+      createDirectory: jest.fn(),
+      readDirectory: jest.fn().mockReturnValue([]),
+      stat: jest.fn().mockReturnValue(2),
+      writeFile: jest.fn(),
+    },
+  },
+  Uri: {
+    joinPath: jest.fn().mockImplementation((inputUri: vscode.Uri, path) => {
+      return {
+        path: inputUri.path + path,
+        fsPath: inputUri.fsPath + path,
+      };
+    }),
+  },
+}));
+
+const context: any = {
+  extensionUri: { fsPath: "/test" },
+  globalStorageUri: { fsPath: "/storagePath" },
+  subscriptions: [],
+};
+
+describe("Test Analysis CLI command functionality", () => {
+  test("Cobol Analysis - Java", async () => {
+    const testAnalysis = new RunAnalysis(
+      context.globalStorageUri,
+      context.extensionUri,
+    );
+    vscode.window.showQuickPick = jest.fn().mockReturnValue("Java");
+
+    const runCobolAnalysisCommandSpy = jest.spyOn(
+      testAnalysis as any,
+      "runCobolAnalysisCommand",
+    );
+    const getCurrentFileLocationSpy = jest.spyOn(
+      testAnalysis as any,
+      "getCurrentFileLocation",
+    );
+    const buildJavaCommandSpy = jest.spyOn(
+      testAnalysis as any,
+      "buildJavaCommand",
+    );
+    const buildNativeCommandSpy = jest.spyOn(
+      testAnalysis as any,
+      "buildNativeCommand",
+    );
+    const buildAnalysisCommandPortionSpy = jest.spyOn(
+      testAnalysis as any,
+      "buildAnalysisCommandPortion",
+    );
+    const sendToTerminalSpy = jest.spyOn(testAnalysis as any, "sendToTerminal");
+
+    await testAnalysis.runCobolAnalysisCommand();
+
+    expect(runCobolAnalysisCommandSpy).toHaveBeenCalled();
+    expect(getCurrentFileLocationSpy).toHaveBeenCalled();
+    expect(buildJavaCommandSpy).toHaveBeenCalledWith("/storagePath");
+    expect(buildJavaCommandSpy).toHaveReturnedWith(
+      'java -jar "/test/server/jar/server.jar" analysis -s "/storagePath" -cf=.',
+    );
+    expect(buildAnalysisCommandPortionSpy).toHaveReturnedWith(
+      'analysis -s "/storagePath" -cf=.',
+    );
+    expect(sendToTerminalSpy).toHaveBeenCalled();
+
+    expect(buildNativeCommandSpy).toHaveBeenCalledTimes(0);
+  });
+
+  test("Cobol Analysis - Native", async () => {
+    const testAnalysis = new RunAnalysis(
+      context.globalStorageUri,
+      context.extensionUri,
+    );
+    vscode.window.showQuickPick = jest.fn().mockReturnValue("Native");
+
+    const runCobolAnalysisCommandSpy = jest.spyOn(
+      testAnalysis as any,
+      "runCobolAnalysisCommand",
+    );
+    const getCurrentFileLocationSpy = jest.spyOn(
+      testAnalysis as any,
+      "getCurrentFileLocation",
+    );
+    const buildJavaCommandSpy = jest.spyOn(
+      testAnalysis as any,
+      "buildJavaCommand",
+    );
+    const buildNativeCommandSpy = jest.spyOn(
+      testAnalysis as any,
+      "buildNativeCommand",
+    );
+    const buildAnalysisCommandPortionSpy = jest.spyOn(
+      testAnalysis as any,
+      "buildAnalysisCommandPortion",
+    );
+    const sendToTerminalSpy = jest.spyOn(testAnalysis as any, "sendToTerminal");
+
+    await testAnalysis.runCobolAnalysisCommand();
+
+    expect(runCobolAnalysisCommandSpy).toHaveBeenCalled();
+    expect(getCurrentFileLocationSpy).toHaveBeenCalled();
+    expect(buildNativeCommandSpy).toHaveBeenCalledWith(
+      "/storagePath",
+      process.platform,
+    );
+    expect(buildAnalysisCommandPortionSpy).toHaveReturnedWith(
+      'analysis -s "/storagePath" -cf=.',
+    );
+    expect(sendToTerminalSpy).toHaveBeenCalled();
+
+    expect(buildJavaCommandSpy).toHaveBeenCalledTimes(0);
+  });
+
+  test("Cobol Analysis - Undefined Type", async () => {
+    const testAnalysis = new RunAnalysis(
+      context.globalStorageUri,
+      context.extensionUri,
+    );
+    vscode.window.showQuickPick = jest.fn().mockReturnValue(undefined);
+
+    const runCobolAnalysisCommandSpy = jest.spyOn(
+      testAnalysis as any,
+      "runCobolAnalysisCommand",
+    );
+    const getVersionToRunSpy = jest.spyOn(
+      testAnalysis as any,
+      "getVersionToRun",
+    );
+    const buildJavaCommandSpy = jest.spyOn(
+      testAnalysis as any,
+      "buildJavaCommand",
+    );
+    const buildNativeCommandSpy = jest.spyOn(
+      testAnalysis as any,
+      "buildNativeCommand",
+    );
+
+    await testAnalysis.runCobolAnalysisCommand();
+
+    expect(runCobolAnalysisCommandSpy).toHaveBeenCalled();
+    expect(getVersionToRunSpy).toHaveBeenCalled();
+    expect(buildJavaCommandSpy).not.toHaveBeenCalled();
+    expect(buildNativeCommandSpy).not.toHaveBeenCalled();
+  });
+
+  test("Cobol Analysis - Save temp file", async () => {
+    const testAnalysis = new RunAnalysis(
+      context.globalStorageUri,
+      context.extensionUri,
+    );
+    vscode.window.showQuickPick = jest.fn().mockReturnValue("Java");
+    if (vscode.window.activeTextEditor) {
+      vscode.window.activeTextEditor.document.getText = jest
+        .fn()
+        .mockReturnValue("Test data");
+      jest.replaceProperty(
+        vscode.window.activeTextEditor.document.uri,
+        "scheme",
+        "untitled",
+      );
+    }
+
+    const saveTempFileSpy = jest.spyOn(testAnalysis as any, "saveTempFile");
+
+    await testAnalysis.runCobolAnalysisCommand();
+
+    expect(saveTempFileSpy).toHaveBeenCalled();
+  });
+
+  test("Cobol - Java - No file location", () => {
+    const testAnalysis = new RunAnalysis(
+      context.globalStorageUri,
+      context.extensionUri,
+    );
+
+    const result = (testAnalysis as any).buildJavaCommand("");
+
+    expect(result).toBe("");
+  });
+
+  test("Cobol - getServerPath", () => {
+    const testAnalysis = new RunAnalysis(
+      context.globalStorageUri,
+      context.extensionUri,
+    );
+
+    let result = (testAnalysis as any).getServerPath("initialPath", "win32");
+    expect(result).toBe("initialPath");
+
+    result = (testAnalysis as any).getServerPath("initialPath", "linux");
+    expect(result).toBe("initialPath/server-linux");
+
+    result = (testAnalysis as any).getServerPath("initialPath", "darwin");
+    expect(result).toBe("initialPath/server-mac");
+
+    result = (testAnalysis as any).getServerPath("initialPath", "other");
+    expect(result).toBe("");
+  });
+});
---
+++ clients/cobol-lsp-vscode-extension/src/commands/RunAnalysisCLI.ts
@@ -0,0 +1,237 @@
+/*
+ * Copyright (c) 2024 Broadcom.
+ * The term "Broadcom" refers to Broadcom Inc. and/or its subsidiaries.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Broadcom, Inc. - initial API and implementation
+ */
+
+import * as vscode from "vscode";
+import { Terminal } from "vscode";
+
+export interface AnalysisResults {
+  typeToRun: string;
+  copybookLocation: string;
+}
+
+/**
+ * Class containing functions used to create the command needed to run the analysis from the command line
+ * Handles Java or Native builds with or without copybook support
+ */
+export class RunAnalysis {
+  protected runNative: boolean;
+  protected copybookConfigLocation: string;
+  protected globalStorageUri: vscode.Uri;
+  protected extensionUri: vscode.Uri;
+
+  constructor(globalStorageUri: vscode.Uri, extensionPath: vscode.Uri) {
+    this.runNative = false;
+    this.copybookConfigLocation = "";
+    this.globalStorageUri = globalStorageUri;
+    this.extensionUri = extensionPath;
+  }
+
+  /**
+   * Starts the process to gather input from the user to create the COBOL CLI analysis command.
+   */
+  public async runCobolAnalysisCommand() {
+    const activeEditor = vscode.window.activeTextEditor;
+    if (!activeEditor) {
+      return;
+    }
+
+    const result = {} as Partial<AnalysisResults>;
+    await this.getVersionToRun(result);
+    await this.getCopybookConfigLocation(result);
+
+    if (
+      result.typeToRun === undefined ||
+      result.copybookLocation === undefined
+    ) {
+      return;
+    }
+
+    this.runNative = result.typeToRun === "Native";
+    this.copybookConfigLocation = result.copybookLocation;
+
+    const command = await this.buildCommand();
+    if (command !== "") {
+      this.sendToTerminal(command);
+    }
+  }
+
+  /**
+   *  Prompt the user for whether to run the Java or Native version.
+   */
+  public async getVersionToRun(result: Partial<AnalysisResults>) {
+    result.typeToRun = await vscode.window.showQuickPick(["Java", "Native"], {
+      placeHolder: "Select Java or Native",
+    });
+  }
+
+  /**
+   * Prompt the user for the location of the copybook config file.
+   */
+  public async getCopybookConfigLocation(result: Partial<AnalysisResults>) {
+    result.copybookLocation = "";
+  }
+
+  /**
+   * Encapsulates the handling of building the command
+   * @protected
+   */
+  protected async buildCommand() {
+    const currentFileLocation = await this.getCurrentFileLocation();
+    if (!currentFileLocation || currentFileLocation === "") {
+      return "";
+    }
+
+    if (this.runNative) {
+      return this.buildNativeCommand(currentFileLocation, process.platform);
+    }
+
+    return this.buildJavaCommand(currentFileLocation);
+  }
+
+  /**
+   * Creates the command to run using the native build.
+   * @param currentFileLocation - Location of the main cobol file being analyzed.
+   * @param platform - Result from Node.js' "process.platform".
+   * @protected
+   */
+  protected buildNativeCommand(currentFileLocation: string, platform: string) {
+    const serverPath = this.extensionUri.fsPath + "/server/native";
+    const result = this.getServerPath(serverPath, platform);
+
+    if (result === "") {
+      return "";
+    }
+
+    return result + " " + this.buildAnalysisCommandPortion(currentFileLocation);
+  }
+
+  protected getServerPath(serverPath: string, platform: string) {
+    switch (platform) {
+      case "win32":
+        return serverPath;
+      case "linux":
+        return (serverPath += "/server-linux");
+      case "darwin": // macOS
+        return (serverPath += "/server-mac");
+      default: // Only Windows, Linux and Mac are supported.
+        return "";
+    }
+  }
+
+  /**
+   * Creates the command to run using the java build.
+   * @param currentFileLocation - Location of the main cobol file being analyzed.
+   * @protected
+   */
+  protected buildJavaCommand(currentFileLocation: string) {
+    const extensionFolder: string | undefined =
+      this.extensionUri.fsPath + "/server/jar/server.jar";
+
+    if (extensionFolder && currentFileLocation !== "") {
+      return (
+        'java -jar "' +
+        extensionFolder +
+        '" ' +
+        this.buildAnalysisCommandPortion(currentFileLocation)
+      );
+    }
+
+    return "";
+  }
+
+  /**
+   * Provides the portion of the command from "analysis" onwards.
+   * Is the same for both the Java and Native builds.
+   * @param currentFileLocation - Location of the main cobol file being analyzed.
+   * @protected
+   */
+  protected buildAnalysisCommandPortion(currentFileLocation: string) {
+    const copyBookCommand = `-cf=${
+      this.copybookConfigLocation === ""
+        ? "."
+        : '"' + this.copybookConfigLocation + '"'
+    }`;
+
+    return 'analysis -s "' + currentFileLocation + '" ' + copyBookCommand;
+  }
+
+  /**
+   * Sends a given command to a terminal.
+   * Checks to see if one named "Analysis" is already created, if so clear and reuse it.
+   * @param command - The command to run from the terminal.
+   * @protected
+   */
+  protected sendToTerminal(command: string) {
+    const existingTerminal = vscode.window.terminals.find(
+      (term: Terminal) => term.name === "Analysis",
+    );
+    const terminal = existingTerminal
+      ? existingTerminal
+      : vscode.window.createTerminal("Analysis");
+
+    terminal.sendText(command);
+    terminal.show(true);
+  }
+
+  /**
+   * Retrieves the location of the currently open file.
+   * If it is not saved, creates a temporary one on the disk to send to the command line with a copy of the contents.
+   *    - Does not result in the actual file being saved to the end user.
+   * @protected
+   */
+  protected async getCurrentFileLocation() {
+    if (vscode.window.activeTextEditor) {
+      if (vscode.window.activeTextEditor.document.uri.scheme !== "file") {
+        return this.saveTempFile();
+      } else {
+        return vscode.window.activeTextEditor.document.uri.fsPath;
+      }
+    }
+    return "";
+  }
+
+  /**
+   * Creates a temporary one on the disk to send to the command line with a copy of the contents.
+   *    - Does not result in the actual file being saved to the end user.
+   * @protected
+   */
+  protected async saveTempFile() {
+    const data = vscode.window.activeTextEditor?.document.getText();
+
+    if (data) {
+      const tempFolderUri = vscode.Uri.joinPath(
+        this.globalStorageUri,
+        "tempAnalysisFiles",
+      );
+      await vscode.workspace.fs.createDirectory(tempFolderUri);
+      const tempFolderResult = await vscode.workspace.fs.readDirectory(
+        tempFolderUri,
+      );
+      await Promise.all(
+        tempFolderResult.map(async (value) => {
+          await vscode.workspace.fs.delete(
+            vscode.Uri.joinPath(tempFolderUri, value[0]),
+          );
+        }),
+      );
+
+      const newFileName = Date.now() + ".cbl";
+      const newUri = vscode.Uri.joinPath(tempFolderUri, newFileName);
+      await vscode.workspace.fs.writeFile(newUri, Buffer.from(data));
+      return newUri.fsPath;
+    }
+
+    return "";
+  }
+}
