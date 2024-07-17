e83e997d79a0c142a21a59cf5ecd3d9acf72b604
mm-broadcom <michael.messer@broadcom.com>
2024-07-17 08:17:46 -0400

    feat: Cli analysis improvements (#2379)
    * Add IOException handling for Processors
    
    Added IOException handling for Processors whether it is missing or had issues with reading.
    
    * Allow Analysis without copybook folder
    
    Allow the user to run analysis without the copybook folder/extensions or the workspace folder being provided.
    
    * Add ability to hide diagnostics
    
    Add ability/flag to hide diagnostics and only print the URI, dialect/language, timings and garbage-collection/memory data.
    
    * Revise labels to be clearer
    
    * Print Extended Source to terminal if flag is present
    
    Print Extended Source to terminal if flag is present
    
    * Add ability to save extended source
    
    add ability to save extended source to a specified file/location.
    
    * Read file from stdin
    
    * Add choice for diagnostics to VSCode command
    
    Add choice to show/hide diagnostics to the VSCode command.
    
    * Refactored code as requested
    
    Refactored some of the code, added named constants for the return codes and cleaned up some exception handling code as requested on the PR.
    
    * Fixed missed constants

--- server/engine/src/main/java/org/eclipse/lsp/cobol/cli/command/CliAnalysis.java
+++ server/engine/src/main/java/org/eclipse/lsp/cobol/cli/command/CliAnalysis.java
@@ -18,16 +18,25 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.lang.management.GarbageCollectorMXBean;
 import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryPoolMXBean;
 import java.lang.management.MemoryType;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.stream.Collectors;
+
+import lombok.extern.slf4j.Slf4j;
+
 import org.eclipse.lsp.cobol.cli.di.CliModule;
 import org.eclipse.lsp.cobol.cli.modules.CliClientProvider;
 import org.eclipse.lsp.cobol.common.dialects.CobolLanguageId;
@@ -37,18 +46,16 @@ import picocli.CommandLine;
  * Analysis cli command
  */
 @CommandLine.Command(name = "analysis", description = "analyse cobol source")
+@Slf4j
 public class CliAnalysis implements Callable<Integer> {
     @CommandLine.ParentCommand
     private Cli parent;
 
-    @CommandLine.Option(
-            names = {"-s", "--source"},
-            required = true,
-            description = "The COBOL program file.")
-    private File src;
-
     @CommandLine.ArgGroup(multiplicity = "1")
-    private Args args;
+    private InputConfig inputConfig = new InputConfig();
+
+    @CommandLine.ArgGroup()
+    private Args args = new Args();
 
     @CommandLine.Option(
             description = "Supported dialect values: ${COMPLETION-CANDIDATES}",
@@ -56,10 +63,29 @@ public class CliAnalysis implements Callable<Integer> {
             defaultValue = "COBOL")
     private CobolLanguageId dialect;
 
+    @CommandLine.Option(
+            description = "Hide diagnostics",
+            names = {"-nd", "--no-diag", "--no-diagnostic", "--no-diagnostics"}
+    )
+    private boolean hideDiagnostics;
+
+    @CommandLine.ArgGroup(exclusive = false)
+    private ExtendedSourceConfig extendedSourceConfig = new ExtendedSourceConfig();
+
+
     @Override
     public Integer call() throws Exception {
-        if (args.workspaceConfig != null)
-            parent.initProcessorGroupsReader(args.workspaceConfig.workspace);
+        if (args.workspaceConfig != null) {
+            try {
+                parent.initProcessorGroupsReader(args.workspaceConfig.workspace);
+            } catch (Exception e) {
+                return Cli.FAILURE;
+            }
+        }
+
+        if (inputConfig.useStdin) {
+            createTemporaryFileFromInput();
+        }
 
         Injector diCtx = Guice.createInjector(new CliModule());
         CliClientProvider cliClientProvider = diCtx.getInstance(CliClientProvider.class);
@@ -67,31 +93,39 @@ public class CliAnalysis implements Callable<Integer> {
         cliClientProvider.setCpyPaths(createCopybooksPaths());
         cliClientProvider.setCpyExt(createCopybooksExtensions());
         JsonObject result = new JsonObject();
-        result.addProperty("uri", src.toURI().toString());
+        result.addProperty("uri", (inputConfig.useStdin) ? "N/A (User Input)" : inputConfig.src.toURI().toString());
         result.addProperty("language", dialect.getId());
         try {
-            Cli.Result analysisResult = parent.runAnalysis(src, dialect, diCtx, true);
+            Cli.Result analysisResult = parent.runAnalysis(inputConfig.src, dialect, diCtx, true);
             parent.addTiming(result, analysisResult.ctx.getBenchmarkSession());
-            JsonArray diagnostics = new JsonArray();
-            analysisResult
-                    .ctx
-                    .getAccumulatedErrors()
-                    .forEach(
-                            err -> {
-                                JsonObject diagnostic = CliUtils.diagnosticToJson(err);
-                                diagnostics.add(diagnostic);
-                            });
-            result.add("diagnostics", diagnostics);
-            result.addProperty("lines", String.valueOf(analysisResult.ctx.getExtendedDocument().toString().split("\n").length));
-            result.addProperty("size", String.valueOf(analysisResult.ctx.getExtendedDocument().toString().length()));
+            if (!hideDiagnostics) {
+                generateDiagnostics(analysisResult, result);
+            }
             collectGcAndMemoryStats(result);
             System.out.println(CliUtils.GSON.toJson(result));
-            return 0;
+
+            handleExtendedSource(analysisResult);
+            return Cli.SUCCESS;
         } catch (Exception e) {
             result.addProperty("crash", e.getMessage() != null && e.getMessage().isEmpty() ? "error" : e.getMessage());
             System.out.println(CliUtils.GSON.toJson(result));
-            return 1;
+            return Cli.FAILURE;
         }
+    }
+
+    private void generateDiagnostics(Cli.Result analysisResult, JsonObject result) {
+        JsonArray diagnostics = new JsonArray();
+        analysisResult
+                .ctx
+                .getAccumulatedErrors()
+                .forEach(
+                        err -> {
+                            JsonObject diagnostic = CliUtils.diagnosticToJson(err);
+                            diagnostics.add(diagnostic);
+                        });
+        result.add("diagnostics", diagnostics);
+        result.addProperty("lines", String.valueOf(analysisResult.ctx.getExtendedDocument().toString().split("\n").length));
+        result.addProperty("size", String.valueOf(analysisResult.ctx.getExtendedDocument().toString().length()));
     }
 
     private void collectGcAndMemoryStats(JsonObject result) {
@@ -108,9 +142,9 @@ public class CliAnalysis implements Callable<Integer> {
                 garbageCollectionTime += time;
             }
         }
-        result.addProperty("gc.count", totalGarbageCollections);
+        result.addProperty("Garbage Collection Count", totalGarbageCollections);
         // milliseconds to seconds
-        result.addProperty("gc.time", garbageCollectionTime * 0.001);
+        result.addProperty("Garbage Collection Time", garbageCollectionTime * 0.001);
 
         List<MemoryPoolMXBean> pools = ManagementFactory.getMemoryPoolMXBeans();
         long total = 0;
@@ -120,7 +154,22 @@ public class CliAnalysis implements Callable<Integer> {
                 total = total + peakUsed;
             }
         }
-        result.addProperty("memory.heap_peak", total / 1024 / 1024);
+        result.addProperty("Peak Heap Memory", total / 1024 / 1024);
+    }
+
+    /**
+     * input config options
+     */
+    static class InputConfig {
+        @CommandLine.Option(
+                names = {"-s", "--source"},
+                description = "The COBOL program file.")
+        private File src;
+
+        @CommandLine.Option(
+                names = {"-si", "-sf", "--source-input", "--stdin"},
+                description = "Prompt to read input from stdin.")
+        private boolean useStdin;
     }
 
     /**
@@ -149,21 +198,62 @@ public class CliAnalysis implements Callable<Integer> {
     }
 
     /**
+     * extended source options
+     */
+    static class ExtendedSourceConfig {
+        @CommandLine.Option(
+                description = "Print extended source to terminal",
+                names = {"-es", "-ex", "--extended-source"}
+        )
+        private boolean printExtendedSource;
+
+        @CommandLine.Option(
+                description = "Path where to save extended output",
+                names = {"-esf", "-exf", "--save-extended-source"}
+        )
+        private File extendedSourcePath;
+    }
+
+    /**
      * options for analysis command
      */
     static class Args {
         @CommandLine.ArgGroup(exclusive = false)
-        ExplicitConfig explicitConfig;
+        ExplicitConfig explicitConfig = new ExplicitConfig();
 
         @CommandLine.ArgGroup(exclusive = false)
         WorkspaceConfig workspaceConfig;
+    }
+
+    private void handleExtendedSource(Cli.Result analysisResult) {
+        String formattedExtendedDoc = "       " + analysisResult.ctx.getExtendedDocument().toString().trim(); // Trim removes the necessary spaces prior to the first line of COBOL code thus it must be put back in.
+        if (extendedSourceConfig.printExtendedSource) {
+            System.out.println("\n------------ Extended Source Begins Below -----------");
+            System.out.println(formattedExtendedDoc);
+        }
+
+        if (extendedSourceConfig.extendedSourcePath != null) {
+            Path outputPath = extendedSourceConfig.extendedSourcePath.toPath();
+            try {
+                if (outputPath.toString().toLowerCase().contains(".cbl")) {
+                    Files.createDirectories(outputPath.getParent());
+                } else {
+                    Files.createDirectories(outputPath);
+                    outputPath = outputPath.resolve("extended-source.cbl");
+                }
+
+                Files.write(outputPath, formattedExtendedDoc.getBytes(StandardCharsets.UTF_8));
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     private List<File> createCopybooksPaths() {
         if (args.workspaceConfig != null && parent.processorGroupsResolver != null) {
             return parent
                     .processorGroupsResolver
-                    .resolveCopybooksPaths(src.toPath(), args.workspaceConfig.workspace)
+                    .resolveCopybooksPaths(inputConfig.src.toPath(), args.workspaceConfig.workspace)
                     .stream()
                     .map(Path::toFile)
                     .collect(Collectors.toList());
@@ -174,8 +264,24 @@ public class CliAnalysis implements Callable<Integer> {
     private List<String> createCopybooksExtensions() {
         if (args.workspaceConfig != null && parent.processorGroupsResolver != null) {
             return parent.processorGroupsResolver.resolveCopybooksExtensions(
-                    src.toPath(), args.workspaceConfig.workspace);
+                    inputConfig.src.toPath(), args.workspaceConfig.workspace);
         }
         return Arrays.asList(args.explicitConfig.cpyExt);
+    }
+
+    private void createTemporaryFileFromInput() {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            inputConfig.src = File.createTempFile("cobol-analysis-temp", ".cbl");
+            StringBuilder input = new StringBuilder();
+            String temp;
+            while ((temp = bufferedReader.readLine()) != null) {
+                input.append(temp).append(System.lineSeparator());
+            }
+            Files.write(inputConfig.src.toPath(), input.toString().getBytes(StandardCharsets.UTF_8));
+            inputConfig.src.deleteOnExit();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
