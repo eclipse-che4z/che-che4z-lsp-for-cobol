85e84c088c1f53d9bd7a12ba1f258556a7a1eac6
Michael Messer <michael.messer@broadcom.com>
2024-08-13 13:42:52 -0400

    More tests

--- server/engine/src/test/java/org/eclipse/lsp/cobol/usecases/TestCicsConverseStatement.java
+++ server/engine/src/test/java/org/eclipse/lsp/cobol/usecases/TestCicsConverseStatement.java
@@ -44,14 +44,33 @@ public class TestCicsConverseStatement {
                     + "       01 {$*maxLen} PIC S9 VALUE +10.\n"
                     + "       01 {$*toLen} PIC S9 VALUE +10.\n"
                     + "       01 {$*intoVar} PIC S9 VALUE +1000.\n"
+                    + "       01 {$*convidVar} PIC S9 VALUE +1000.\n"
+                    + "       01 {$*stateVar} PIC S9 VALUE +1000.\n"
+                    + "       01 {$*attachVar} PIC S9 VALUE +1000.\n"
+                    + "       01 {$*ctlCharVar} PIC S9 VALUE +1000.\n"
                     + "       PROCEDURE DIVISION.\n"
                     + "            EXEC CICS \n"
                     + "            END-EXEC.";
 
-    // Test Strings
-    private static final String DEFAULT_VALID = "CONVERSE FROM({$fromVar}) FROMLENGTH({$fromLen}) INTO({$intoVar}) TOLENGTH({$toLen}) MAXLENGTH({$maxLen})";
-    private static final String DEFAULT_VALID_NT = "CONVERSE FROM({$fromVar}) FROMLENGTH({$fromLen}) INTO({$intoVar}) TOLENGTH({$toLen}) MAXLENGTH({$maxLen}) NOTRUNCATE";
+    private static final String CONVERSE = "CONVERSE ";
+    private static final String FROM = "FROM({$fromVar}) FROMLENGTH({$fromLen}) ";
+    private static final String FROM_INTO = FROM + "INTO({$intoVar}) ";
+    private static final String FROM_INTO_TO = FROM_INTO + "TOLENGTH({$toLen}) ";
 
+    // Test Strings
+    private static final String DEFAULT_VALID = CONVERSE + FROM_INTO_TO + "MAXLENGTH({$maxLen})";
+    private static final String DEFAULT_VALID_NT = CONVERSE + FROM_INTO_TO + "MAXLENGTH({$maxLen}) NOTRUNCATE";
+
+    private static final String APPC_VALID = CONVERSE + "CONVID({$convidVar}) " + FROM_INTO_TO + "MAXLENGTH({$maxLen}) STATE({$stateVar})";
+
+    private static final String LU23_3270_VALID1 = CONVERSE + FROM_INTO + "ERASE DEFAULT TOLENGTH({$toLen}) MAXLENGTH({$maxLen}) DEFRESP NOTRUNCATE ASIS";
+    private static final String LU23_3270_VALID2 = CONVERSE + FROM_INTO + "ERASE DEFAULT CTLCHAR({$ctlCharVar}) TOLENGTH({$toLen}) MAXLENGTH({$maxLen}) DEFRESP NOTRUNCATE ASIS";
+    private static final String LU23_3270_VALID3 = CONVERSE + FROM_INTO + "ERASE ALTERNATE CTLCHAR({$ctlCharVar}) TOLENGTH({$toLen}) MAXLENGTH({$maxLen})";
+    private static final String LU23_3270_VALID4 = CONVERSE + FROM_INTO + "ERASE TOLENGTH({$toLen}) MAXLENGTH({$maxLen})";
+    private static final String LU23_3270_VALID5 = CONVERSE + FROM_INTO + "CTLCHAR({$ctlCharVar}) TOLENGTH({$toLen}) MAXLENGTH({$maxLen})";
+    private static final String LU23_3270_VALID6 = CONVERSE + FROM_INTO + "STRFIELD TOLENGTH({$toLen}) MAXLENGTH({$maxLen})";
+    
+    private static final String LU61_VALID = CONVERSE + FROM + "SESSION({$convidVar}) ATTACHID({$attachVar}) INTO({$intoVar}) TOLENGTH({$toLen}) MAXLENGTH({$maxLen}) NOTRUNCATE DEFRESP";
 
     // Utility Functions
     private static void noErrorTest(String newCommand) {
@@ -75,5 +94,25 @@ public class TestCicsConverseStatement {
     @Test
     void testDefaultValidNT() {
         noErrorTest(DEFAULT_VALID_NT);
+    }
+
+    @Test
+    void testAPPCValid() {
+        noErrorTest(APPC_VALID);
+    }
+
+    @Test
+    void testLU61Valid() {
+        noErrorTest(LU61_VALID);
+    }
+
+    @Test
+    void testLU23Valid() {
+        noErrorTest(LU23_3270_VALID1);
+        noErrorTest(LU23_3270_VALID2);
+        noErrorTest(LU23_3270_VALID3);
+        noErrorTest(LU23_3270_VALID4);
+        noErrorTest(LU23_3270_VALID5);
+        noErrorTest(LU23_3270_VALID6);
     }
 }
