17dd1377d938505a267a54d9c1c669589d70d228
ap891843 <aman.prashant@broadcom.com>
2024-08-16 14:42:43 +0200

    fix: adjust copy node at right location in the parent's child list structure
    Signed-off-by: ap891843 <aman.prashant@broadcom.com>
    
    test: add test as  suggested in PR#2439
    
    Signed-off-by: ap891843 <aman.prashant@broadcom.com>

--- server/engine/src/main/java/org/eclipse/lsp/cobol/core/engine/processors/RootNodeUpdateCopyNodesByPositionInTree.java
+++ server/engine/src/main/java/org/eclipse/lsp/cobol/core/engine/processors/RootNodeUpdateCopyNodesByPositionInTree.java
@@ -22,6 +22,7 @@ import org.eclipse.lsp.cobol.common.processor.Processor;
 import org.eclipse.lsp.cobol.common.model.tree.RootNode;
 import org.eclipse.lsp.cobol.common.utils.RangeUtils;
 import org.eclipse.lsp4j.Location;
+import org.eclipse.lsp4j.Range;
 
 import java.util.List;
 
@@ -39,11 +40,13 @@ public class RootNodeUpdateCopyNodesByPositionInTree implements Processor<RootNo
         node.getChildren().stream().filter(Node.hasType(NodeType.COPY)).collect(toList());
     nodes.forEach(node::removeChild);
     nodes.forEach(
-        it ->
-            RangeUtils.findNodeByPosition(
-                    node, it.getLocality().getUri(), it.getLocality().getRange().getStart())
-                .orElse(node)
-                .addChild(it));
+        it -> {
+          Node parentNode =
+              RangeUtils.findNodeByPosition(
+                      node, it.getLocality().getUri(), it.getLocality().getRange().getStart())
+                  .orElse(node);
+            parentNode.addChildAt(getNodeInsertionIndex(parentNode.getChildren(), it), it);
+        });
 
     List<CopyNode> copyNodes = node.getDepthFirstStream()
         .filter(Node.hasType(NodeType.COPY))
@@ -51,6 +54,32 @@ public class RootNodeUpdateCopyNodesByPositionInTree implements Processor<RootNo
         .collect(toList());
 
     copyNodes.forEach(c -> registerCopyUsage(copyNodes, c.getNameLocation(), c.getUri()));
+  }
+
+  private int getNodeInsertionIndex(List<Node> nodes, Node nodeToInsert) {
+    int nodeSize = nodes.size();
+    String nodeToInsertUri = nodeToInsert.getLocality().getUri();
+    Range nodeToInsertRange = nodeToInsert.getLocality().getRange();
+    int nodeToInsertLine = nodeToInsertRange.getStart().getLine();
+    int nodeToInsertCharacter = nodeToInsertRange.getStart().getCharacter();
+
+    for (int index = 0; index < nodeSize; index++) {
+      Node currentNode = nodes.get(index);
+      if (currentNode.getLocality().getUri().equals(nodeToInsertUri)) {
+        Range currentNodeRange = currentNode.getLocality().getRange();
+        int currentNodeLine = currentNodeRange.getStart().getLine();
+        int currentNodeCharacter = currentNodeRange.getStart().getCharacter();
+
+        if (currentNodeLine == nodeToInsertLine) {
+          if (currentNodeCharacter > nodeToInsertCharacter) {
+            return index;
+          }
+        } else if (currentNodeLine > nodeToInsertLine) {
+          return index;
+        }
+      }
+    }
+    return nodeSize;
   }
 
   private void registerCopyUsage(List<CopyNode> node, Location nameLocation, String uri) {
