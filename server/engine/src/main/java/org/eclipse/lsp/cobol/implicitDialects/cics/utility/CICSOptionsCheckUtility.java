be5fe594df272aac105a8b6fe31b155cbe3bacd1
Michael Messer <michael.messer@broadcom.com>
2024-09-05 13:10:50 -0400

    Add CICSConverse to optionsMap
    Added the converse class to the options map such that the rules are processed.

--- server/engine/src/main/java/org/eclipse/lsp/cobol/implicitDialects/cics/utility/CICSOptionsCheckUtility.java
+++ server/engine/src/main/java/org/eclipse/lsp/cobol/implicitDialects/cics/utility/CICSOptionsCheckUtility.java
@@ -33,6 +33,9 @@ public class CICSOptionsCheckUtility {
     optionsMap.put(
         CICSAllocateOptionsCheckUtility.RULE_INDEX,
         new CICSAllocateOptionsCheckUtility(context, errors));
+    optionsMap.put(
+        CICSConverseOptionsCheckUtility.RULE_INDEX,
+        new CICSConverseOptionsCheckUtility(context, errors));
   }
 
   /**
