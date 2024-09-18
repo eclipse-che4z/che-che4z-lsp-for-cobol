041be387f94f0f708b7c6c9020d8fb39e0e84588
Kutlu <kutlu.ozel@broadcom.com>
2024-09-18 14:54:15 +0200

    add duplication check

--- server/engine/src/main/java/org/eclipse/lsp/cobol/implicitDialects/cics/utility/CICSOptionsCheckUtility.java
+++ server/engine/src/main/java/org/eclipse/lsp/cobol/implicitDialects/cics/utility/CICSOptionsCheckUtility.java
@@ -33,6 +33,9 @@ public class CICSOptionsCheckUtility {
     optionsMap.put(
         CICSAllocateOptionsCheckUtility.RULE_INDEX,
         new CICSAllocateOptionsCheckUtility(context, errors));
+    optionsMap.put(
+        CICSAsktimeOptionsCheckUtility.RULE_INDEX,
+        new CICSAsktimeOptionsCheckUtility(context, errors));
   }
 
   /**
