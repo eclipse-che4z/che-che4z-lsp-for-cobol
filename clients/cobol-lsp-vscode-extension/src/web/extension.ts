4d493e346276bd234b45a0cc641e65d970b6c35e
Jan Hybl <jan.hybl@broadcom.com>
2024-10-07 13:10:22 +0200

    fix: code review fixes
    fix: formatting

--- clients/cobol-lsp-vscode-extension/src/web/extension.ts
+++ clients/cobol-lsp-vscode-extension/src/web/extension.ts
@@ -1,6 +1,9 @@
 // This method is called when your extension is activated
+
+import { ExtensionContext } from "vscode";
+
 // Your extension is activated the very first time the command is executed
-export function activate() {}
+export function activate(_context: ExtensionContext) {}
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
