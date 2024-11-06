cd6136c3bca9d4165542b937e50fc757b9da4ead
Jan Hybl <jan.hybl@broadcom.com>
2024-11-06 09:04:22 +0100

    Improved typing and configuration parsing

--- clients/cobol-lsp-vscode-extension/src/services/SmartTabSettings.ts
+++ clients/cobol-lsp-vscode-extension/src/services/SmartTabSettings.ts
@@ -1,8 +1,9 @@
 import * as vscode from "vscode";
 import { SETTINGS_TAB_CONFIG } from "../constants";
+import * as t from "io-ts";
+import { decodeUnknown, DecodingError } from "./util/decoder";
 
 export class TabRule {
-  // tslint:disable-next-line:no-unnecessary-initializer
   public constructor(
     public stops: number[],
     public maxPosition: number,
@@ -14,39 +15,71 @@ export class TabSettings {
   public constructor(public rules: TabRule[], public defaultRule: TabRule) {}
 }
 
+const TabSettingsBoolean = t.boolean;
+const TabSettingsNumbers = t.array(t.number);
+const TabSettingsObject = t.partial({
+  default: TabSettingsNumbers,
+  anchors: t.record(t.string, TabSettingsNumbers),
+});
+
+const TabSettingsType = t.union([
+  TabSettingsBoolean,
+  TabSettingsNumbers,
+  TabSettingsObject,
+]);
+
+/**
+ * Parses configuration input into one of valid configuration types.
+ * `Undefined` is returned if the incorrect input is used.
+ */
+function parseConfiguration(input: unknown) {
+  try {
+    return decodeUnknown(TabSettingsType, input);
+  } catch (err) {
+    if (err instanceof DecodingError) {
+      return void 0;
+    }
+    throw err;
+  }
+}
+
+const DEFAULT_RULE = new TabRule([0, 6, 7, 11], 72);
+
 /**
  * Retrieves and parse tab settings configuration that can be boolean, array or an object
  * @returns a TabSettings object
  */
 export function getTabSettings(): TabSettings {
-  const config = vscode.workspace.getConfiguration().get(SETTINGS_TAB_CONFIG);
-  let settings = new TabSettings([], new TabRule([0, 6, 7, 11], 72));
-  if (Array.isArray(config)) {
-    const stops = config as number[];
-    if (stops !== undefined && stops.length > 0) {
-      const tabRule = new TabRule(stops, stops[stops.length - 1]);
+  const input = vscode.workspace.getConfiguration().get(SETTINGS_TAB_CONFIG);
+  const config = parseConfiguration(input);
+
+  let settings = new TabSettings([], DEFAULT_RULE);
+
+  if (typeof config === "boolean") {
+    // handled by the keybindings section of package.json
+  } else if (Array.isArray(config)) {
+    if (config.length > 0) {
+      const tabRule = new TabRule(config, config[config.length - 1]);
       settings = new TabSettings([], tabRule);
     }
   } else if (typeof config === "object") {
-    const obj = config as { default: number[]; anchors: number };
-    let defaultRule = new TabRule([0, 6, 7, 11], 72);
-    const stops = obj.default as number[];
+    let defaultRule = DEFAULT_RULE;
+
+    const stops = config.default;
     if (stops !== undefined && stops.length > 0) {
       defaultRule = new TabRule(stops, stops[stops.length - 1]);
     }
+
     const rules: TabRule[] = [];
-    const anchors = obj.anchors;
-    if (obj.anchors !== undefined && Object.keys(anchors).length > 0) {
-      const keys = Object.keys(anchors);
-      const values = Object.values(anchors);
-      for (let i = 0; i < keys.length; i++) {
-        const regex = keys[i] as string;
-        const stops = values[i] as number[];
-        if (regex !== undefined && stops !== undefined && stops.length > 0) {
+    if (config.anchors) {
+      for (const regex in config.anchors) {
+        const stops = config.anchors[regex];
+        if (stops.length > 0) {
           rules.push(new TabRule(stops, stops[stops.length - 1], regex));
         }
       }
     }
+
     settings = new TabSettings(rules, defaultRule);
   }
   return settings;
