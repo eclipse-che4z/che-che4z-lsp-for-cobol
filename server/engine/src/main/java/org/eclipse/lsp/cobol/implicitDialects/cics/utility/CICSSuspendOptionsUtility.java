c629f405202caf26a859d0aa2594e4d87d482e63
Sam Beard <samuel.beard@broadcom.com>
2024-11-21 15:13:11 -0500

    fix: SUSPEND options

---
+++ server/engine/src/main/java/org/eclipse/lsp/cobol/implicitDialects/cics/utility/CICSSuspendOptionsUtility.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2024 Broadcom.
+ * The term "Broadcom" refers to Broadcom Inc. and/or its subsidiaries.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Broadcom, Inc. - initial API and implementation
+ *
+ */
+
+package org.eclipse.lsp.cobol.implicitDialects.cics.utility;
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.eclipse.lsp.cobol.common.dialects.DialectProcessingContext;
+import org.eclipse.lsp.cobol.common.error.ErrorSeverity;
+import org.eclipse.lsp.cobol.common.error.SyntaxError;
+import org.eclipse.lsp.cobol.implicitDialects.cics.CICSLexer;
+import org.eclipse.lsp.cobol.implicitDialects.cics.CICSParser;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.eclipse.lsp.cobol.implicitDialects.cics.CICSParser.RULE_cics_suspend;
+
+/** Checks CICS ReadNext ReadPrev rules for required and invalid options */
+public class CICSSuspendOptionsUtility extends CICSOptionsCheckBaseUtility {
+
+  public static final int RULE_INDEX = RULE_cics_suspend;
+
+  private static final Map<Integer, ErrorSeverity> DUPLICATE_CHECK_OPTIONS =
+      new HashMap<Integer, ErrorSeverity>() {
+        {
+          put(CICSLexer.SUSPEND, ErrorSeverity.ERROR);
+          put(CICSLexer.ACQACTIVITY, ErrorSeverity.ERROR);
+          put(CICSLexer.ACQPROCESS, ErrorSeverity.ERROR);
+          put(CICSLexer.ACTIVITY, ErrorSeverity.ERROR);
+        }
+      };
+
+  public CICSSuspendOptionsUtility(DialectProcessingContext context, List<SyntaxError> errors) {
+    super(context, errors, DUPLICATE_CHECK_OPTIONS);
+  }
+
+  /**
+   * Entrypoint to check CICS SUSPEND rule options
+   *
+   * @param ctx ParserRuleContext subclass containing options
+   * @param <E> A subclass of ParserRuleContext
+   */
+  public <E extends ParserRuleContext> void checkOptions(E ctx) {
+    if (ctx.getParent().getRuleIndex() == RULE_cics_suspend) {
+      checkSuspend((CICSParser.Cics_suspendContext) ctx.getParent());
+    }
+    checkDuplicates(ctx.getParent());
+  }
+
+  @SuppressWarnings("unchecked")
+  private void checkSuspend(CICSParser.Cics_suspendContext ctx) {
+    checkHasMutuallyExclusiveOptions("ACQACTIVITY or ACQPROCESS or ACTIVITY",
+            ctx.ACQACTIVITY(), ctx.ACQPROCESS(), ctx.ACTIVITY());
+  }
+}
