f45497fd96b51eecd2b9b6ef8b4cd4a3063b0a7c
Michael Messer <michael.messer@broadcom.com>
2024-11-22 14:45:21 -0500

    Add valid and invalid tests

---
+++ server/engine/src/test/java/org/eclipse/lsp/cobol/usecases/TestCicsGet64ContainerStatement.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2024 Broadcom.
+ * The term "Broadcom" refers to Broadcom Inc. and/or its subsidiaries.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Broadcom, Inc. - initial API and implementation
+ *
+ */
+
+package org.eclipse.lsp.cobol.usecases;
+
+import org.eclipse.lsp.cobol.common.error.ErrorSource;
+import org.eclipse.lsp.cobol.usecases.common.CICSTestUtils;
+import org.eclipse.lsp4j.Diagnostic;
+import org.eclipse.lsp4j.DiagnosticSeverity;
+import org.eclipse.lsp4j.Range;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+/**
+ * Test GET64 CONTAINER commands. Documentation link: <a
+ * href="https://www.ibm.com/docs/en/cics-ts/6.x?topic=summary-get64-container">GET64 CONTAINER
+ * Command</a>
+ *
+ * <p>This class tests all variations of the GET64 CONTAINER command found in the link above.
+ */
+public class TestCicsGet64ContainerStatement {
+
+    // Test Strings
+    private static final String GET64CONTAINER_VALID_1 = "GET64 CONTAINER(123) INTO(123) FLENGTH(123) BYTEOFFSET(8) CONVERTST(123) CCSID(123)";
+    private static final String GET64CONTAINER_VALID_2 = "GET64 CONTAINER(123) NODATA FLENGTH(123)";
+
+    private static final String GET64CONTAINER_INVALID_1 = "GET64 CONTAINER(123) INTO(123) {NODATA|errorOne} FLENGTH(123)";
+    private static final String GET64CONTAINER_INVALID_2 = "GET64 CONTAINER(123) NODATA FLENGTH(123) {BYTEOFFSET|errorOne}(123)";
+
+    // Test Functions
+    @Test
+    void testCicsGet64ContainerValid() {
+        CICSTestUtils.noErrorTest(GET64CONTAINER_VALID_1);
+        CICSTestUtils.noErrorTest(GET64CONTAINER_VALID_2);
+    }
+
+    // Invalid Tests
+    @Test
+    void testCicsGet64ContainerInvalid_1() {
+        HashMap<String, Diagnostic> expectedDiagnostics = new HashMap<>();
+        expectedDiagnostics.put("errorOne", new Diagnostic(new Range(), "Options \"INTO, SET, NODATA\" are mutually exclusive.", DiagnosticSeverity.Error, ErrorSource.PARSING.getText()));
+        CICSTestUtils.errorTest(GET64CONTAINER_INVALID_1, expectedDiagnostics);
+    }
+
+    @Test
+    void testCicsGet64ContainerInvalid_2() {
+        HashMap<String, Diagnostic> expectedDiagnostics = new HashMap<>();
+        expectedDiagnostics.put("errorOne", new Diagnostic(new Range(), "Invalid option provided: BYTEOFFSET", DiagnosticSeverity.Error, ErrorSource.PARSING.getText()));
+        CICSTestUtils.errorTest(GET64CONTAINER_INVALID_2, expectedDiagnostics);
+    }
+
+}
