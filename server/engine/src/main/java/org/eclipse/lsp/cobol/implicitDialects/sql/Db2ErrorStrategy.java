90edf04a231bcec26dea4d99365141d8bdf07e1f
Iurii Shchekochikhin <amagex@gmail.com>
2024-11-22 11:32:13 +0100

    Keep ExecRuleContext children head instead of recreating all of them on DB2 SQL parsing error recovery.

--- server/engine/src/main/java/org/eclipse/lsp/cobol/implicitDialects/sql/Db2ErrorStrategy.java
+++ server/engine/src/main/java/org/eclipse/lsp/cobol/implicitDialects/sql/Db2ErrorStrategy.java
@@ -21,8 +21,6 @@ import org.antlr.v4.runtime.*;
 import org.eclipse.lsp.cobol.common.message.MessageService;
 import org.eclipse.lsp.cobol.common.message.MessageServiceProvider;
 
-import java.util.List;
-
 import static java.util.Optional.ofNullable;
 
 /**
@@ -30,126 +28,137 @@ import static java.util.Optional.ofNullable;
  */
 @Slf4j
 public class Db2ErrorStrategy extends DefaultErrorStrategy implements MessageServiceProvider {
-  private static final String REPORT_NO_VIABLE_ALTERNATIVE =
-      "ErrorStrategy.reportNoViableAlternative";
-  private static final String REPORT_MISSING_TOKEN = "ErrorStrategy.reportMissingToken";
-  private static final String REPORT_MISSING_END_EXEC = "db2Parser.missingEndExec";
-  @Getter @Setter private MessageService messageService;
-  @Getter @Setter private ErrorMessageHelper errorMessageHelper;
+    private static final String REPORT_NO_VIABLE_ALTERNATIVE =
+            "ErrorStrategy.reportNoViableAlternative";
+    private static final String REPORT_MISSING_TOKEN = "ErrorStrategy.reportMissingToken";
+    private static final String REPORT_MISSING_END_EXEC = "db2Parser.missingEndExec";
+    @Getter
+    @Setter
+    private MessageService messageService;
+    @Getter
+    @Setter
+    private ErrorMessageHelper errorMessageHelper;
 
-  Db2ErrorStrategy(MessageService messageService) {
-    this.messageService = messageService;
-    this.errorMessageHelper = new ErrorMessageHelper(messageService);
-  }
-
-  @Override
-  public void reportError(Parser recognizer, RecognitionException e) {
-    // if we've already reported an error and have not matched a token
-    // yet successfully, don't report any errors.
-    if (inErrorRecoveryMode(recognizer)) {
-      return; // don't report spurious errors
-    }
-    beginErrorCondition(recognizer);
-
-    if (e instanceof NoViableAltException) {
-      reportNoViableAlternative(recognizer, (NoViableAltException) e);
-    } else if (e instanceof InputMismatchException) {
-      reportInputMismatch(recognizer, (InputMismatchException) e);
-    } else if (e instanceof FailedPredicateException) {
-      reportFailedPredicate(recognizer, (FailedPredicateException) e);
-    } else {
-      reportUnrecognizedException(recognizer, e);
-    }
-  }
-
-  private void reportUnrecognizedException(Parser recognizer, RecognitionException e) {
-    LOG.error("unknown recognition error type: " + e.getClass().getName());
-    recognizer.notifyErrorListeners(e.getOffendingToken(), e.getMessage(), e);
-  }
-
-  @Override
-  protected void reportInputMismatch(Parser recognizer, InputMismatchException e) {
-    Token token = e.getOffendingToken();
-    String msg =
-        errorMessageHelper.getInputMismatchMessage(recognizer, e, token, getOffendingToken(e));
-    recognizer.notifyErrorListeners(token, msg, e);
-  }
-
-  @Override
-  protected void reportNoViableAlternative(Parser recognizer, NoViableAltException e) {
-    String messageParams = errorMessageHelper.retrieveInputForNoViableException(recognizer, e);
-    String msg = messageService.getMessage(REPORT_NO_VIABLE_ALTERNATIVE, messageParams);
-    recognizer.notifyErrorListeners(e.getStartToken(), msg, e);
-  }
-
-  @Override
-  protected void reportUnwantedToken(Parser recognizer) {
-    if (inErrorRecoveryMode(recognizer)) {
-      return;
-    }
-    beginErrorCondition(recognizer);
-    Token currentToken = recognizer.getCurrentToken();
-    String msg = errorMessageHelper.getUnwantedTokenMessage(recognizer, currentToken);
-    recognizer.notifyErrorListeners(currentToken, msg, null);
-  }
-
-  @Override
-  protected void reportMissingToken(Parser recognizer) {
-    if (inErrorRecoveryMode(recognizer)) {
-      return;
+    Db2ErrorStrategy(MessageService messageService) {
+        this.messageService = messageService;
+        this.errorMessageHelper = new ErrorMessageHelper(messageService);
     }
 
-    beginErrorCondition(recognizer);
-    String msg = recognizer.getExpectedTokens().contains(Db2SqlLexer.END_EXEC)
-              ? messageService.getMessage(REPORT_MISSING_END_EXEC)
-              : messageService.getMessage(
-                      REPORT_MISSING_TOKEN,
-                      errorMessageHelper.getExpectedText(recognizer),
-                      ErrorMessageHelper.getRule(recognizer));
-    recognizer.notifyErrorListeners(recognizer.getCurrentToken(), msg, null);
-  }
+    @Override
+    public void reportError(Parser recognizer, RecognitionException e) {
+        // if we've already reported an error and have not matched a token
+        // yet successfully, don't report any errors.
+        if (inErrorRecoveryMode(recognizer)) {
+            return; // don't report spurious errors
+        }
+        beginErrorCondition(recognizer);
 
-  private String getOffendingToken(InputMismatchException e) {
-    return getTokenErrorDisplay(e.getOffendingToken());
-  }
-
-  @Override
-  public Token recoverInline(Parser recognizer) throws RecognitionException {
-    ParserRuleContext context = recognizer.getContext();
-    if (context instanceof Db2SqlParser.ExecRuleContext) {
-      int startIdx = context.start.getTokenIndex();
-      TokenStream input = recognizer.getInputStream();
-      input.seek(startIdx + 1); // SKIP EXEC_SQL
-      Token t = input.LT(1);
-      Db2SqlParser.SqlCodeContext sqlCodeContext = ((Db2SqlParser.ExecRuleContext) context).sqlCode();
-      ofNullable(sqlCodeContext.children).ifPresent(List::clear);
-      while (!isInAriaA(t) && !isSqlStopChar(t)
-              && t.getType() != Db2SqlLexer.DOT_FS
-              && t.getType() != Token.EOF) {
-          input.consume();
-          sqlCodeContext.addChild(recognizer.createTerminalNode(sqlCodeContext, t));
-          sqlCodeContext.stop = t;
-          t = input.LT(1);
-      }
-      // Consume ';'
-      if (isSqlStopChar(t)) {
-        input.consume();
-        sqlCodeContext.addChild(recognizer.createTerminalNode(sqlCodeContext, t));
-        sqlCodeContext.stop = t;
-      }
-      Token missingSymbol = getMissingSymbol(recognizer);
-      recognizer.notifyErrorListeners(missingSymbol, messageService.getMessage(REPORT_MISSING_END_EXEC), null);
-      return missingSymbol;
+        if (e instanceof NoViableAltException) {
+            reportNoViableAlternative(recognizer, (NoViableAltException) e);
+        } else if (e instanceof InputMismatchException) {
+            reportInputMismatch(recognizer, (InputMismatchException) e);
+        } else if (e instanceof FailedPredicateException) {
+            reportFailedPredicate(recognizer, (FailedPredicateException) e);
+        } else {
+            reportUnrecognizedException(recognizer, e);
+        }
     }
-    return super.recoverInline(recognizer);
-  }
 
-  private static boolean isSqlStopChar(Token t) {
-    return t.getType() == Db2SqlLexer.SEMICOLON_FS || t.getType() == Db2SqlLexer.SEMICOLONSEPARATORSQL;
-  }
+    private void reportUnrecognizedException(Parser recognizer, RecognitionException e) {
+        LOG.error("unknown recognition error type: " + e.getClass().getName());
+        recognizer.notifyErrorListeners(e.getOffendingToken(), e.getMessage(), e);
+    }
 
-  private boolean isInAriaA(Token t) {
-    int pos = t.getCharPositionInLine() + 1;
-    return 8 <= pos && pos <= 11;
-  }
+    @Override
+    protected void reportInputMismatch(Parser recognizer, InputMismatchException e) {
+        Token token = e.getOffendingToken();
+        String msg =
+                errorMessageHelper.getInputMismatchMessage(recognizer, e, token, getOffendingToken(e));
+        recognizer.notifyErrorListeners(token, msg, e);
+    }
+
+    @Override
+    protected void reportNoViableAlternative(Parser recognizer, NoViableAltException e) {
+        String messageParams = errorMessageHelper.retrieveInputForNoViableException(recognizer, e);
+        String msg = messageService.getMessage(REPORT_NO_VIABLE_ALTERNATIVE, messageParams);
+        recognizer.notifyErrorListeners(e.getStartToken(), msg, e);
+    }
+
+    @Override
+    protected void reportUnwantedToken(Parser recognizer) {
+        if (inErrorRecoveryMode(recognizer)) {
+            return;
+        }
+        beginErrorCondition(recognizer);
+        Token currentToken = recognizer.getCurrentToken();
+        String msg = errorMessageHelper.getUnwantedTokenMessage(recognizer, currentToken);
+        recognizer.notifyErrorListeners(currentToken, msg, null);
+    }
+
+    @Override
+    protected void reportMissingToken(Parser recognizer) {
+        if (inErrorRecoveryMode(recognizer)) {
+            return;
+        }
+
+        beginErrorCondition(recognizer);
+        String msg = recognizer.getExpectedTokens().contains(Db2SqlLexer.END_EXEC)
+                ? messageService.getMessage(REPORT_MISSING_END_EXEC)
+                : messageService.getMessage(
+                REPORT_MISSING_TOKEN,
+                errorMessageHelper.getExpectedText(recognizer),
+                ErrorMessageHelper.getRule(recognizer));
+        recognizer.notifyErrorListeners(recognizer.getCurrentToken(), msg, null);
+    }
+
+    private String getOffendingToken(InputMismatchException e) {
+        return getTokenErrorDisplay(e.getOffendingToken());
+    }
+
+    @Override
+    public Token recoverInline(Parser recognizer) throws RecognitionException {
+        ParserRuleContext context = recognizer.getContext();
+        if (context instanceof Db2SqlParser.ExecRuleContext) {
+            Db2SqlParser.SqlCodeContext sqlCodeContext = ((Db2SqlParser.ExecRuleContext) context).sqlCode();
+            int nodeCount = adjustConsumedNodes(recognizer, sqlCodeContext);
+            ofNullable(sqlCodeContext.children).ifPresent(ch -> ch.subList(nodeCount, ch.size()).clear());
+            Token missingSymbol = getMissingSymbol(recognizer);
+            recognizer.notifyErrorListeners(missingSymbol, messageService.getMessage(REPORT_MISSING_END_EXEC), null);
+            return missingSymbol;
+        }
+        return super.recoverInline(recognizer);
+    }
+
+    private int adjustConsumedNodes(Parser recognizer, Db2SqlParser.SqlCodeContext sqlCodeContext) {
+        int nodeCount = 0;
+        int startIdx = sqlCodeContext.start.getTokenIndex();
+        TokenStream input = recognizer.getInputStream();
+        input.seek(startIdx);
+        Token t = input.LT(1);
+
+        while (!isInAriaA(t) && !isSqlStopChar(t)
+                && t.getType() != Db2SqlLexer.DOT_FS
+                && t.getType() != Token.EOF) {
+            input.consume();
+            nodeCount++;
+            sqlCodeContext.stop = t;
+            t = input.LT(1);
+        }
+        // Consume ';'
+        if (isSqlStopChar(t)) {
+            input.consume();
+            nodeCount++;
+            sqlCodeContext.stop = t;
+        }
+        return nodeCount;
+    }
+
+    private static boolean isSqlStopChar(Token t) {
+        return t.getType() == Db2SqlLexer.SEMICOLON_FS || t.getType() == Db2SqlLexer.SEMICOLONSEPARATORSQL;
+    }
+
+    private boolean isInAriaA(Token t) {
+        int pos = t.getCharPositionInLine() + 1;
+        return 8 <= pos && pos <= 11;
+    }
 }
