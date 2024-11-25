2880c9c02f1224fc5b6e499d2eae0cc87686d567
Michael Messer <michael.messer@broadcom.com>
2024-11-25 13:24:56 -0500

    Add checks and tie into map

--- server/engine/src/main/java/org/eclipse/lsp/cobol/implicitDialects/cics/utility/CICSOptionsCheckUtility.java
+++ server/engine/src/main/java/org/eclipse/lsp/cobol/implicitDialects/cics/utility/CICSOptionsCheckUtility.java
@@ -93,6 +93,9 @@ public class CICSOptionsCheckUtility {
     optionsMap.put(
         CICSReadNextReadPrevOptionsUtility.RULE_INDEX,
         new CICSReadNextReadPrevOptionsUtility(context, errors));
+    optionsMap.put(
+        CICSRewriteOptionsCheckUtility.RULE_INDEX,
+        new CICSRewriteOptionsCheckUtility(context, errors));
   }
 
   /**
