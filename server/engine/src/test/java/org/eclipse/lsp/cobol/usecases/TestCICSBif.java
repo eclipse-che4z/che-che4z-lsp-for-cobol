cd7df862e2cd6b710acd0b37d0c8bbb72d7272c1
Ilidio Lopes <ilidio.depinalopes@broadcom.com>
2024-11-29 15:14:04 +0100

    pr comments

--- server/engine/src/test/java/org/eclipse/lsp/cobol/usecases/TestCICSBif.java
+++ server/engine/src/test/java/org/eclipse/lsp/cobol/usecases/TestCICSBif.java
@@ -13,18 +13,14 @@
  *
  */
 package org.eclipse.lsp.cobol.usecases;
-
-        import com.google.common.collect.ImmutableMap;
-        import org.eclipse.lsp.cobol.common.error.ErrorSource;
-        import org.eclipse.lsp.cobol.usecases.common.CICSTestUtils;
-        import org.eclipse.lsp4j.Diagnostic;
-        import org.eclipse.lsp4j.DiagnosticSeverity;
-        import org.eclipse.lsp4j.Position;
-        import org.eclipse.lsp4j.Range;
-        import org.junit.jupiter.api.Test;
-
-        import java.util.Map;
-
+import com.google.common.collect.ImmutableMap;
+import org.eclipse.lsp.cobol.common.error.ErrorSource;
+import org.eclipse.lsp.cobol.usecases.common.CICSTestUtils;
+import org.eclipse.lsp4j.Diagnostic;
+import org.eclipse.lsp4j.DiagnosticSeverity;
+import org.eclipse.lsp4j.Range;
+import org.junit.jupiter.api.Test;
+import java.util.Map;
 /**
  * Test CICS BIF commands. Documentation link:
  *<a href="https://www.ibm.com/docs/en/cics-ts/6.x?topic=summary-bif-deedit">BIF DEEDIT Command</a>
@@ -34,12 +30,10 @@ package org.eclipse.lsp.cobol.usecases;
 public class TestCICSBif {
     private static final String BIF_DEEDIT_FIELD_VALID =
             "BIF DEEDIT FIELD({$varFour})";
-
     private static final String BIF_DEEDIT_FIELD_LENGTH_VALID =
             "BIF DEEDIT FIELD({$varFour}) LENGTH(100)";
-
     private static final String BIF_DEEDIT_INVALID =
-            "BIF {DEEDIT | errorMissingField }";
+            "BIF {DEEDIT|errorMissingField }";
     private static final String BIF_DIGEST_RECORD_RECORDLEN_RESULT_VALID =
             "BIF DIGEST RECORD(100) RECORDLEN(100) RESULT({$varFour})";
     private static final String BIF_DIGEST_RECORD_RECORDLEN_HEX_RESULT_VALID =
@@ -51,20 +45,17 @@ public class TestCICSBif {
     private static final String BIF_DIGEST_RECORD_RECORDLEN_DIGESTTYPE_RESULT_VALID =
             "BIF DIGEST RECORD(100) RECORDLEN(100) DIGESTTYPE({$varFour}) RESULT({$varFour})";
     private static final String BIF_DIGEST_DIGESTTYPE_INVALID =
-            "BIF DIGEST {RECORD |errorResultMissing }(100) RECORDLEN(100) {HEX | errorDigestType} {BASE64 | errorDigestType2}";
+            "BIF DIGEST RESULT({$varFour}) RECORD({$varFour}) RECORDLEN({$varFour}) {HEX|errorDigestType} {BASE64|errorDigestType2}";
     private static final String BIF_DIGEST_RESULT_MISSING_INVALID =
-            "BIF DIGEST RECORD(100) {RECORDLEN | errorResultMissing}(100)";
-
+            "BIF {_DIGEST RECORD(100) RECORDLEN(100)|errorResultMissing_}";
     @Test
     void testBifDeeditField() {
         CICSTestUtils.noErrorTest(BIF_DEEDIT_FIELD_VALID);
     }
-
     @Test
     void testBifDeeditFieldLengthValid() {
         CICSTestUtils.noErrorTest(BIF_DEEDIT_FIELD_LENGTH_VALID);
     }
-
     @Test
     void testBifDeeditDigestInvalid() {
         Map<String, Diagnostic> expectedDiagnostic =
@@ -75,7 +66,6 @@ public class TestCICSBif {
                                 "Missing required option: FIELD",
                                 DiagnosticSeverity.Error,
                                 ErrorSource.PARSING.getText()));
-
         CICSTestUtils.errorTest(BIF_DEEDIT_INVALID, expectedDiagnostic);
     }
     @Test
@@ -102,25 +92,19 @@ public class TestCICSBif {
     void testBifDigestDigesttypeInvalid() {
         Map<String, Diagnostic> expectedDiagnostic =
                 ImmutableMap.of(
-                        "errorResultMissing",
-                        new Diagnostic(
-                                new Range(new Position(14, 12), new Position(18, 18)),
-                                "Missing required option: RESULT",
-                                DiagnosticSeverity.Error,
-                                ErrorSource.PARSING.getText()),
                         "errorDigestType",
                         new Diagnostic(
-                                new Range(new Position(17, 12), new Position(17, 15)),
+                                new Range(),
                                 "Exactly one option required, options are mutually exclusive: HEX or BINARY or BASE64 or DIGESTTYPE",
                                 DiagnosticSeverity.Error,
                                 ErrorSource.PARSING.getText()),
                         "errorDigestType2",
                         new Diagnostic(
-                                new Range(new Position(18, 12), new Position(18, 18)),
+                                new Range(),
                                 "Exactly one option required, options are mutually exclusive: HEX or BINARY or BASE64 or DIGESTTYPE",
                                 DiagnosticSeverity.Error,
                                 ErrorSource.PARSING.getText())
-                        );
+                );
         CICSTestUtils.errorTest(BIF_DIGEST_DIGESTTYPE_INVALID, expectedDiagnostic);
     }
     @Test
@@ -129,10 +113,28 @@ public class TestCICSBif {
                 ImmutableMap.of(
                         "errorResultMissing",
                         new Diagnostic(
-                                new Range(new Position(14, 12), new Position(16, 26)),
+                                new Range(),
                                 "Missing required option: RESULT",
                                 DiagnosticSeverity.Error,
                                 ErrorSource.PARSING.getText()));
         CICSTestUtils.errorTest(BIF_DIGEST_RESULT_MISSING_INVALID, expectedDiagnostic);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
