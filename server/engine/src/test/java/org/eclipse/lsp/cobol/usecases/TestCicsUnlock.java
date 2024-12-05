06614fc46e0719e32b62390911b9ed2343d93df4
Sam Beard <samuel.beard@broadcom.com>
2024-12-05 15:31:20 -0500

    Add Invalid test

--- server/engine/src/test/java/org/eclipse/lsp/cobol/usecases/TestCicsUnlock.java
+++ server/engine/src/test/java/org/eclipse/lsp/cobol/usecases/TestCicsUnlock.java
@@ -14,7 +14,13 @@
  */
 package org.eclipse.lsp.cobol.usecases;
 
+import com.google.common.collect.ImmutableMap;
+import org.eclipse.lsp.cobol.common.error.ErrorSource;
 import org.eclipse.lsp.cobol.usecases.common.CICSTestUtils;
+import org.eclipse.lsp4j.Diagnostic;
+import org.eclipse.lsp4j.DiagnosticSeverity;
+import org.eclipse.lsp4j.Position;
+import org.eclipse.lsp4j.Range;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -36,6 +42,8 @@ public class TestCicsUnlock {
   private static final String BARE_OPTIONS =
           "UNLOCK FILE({$varOne})";
 
+  private static final String INVALID = "UNLOCK {SYSID(3) | error}";
+
   @Test
   void testAllOptions() {
     CICSTestUtils.noErrorTest(ALL_OPTIONS);
@@ -54,5 +62,16 @@ public class TestCicsUnlock {
   @Test
   void testBareOptions() {
     CICSTestUtils.noErrorTest(BARE_OPTIONS);
+  }
+
+  @Test
+  void testInvalid() {
+    CICSTestUtils.errorTest(INVALID, ImmutableMap.of(
+            "error",
+            new Diagnostic(
+                    new Range(new Position(12, 12), new Position(13, 20)),
+                    "Missing required option: FILE",
+                    DiagnosticSeverity.Error,
+                    ErrorSource.PARSING.getText())));
   }
 }
