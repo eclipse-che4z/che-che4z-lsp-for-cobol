a40d2b9cc825a99da674ad703a9279d1ff0c5c76
Iurii Shchekochikhin <amagex@gmail.com>
2024-12-09 13:49:31 +0100

    chore: add a test for copybook name completion inside copybooks

--- server/engine/src/test/java/org/eclipse/lsp/cobol/service/delegates/completions/CopybookNameCompletionTest.java
+++ server/engine/src/test/java/org/eclipse/lsp/cobol/service/delegates/completions/CopybookNameCompletionTest.java
@@ -26,6 +26,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -41,24 +42,56 @@ class CopybookNameCompletionTest {
   @Test
   void testCopybookNameCompletion() {
     when(copybookNameService.getNames("id")).thenReturn(Arrays.asList(
-        CopybookName.builder().displayName("Copy1").build(),
-        CopybookName.builder().displayName("copy2").build(),
-        CopybookName.builder().displayName("no-copy").build()));
+            CopybookName.builder().displayName("Copy1").build(),
+            CopybookName.builder().displayName("copy2").build(),
+            CopybookName.builder().displayName("no-copy").build()));
 
     SourceUnitGraph sourceUnitGraph = mock(SourceUnitGraph.class);
     when(sourceUnitGraph.getAllAssociatedFilesForACopybook(anyString())).thenReturn(new ArrayList<>());
     CopybookNameCompletion copybookNameCompletion = new CopybookNameCompletion(copybookNameService, sourceUnitGraph);
     CobolDocumentModel document = new CobolDocumentModel("id", TEXT, AnalysisResult.builder().build());
     CompletionParams params =
-        new CompletionParams(new TextDocumentIdentifier("id"), new Position(1, 16));
+            new CompletionParams(new TextDocumentIdentifier("id"), new Position(1, 16));
 
     List<CompletionItem> completionItems =
-        new Completions(ImmutableSet.of(copybookNameCompletion))
-            .collectFor(document, params)
-            .getItems();
+            new Completions(ImmutableSet.of(copybookNameCompletion))
+                    .collectFor(document, params)
+                    .getItems();
 
     assertEquals(3, completionItems.size());
     assertEquals("Copy1", completionItems.get(0).getLabel());
     assertEquals(CompletionItemKind.Class, completionItems.get(0).getKind());
+  }
+  @Test
+
+  void testCopybookNameCompletionWithinACopybook() {
+    when(copybookNameService.getNames("cId")).thenReturn(Arrays.asList(
+            CopybookName.builder().displayName("Copy1").build(),
+            CopybookName.builder().displayName("Copy2").build(),
+            CopybookName.builder().displayName("no-copy").build()));
+    when(copybookNameService.getNames("pId")).thenReturn(
+            Collections.singletonList(CopybookName.builder().displayName("Copy3").build()));
+
+    SourceUnitGraph sourceUnitGraph = mock(SourceUnitGraph.class);
+    when(sourceUnitGraph.getAllAssociatedFilesForACopybook(anyString())).thenReturn(Collections.singletonList("pId"));
+    CopybookNameCompletion copybookNameCompletion = new CopybookNameCompletion(copybookNameService, sourceUnitGraph);
+    CobolDocumentModel document = new CobolDocumentModel("cId", TEXT, AnalysisResult.builder().build());
+    CompletionParams params =
+            new CompletionParams(new TextDocumentIdentifier("cId"), new Position(1, 16));
+
+    List<CompletionItem> completionItems =
+            new Completions(ImmutableSet.of(copybookNameCompletion))
+                    .collectFor(document, params)
+                    .getItems();
+
+    assertEquals(4, completionItems.size());
+    assertEquals("Copy1", completionItems.get(0).getLabel());
+    assertEquals("Copy2", completionItems.get(1).getLabel());
+    assertEquals("no-copy", completionItems.get(2).getLabel());
+    assertEquals("Copy3", completionItems.get(3).getLabel());
+    assertEquals(CompletionItemKind.Class, completionItems.get(0).getKind());
+    assertEquals(CompletionItemKind.Class, completionItems.get(1).getKind());
+    assertEquals(CompletionItemKind.Class, completionItems.get(2).getKind());
+    assertEquals(CompletionItemKind.Class, completionItems.get(3).getKind());
   }
 }
