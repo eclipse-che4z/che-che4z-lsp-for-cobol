96bb9a0ac08f34da978e736086a83c06f4ee32f1
Michael Messer <michael.messer@broadcom.com>
2024-12-11 12:26:20 -0500

    Add to options map

--- server/engine/src/main/java/org/eclipse/lsp/cobol/implicitDialects/cics/utility/CICSOptionsCheckUtility.java
+++ server/engine/src/main/java/org/eclipse/lsp/cobol/implicitDialects/cics/utility/CICSOptionsCheckUtility.java
@@ -144,6 +144,9 @@ public class CICSOptionsCheckUtility {
     optionsMap.put(
         CICSWSAEPRUtility.RULE_INDEX,
         new CICSWSAEPRUtility(context, errors));
+    optionsMap.put(
+        CICSSysSetOptionsCheckUtility.RULE_INDEX,
+        new CICSSysSetOptionsCheckUtility(context, errors));
   }
 
   /**
