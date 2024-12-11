de8bf463921bcad3317ecf7311440a9337ff166b
Michael Messer <michael.messer@broadcom.com>
2024-12-11 10:58:34 -0500

    Add function for obsolete tokens.

--- server/engine/src/main/java/org/eclipse/lsp/cobol/implicitDialects/cics/utility/CICSOptionsCheckBaseUtility.java
+++ server/engine/src/main/java/org/eclipse/lsp/cobol/implicitDialects/cics/utility/CICSOptionsCheckBaseUtility.java
@@ -190,6 +190,34 @@ public abstract class CICSOptionsCheckBaseUtility {
   }
 
   /**
+   * Helper method to collect analysis errors if the rule context contains obsolete options
+   *
+   * @param rules Generic list of rules to check. Will either be a collection of ParserRuleContext
+   *     or TerminalNode
+   * @param options Options checked to insert into error message
+   */
+  protected <E extends ParseTree> void checkHasObsoleteOptions(List<E> rules, String options) {
+    if (!rules.isEmpty()) {
+      rules.forEach(
+              error ->
+                      throwException(
+                              ErrorSeverity.ERROR, getLocality(error), "Obsolete option provided: ", options));
+    }
+  }
+
+  /**
+   * Helper method to collect analysis errors if the rule context contains obsolete options
+   *
+   * @param rule TerminalNode to check.
+   * @param options Options checked to insert into error message
+   */
+  protected void checkHasObsoleteOptions(TerminalNode rule, String options) {
+    if (!rule.getText().trim().isEmpty()) {
+      throwException(ErrorSeverity.ERROR, getLocality(rule), "Obsolete option provided: ", options);
+    }
+  }
+
+  /**
    * Helper function to check and see if more than one rule was visited out of a set provided.
    *
    * @param options Options checked to insert into error message
