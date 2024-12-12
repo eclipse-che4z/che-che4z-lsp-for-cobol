3d7c90c08ae3a031d6b6683df1ca05e0b43a1500
amanprashant <aman.prashant@broadcom.com>
2024-12-12 13:15:13 +0100

    test: temporary fix
    Signed-off-by: amanprashant <aman.prashant@broadcom.com>

--- server/engine/src/test/java/org/eclipse/lsp/cobol/implicitDialects/cics/processor/CICSImplicitVariablesProcessorTest.java
+++ server/engine/src/test/java/org/eclipse/lsp/cobol/implicitDialects/cics/processor/CICSImplicitVariablesProcessorTest.java
@@ -21,7 +21,6 @@ import org.eclipse.lsp.cobol.common.model.tree.ProgramSubtype;
 import org.eclipse.lsp.cobol.common.model.tree.SectionNode;
 import org.eclipse.lsp.cobol.common.processor.ProcessingContext;
 import org.eclipse.lsp.cobol.common.symbols.VariableAccumulator;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -32,35 +31,31 @@ import static org.mockito.Mockito.*;
  */
 class CICSImplicitVariablesProcessorTest {
   private static final int CICS_INTRODUCED_REGISTERS_COUNT = 72;
-  private ProcessingContext processingContext;
-  private VariableAccumulator variableAccumulator;
-  CICSImplicitVariablesProcessor processor;
-
-  @BeforeEach
-  void init() {
-    processingContext = mock(ProcessingContext.class);
-    variableAccumulator = mock(VariableAccumulator.class);
-
-    when(processingContext.getVariableAccumulator()).thenReturn(variableAccumulator);
-    processor = new CICSImplicitVariablesProcessor();
-  }
 
   @Test
   void testLinkageSectionWhenCicsTranslateEnabled() {
+    ProcessingContext processingContext = mock(ProcessingContext.class);
+    VariableAccumulator variableAccumulator = mock(VariableAccumulator.class);
+
+    when(processingContext.getVariableAccumulator()).thenReturn(variableAccumulator);
     SectionNode sectionNode = new SectionNode(Locality.builder().build(), SectionType.LINKAGE);
     sectionNode.setParent(new ProgramNode(Locality.builder().build(), ProgramSubtype.Program, 0));
 
-    processor.accept(sectionNode, processingContext);
+    new CICSImplicitVariablesProcessor().accept(sectionNode, processingContext);
     verify(variableAccumulator, times(33)).addVariableDefinition(any(), any());
   }
 
   @Test
   void testWorkingSectionWhenCicsTranslateEnabled() {
+    ProcessingContext processingContext = mock(ProcessingContext.class);
+    VariableAccumulator variableAccumulator = mock(VariableAccumulator.class);
+
+    when(processingContext.getVariableAccumulator()).thenReturn(variableAccumulator);
     SectionNode sectionNode =
             new SectionNode(Locality.builder().build(), SectionType.WORKING_STORAGE);
     sectionNode.setParent(new ProgramNode(Locality.builder().build(), ProgramSubtype.Program, 0));
 
-    processor.accept(sectionNode, processingContext);
+    new CICSImplicitVariablesProcessor().accept(sectionNode, processingContext);
     verify(variableAccumulator, times(CICS_INTRODUCED_REGISTERS_COUNT))
             .addVariableDefinition(any(), any());
   }
