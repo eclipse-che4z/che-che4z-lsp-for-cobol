9c8710e49b2ef91588aabe3f391df7974c2304ab
ilidio-lopes <ilidio.depinalopes@broadcom.com>
2025-01-07 13:44:08 +0100

    Update server/engine/src/main/java/org/eclipse/lsp/cobol/implicitDialects/cics/utility/CICSStartbrOptionsCheckUtility.java
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>

--- server/engine/src/main/java/org/eclipse/lsp/cobol/implicitDialects/cics/utility/CICSStartbrOptionsCheckUtility.java
+++ server/engine/src/main/java/org/eclipse/lsp/cobol/implicitDialects/cics/utility/CICSStartbrOptionsCheckUtility.java
@@ -34,7 +34,6 @@ public class CICSStartbrOptionsCheckUtility extends CICSOptionsCheckBaseUtility 
   private static final Map<Integer, ErrorSeverity> DUPLICATE_CHECK_OPTIONS =
           new HashMap<Integer, ErrorSeverity>() {
             {
-              put(CICSLexer.STARTBR, ErrorSeverity.ERROR);
               put(CICSLexer.FILE, ErrorSeverity.ERROR);
               put(CICSLexer.RIDFLD, ErrorSeverity.ERROR);
               put(CICSLexer.KEYLENGTH, ErrorSeverity.ERROR);
