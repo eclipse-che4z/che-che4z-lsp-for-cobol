bace73a48df95805e6ffd35c3bde4bca7ed9633f
Jan Hybl <jan.hybl@broadcom.com>
2025-01-08 15:58:54 +0100

    Fix failing test & fixed missing reflection item

--- clients/cobol-lsp-vscode-extension/src/services/Settings.ts
+++ clients/cobol-lsp-vscode-extension/src/services/Settings.ts
@@ -246,6 +246,19 @@ export class SettingsService {
     ];
     const wsFolders = SettingsUtils.getWorkspaceFoldersPath(true);
 
+    // if no configuration for local or remote copybook paths is provided
+    // use pattern for workspace folder and subfolders as a default value
+    if (
+      paths.length === 0 &&
+      this.getCopybookEndevorDependencySettings() !== ENDEVOR_PROCESSOR &&
+      this.getCopybookConfigValues(PATHS_DSN, documentUri, dialectType)
+        .length === 0 &&
+      this.getCopybookConfigValues(PATHS_USS, documentUri, dialectType)
+        .length === 0
+    ) {
+      paths.push("**");
+    }
+
     return SettingsService.prepareLocalSearchFolders(paths, wsFolders);
   }
 
