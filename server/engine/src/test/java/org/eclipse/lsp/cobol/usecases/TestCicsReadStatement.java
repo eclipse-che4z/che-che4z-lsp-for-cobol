86f2b853a3a1511922d2aca131538ef72e421d92
Michael Messer <michael.messer@broadcom.com>
2025-01-13 14:33:37 -0500

    Revert "Update server/engine/src/test/java/org/eclipse/lsp/cobol/usecases/TestCicsReadStatement.java"
    This reverts commit 6ff2616e9d7012f4b435bcc0bd07c46ef19a6987.

--- server/engine/src/test/java/org/eclipse/lsp/cobol/usecases/TestCicsReadStatement.java
+++ server/engine/src/test/java/org/eclipse/lsp/cobol/usecases/TestCicsReadStatement.java
@@ -58,15 +58,15 @@ public class TestCicsReadStatement {
     @Test
     void testReadInvalid1() {
         HashMap<String, Diagnostic> expectedDiagnostics = new HashMap<>();
-        expectedDiagnostics.put("errorOne", new Diagnostic(new Range(), "Exactly one option required, none provided: FILE or DATASET", DiagnosticSeverity.Error, ErrorSource.PARSING.getText()));
+        expectedDiagnostics.put("errorOne", new Diagnostic(new Range(), "Exactly one option required, none provided: FILE instead of DATASET", DiagnosticSeverity.Error, ErrorSource.PARSING.getText()));
         CICSTestUtils.errorTest(READ_INVALID_1, expectedDiagnostics);
     }
 
     @Test
     void testReadInvalid2() {
         HashMap<String, Diagnostic> expectedDiagnostics = new HashMap<>();
-        expectedDiagnostics.put("errorOne", new Diagnostic(new Range(), "Exactly one option required, options are mutually exclusive: FILE or DATASET", DiagnosticSeverity.Error, ErrorSource.PARSING.getText()));
-        expectedDiagnostics.put("errorTwo", new Diagnostic(new Range(), "Exactly one option required, options are mutually exclusive: FILE or DATASET", DiagnosticSeverity.Error, ErrorSource.PARSING.getText()));
+        expectedDiagnostics.put("errorOne", new Diagnostic(new Range(), "Exactly one option required, options are mutually exclusive: FILE instead of DATASET", DiagnosticSeverity.Error, ErrorSource.PARSING.getText()));
+        expectedDiagnostics.put("errorTwo", new Diagnostic(new Range(), "Exactly one option required, options are mutually exclusive: FILE instead of DATASET", DiagnosticSeverity.Error, ErrorSource.PARSING.getText()));
         CICSTestUtils.errorTest(READ_INVALID_2, expectedDiagnostics);
     }
 
