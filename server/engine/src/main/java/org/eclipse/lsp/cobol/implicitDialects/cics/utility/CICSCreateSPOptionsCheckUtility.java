a523f6b1960d1b73af5f3193dc9524db55418fd3
Kutlu <kutlu.ozel@broadcom.com>
2025-01-15 10:46:48 +0100

    suggestions

--- server/engine/src/main/java/org/eclipse/lsp/cobol/implicitDialects/cics/utility/CICSCreateSPOptionsCheckUtility.java
+++ server/engine/src/main/java/org/eclipse/lsp/cobol/implicitDialects/cics/utility/CICSCreateSPOptionsCheckUtility.java
@@ -16,7 +16,6 @@ package org.eclipse.lsp.cobol.implicitDialects.cics.utility;
 
         import org.antlr.v4.runtime.ParserRuleContext;
 
-        import org.antlr.v4.runtime.tree.ParseTree;
         import org.antlr.v4.runtime.tree.TerminalNode;
         import org.eclipse.lsp.cobol.common.dialects.DialectProcessingContext;
         import org.eclipse.lsp.cobol.common.error.ErrorSeverity;
@@ -146,15 +145,9 @@ public class CICSCreateSPOptionsCheckUtility extends CICSOptionsCheckBaseUtility
     }
     private void checkRequiredSubOperand(CICSParser.Cics_create_optsContext ctx) {
         if (ctx.children == null) return;
-        ParseTree last = ctx.children.get(ctx.children.size() - 1);
-        if (TerminalNode.class.isAssignableFrom(last.getClass())
-                && Arrays.stream(COMMANDS_WITH_DISCARD_COMPLETE_OPTS).anyMatch(i -> i == ((TerminalNode) last).getSymbol().getType())) {
-            throwException(ErrorSeverity.ERROR, getLocality(last), "", "Operand value required");
-            return;
-        }
-        for (int index = 0; index < ctx.children.size() - 1; index++) {
+        for (int index = 0; index < ctx.children.size(); index++) {
             if (!TerminalNode.class.isAssignableFrom(ctx.children.get(index).getClass())
-                    || CICSParser.Cics_data_valueContext.class.isAssignableFrom(ctx.children.get(index + 1).getClass())) continue;
+                    || (index + 1 < ctx.children.size()) && CICSParser.Cics_data_valueContext.class.isAssignableFrom(ctx.children.get(index + 1).getClass())) continue;
             int tokenIndex = ((TerminalNode) ctx.children.get(index)).getSymbol().getType();
             if (Arrays.stream(COMMANDS_WITH_DISCARD_COMPLETE_OPTS).anyMatch(i -> i == tokenIndex)
             ) {
