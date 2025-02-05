8438a9137e69e0cd680bb5a0ada9d498f242cef1
Michael Messer <michael.messer@broadcom.com>
2025-02-05 09:23:08 -0500

    Fix tests for DB2ENTRY/DB2TRAN

--- server/engine/src/test/java/org/eclipse/lsp/cobol/usecases/TestCicsSysSetStatement.java
+++ server/engine/src/test/java/org/eclipse/lsp/cobol/usecases/TestCicsSysSetStatement.java
@@ -50,7 +50,7 @@ public class TestCicsSysSetStatement {
     private static final String DB2ENTRY_VALID_1 = "SET DB2ENTRY({$varOne})";
     private static final String DB2ENTRY_VALID_2 = "SET DB2ENTRY({$varOne}) ACCOUNTREC({$varOne}) AUTHID({$varOne}) AUTHTYPE({$varOne}) BUSY({$varOne}) DISABLEDACT({$varOne}) ENABLESTATUS({$varOne}) PLAN({$varOne}) PLANEXITNAME({$varOne}) PRIORITY({$varOne}) SHARELOCKS({$varOne}) THREADLIMIT({$varOne}) THREADWAIT({$varOne})";
     private static final String DB2TRAN_VALID_1 = "SET DB2TRAN({$varOne})";
-    private static final String DB2TRAN_VALID_2 = "SET DB2TRAN DB2ENTRY({$varOne}) TRANSID({$varOne})";
+    private static final String DB2TRAN_VALID_2 = "SET DB2TRAN({$varOne}) DB2ENTRY({$varOne}) TRANSID({$varOne})";
     private static final String DELETSHIPPED_VALID_1 = "SET DELETSHIPPED";
     private static final String DELETSHIPPED_VALID_2 = "SET DELETSHIPPED IDLE({$varOne}) INTERVALHRS({$varOne}) INTERVALMINS({$varOne}) INTERVALSECS({$varOne})";
     private static final String DISPATCHER_VALID_1 = "SET DISPATCHER";
@@ -176,9 +176,9 @@ public class TestCicsSysSetStatement {
     private static final String CONNECTION_INVALID_2 = "SET CONNECTION({$varOne}) BACKOUT {COMMIT|errorOne}";
     private static final String DB2CONN_INVALID_1 = "SET DB2CONN TASK {NONE|errorOne}";
     private static final String DB2CONN_INVALID_2 = "SET DB2CONN TWAIT {NOTWAIT|errorOne}";
-    private static final String DB2ENTRY_INVALID_1 = "SET DB2ENTRY ABEND {SQLCODE|errorOne}";
+    private static final String DB2ENTRY_INVALID_1 = "SET DB2ENTRY({$varOne}) ABEND {SQLCODE|errorOne}";
     private static final String DB2ENTRY_INVALID_2 = "SET DB2ENTRY({$varOne}) YES {NO|errorOne}";
-    private static final String DB2TRAN_INVALID_1 = "SET DB2TRAN DB2ENTRY({$varOne}) {DB2ENTRY|errorOne}({$varOne})";
+    private static final String DB2TRAN_INVALID_1 = "SET DB2TRAN({$varOne}) DB2ENTRY({$varOne}) {DB2ENTRY|errorOne}({$varOne})";
     private static final String DELETSHIPPED_INVALID_1 = "SET DELETSHIPPED IDLE({$varOne}) {IDLEHRS|errorOne}({$varOne})";
     private static final String DELETSHIPPED_INVALID_2 = "SET DELETSHIPPED INTERVAL({$varOne}) {INTERVALHRS|errorOne}({$varOne})";
     private static final String DISPATCHER_INVALID_1 = "SET DISPATCHER TIME({$varOne}) {TIME|errorOne}({$varOne})";
