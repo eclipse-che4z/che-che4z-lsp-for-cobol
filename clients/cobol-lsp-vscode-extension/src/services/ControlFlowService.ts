a47b983a9e498b05ba75fa8ac1f6e451b39571c1
Leonid Baranov <leonid.baranov@broadcom.com>
2025-03-12 12:27:04 +0100

    Address comments

--- clients/cobol-lsp-vscode-extension/src/services/ControlFlowService.ts
+++ clients/cobol-lsp-vscode-extension/src/services/ControlFlowService.ts
@@ -75,7 +75,7 @@ interface AnalysisServiceDelegate {
 }
 
 class AnalysisTask {
-  private worker: Worker = new Worker(join(__dirname, "./dist/Worker.js"));
+  private worker: Worker = new Worker("./dist/Worker.js");
 
   constructor(
     private documentUri: string,
