fe977a106db54db23ae6ee7a45cac8d1f6945ae6
Jan Hybl <jan.hybl@broadcom.com>
2025-03-21 15:13:11 +0100

    Increase test timeout

--- clients/cobol-lsp-vscode-extension/src/test/suite/lsp.spec.copybooks.test.ts
+++ clients/cobol-lsp-vscode-extension/src/test/suite/lsp.spec.copybooks.test.ts
@@ -254,7 +254,7 @@ suite("Integration Test Suite: Copybooks", function () {
       );
 
       await vscode.commands.executeCommand("acceptSelectedSuggestion");
-      await helper.sleep(1000);
+      await helper.sleep(500);
       assert.strictEqual(editor.document.lineAt(19).text.trim(), "COPY PAYLIB");
     });
   });
--- clients/cobol-lsp-vscode-extension/src/test/suite/subroutines.spec.test.ts
+++ clients/cobol-lsp-vscode-extension/src/test/suite/subroutines.spec.test.ts
@@ -38,6 +38,7 @@ suite("Integration Test Suite: Subroutines resolving", () => {
   });
 
   test("Subroutines auto completions are provided", async function () {
+    this.timeout(helper.TEST_TIMEOUT);
     const editor = await helper.showDocument("CALL.cbl");
     await helper.insertString(
       editor,
@@ -49,7 +50,7 @@ suite("Integration Test Suite: Subroutines resolving", () => {
     const position = completions.items.findIndex((ci) => ci.label === "SUB1");
     await helper.executeCommandMultipleTimes("selectNextSuggestion", position);
     await vscode.commands.executeCommand("acceptSelectedSuggestion");
-    await helper.sleep(1000);
+    await helper.sleep(500);
     const line = editor.document.lineAt(23).text.trim();
     assert.strictEqual(line, "CALL 'SUB1'.");
   });
