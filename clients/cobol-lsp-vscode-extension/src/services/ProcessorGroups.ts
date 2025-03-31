035d9d76e318dcf5db4563570dec8a912cc270dc
Kutlu <kutlu.ozel@broadcom.com>
2025-03-31 12:58:22 +0200

    add dialects

--- clients/cobol-lsp-vscode-extension/src/services/ProcessorGroups.ts
+++ clients/cobol-lsp-vscode-extension/src/services/ProcessorGroups.ts
@@ -55,6 +55,7 @@ export async function loadProcessorGroupCopybookPaths(
 export async function loadProcessorGroupCopybookPathsConfig(
   item: { scopeUri: string },
   configObject: string[],
+  dialect?: string,
 ): Promise<
   (string | ZoweDatasetConfigModel | ZoweUssConfigModel | EndevorConfigModel)[]
 > {
@@ -68,6 +69,7 @@ export async function loadProcessorGroupCopybookPathsConfig(
       item.scopeUri,
       "libs",
       [] as string[],
+      dialect,
     )),
     ...configObject,
   ];
