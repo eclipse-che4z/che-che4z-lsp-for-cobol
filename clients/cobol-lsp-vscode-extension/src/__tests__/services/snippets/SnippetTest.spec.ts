87c270511382ec8e368df8f2b6a541ac2ff685f4
Michael Messer <michael.messer@broadcom.com>
2025-04-25 09:29:44 -0400

    Ran Prettier, adjusted snippet counts in tests

--- clients/cobol-lsp-vscode-extension/src/__tests__/services/snippets/SnippetTest.spec.ts
+++ clients/cobol-lsp-vscode-extension/src/__tests__/services/snippets/SnippetTest.spec.ts
@@ -78,7 +78,7 @@ describe("Test CompletionProvider", () => {
           context,
         )
       ).length,
-    ).toBe(383);
+    ).toBe(389);
   });
   test("Suggest all IDMS Snippets", async () => {
     const doc = {
@@ -104,7 +104,7 @@ describe("Test CompletionProvider", () => {
           context,
         )
       ).length,
-    ).toBe(314);
+    ).toBe(320);
   });
 
   test("Suggest all Cobol only Snippets", async () => {
@@ -131,7 +131,7 @@ describe("Test CompletionProvider", () => {
           context,
         )
       ).length,
-    ).toBe(304);
+    ).toBe(310);
   });
 
   test("Suggest Snippets when both IDMS and DaCo is set", async () => {
@@ -158,7 +158,7 @@ describe("Test CompletionProvider", () => {
           context,
         )
       ).length,
-    ).toBe(393);
+    ).toBe(399);
   });
 
   test(" Test number of suggestions for COPY when no dialect is selected", async () => {
