6a8496a87e11564f5294132f3570a9e01bb43490
Kutlu <kutlu.ozel@broadcom.com>
2025-05-15 14:47:42 +0200

    return

--- server/engine/src/main/java/org/eclipse/lsp/cobol/implicitDialects/cics/utility/CICSReturnOptionsCheckUtility.java
+++ server/engine/src/main/java/org/eclipse/lsp/cobol/implicitDialects/cics/utility/CICSReturnOptionsCheckUtility.java
@@ -15,6 +15,7 @@
 package org.eclipse.lsp.cobol.implicitDialects.cics.utility;
 
 import static org.eclipse.lsp.cobol.implicitDialects.cics.CICSParser.*;
+import static org.eclipse.lsp.cobol.implicitDialects.cics.utility.CICSOptionsCheckUtility.noLengthOptionsEnabled;
 
 import java.util.*;
 import org.antlr.v4.runtime.ParserRuleContext;
@@ -71,5 +72,9 @@ public class CICSReturnOptionsCheckUtility extends CICSOptionsCheckBaseUtility {
     checkPrerequisiteIsMet(ctx.COMMAREA(), ctx.LENGTH(), ctx, "LENGTH without COMMAREA");
 
     checkPrerequisiteIsMet(ctx.INPUTMSG(), ctx.INPUTMSGLEN(), ctx, "INPUTMSGLEN without INPUTMSG");
+    if (noLengthOptionsEnabled) {
+      if (!ctx.COMMAREA().isEmpty()) checkHasMandatoryOptions(ctx.LENGTH(), ctx, "LENGTH");
+      if (!ctx.INPUTMSG().isEmpty()) checkHasMandatoryOptions(ctx.LENGTH(), ctx, "INPUTMSGLEN");
+    }
   }
 }
--- server/engine/src/test/java/org/eclipse/lsp/cobol/usecases/TestCicsReturnStatement.java
+++ server/engine/src/test/java/org/eclipse/lsp/cobol/usecases/TestCicsReturnStatement.java
@@ -16,6 +16,8 @@
 package org.eclipse.lsp.cobol.usecases;
 
 import java.util.*;
+
+import com.google.common.collect.ImmutableMap;
 import org.eclipse.lsp.cobol.common.error.ErrorSource;
 import org.eclipse.lsp.cobol.usecases.common.CICSTestUtils;
 import org.eclipse.lsp4j.Diagnostic;
@@ -43,6 +45,9 @@ public class TestCicsReturnStatement {
   private static final String RETURN_INVALID_3 =
       "RETURN TRANSID({$varOne}) COMMAREA({$varOne}) LENGTH({$varOne}) IMMEDIATE"
           + " INPUTMSG({$varOne}) INPUTMSGLEN({$varOne}) {ENDACTIVITY|errorOne}";
+  private static final String RETURN_TRANS_NOLENGTH_INVALID =
+      "RETURN {_TRANSID({$varOne}) COMMAREA({$varOne}) IMMEDIATE"
+          + " INPUTMSG({$varOne})|error|errorTwo_}";
 
   // Test Functions
   @Test
@@ -89,5 +94,24 @@ public class TestCicsReturnStatement {
             DiagnosticSeverity.Error,
             ErrorSource.PARSING.getText()));
     CICSTestUtils.errorTest(RETURN_INVALID_3, expectedDiagnostics);
+  }
+
+  @Test
+  void testCicsReturnNoLengthInvalid() {
+    Map<String, Diagnostic> expectedDiagnostic =
+        ImmutableMap.of(
+            "error",
+            new Diagnostic(
+                new Range(),
+                "Missing required option: INPUTMSGLEN",
+                DiagnosticSeverity.Error,
+                ErrorSource.PARSING.getText()),
+            "errorTwo",
+            new Diagnostic(
+                new Range(),
+                "Missing required option: LENGTH",
+                DiagnosticSeverity.Error,
+                ErrorSource.PARSING.getText()));
+    CICSTestUtils.errorTest(RETURN_TRANS_NOLENGTH_INVALID, expectedDiagnostic, "NOLENGTH");
   }
 }
