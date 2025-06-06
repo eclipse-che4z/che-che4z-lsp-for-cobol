ddde06f3d6eb236430d70cff94ff13e1f0584016
slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
2025-06-06 12:31:49 +0200

    fix: Close all editors efficiently

--- clients/cobol-lsp-vscode-extension/src/test/suite/testHelper.ts
+++ clients/cobol-lsp-vscode-extension/src/test/suite/testHelper.ts
@@ -103,12 +103,8 @@ export async function closeActiveEditor() {
 }
 
 export async function closeAllEditors() {
-  let editor = vscode.window.activeTextEditor;
-  while (editor?.document) {
-    // simulate didClose by changing the languageID
-    await closeActiveEditor();
-    editor = vscode.window.activeTextEditor;
-  }
+  await vscode.commands.executeCommand("workbench.action.files.revert");
+  await vscode.commands.executeCommand("workbench.action.closeAllEditors");
 }
 
 export function moveCursor(
