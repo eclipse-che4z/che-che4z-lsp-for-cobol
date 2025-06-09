39f3dce10dc586d95c7be0c0b2ddb2129cf473b6
Michael Messer <michael.messer@broadcom.com>
2025-06-09 09:44:07 -0400

    Removed unnecessary snippets
    Removed:
    - Both METHOD-ID ones
    - PASSWORD
    - POSITION

--- clients/cobol-lsp-vscode-extension/src/__tests__/services/snippets/SnippetTest.spec.ts
+++ clients/cobol-lsp-vscode-extension/src/__tests__/services/snippets/SnippetTest.spec.ts
@@ -78,7 +78,7 @@ describe("Test CompletionProvider", () => {
           context,
         )
       ).length,
-    ).toBe(511);
+    ).toBe(507);
   });
   test("Suggest all IDMS Snippets", async () => {
     const doc = {
@@ -104,7 +104,7 @@ describe("Test CompletionProvider", () => {
           context,
         )
       ).length,
-    ).toBe(442);
+    ).toBe(438);
   });
 
   test("Suggest all Cobol only Snippets", async () => {
@@ -131,7 +131,7 @@ describe("Test CompletionProvider", () => {
           context,
         )
       ).length,
-    ).toBe(432);
+    ).toBe(428);
   });
 
   test("Suggest Snippets when both IDMS and DaCo is set", async () => {
@@ -158,7 +158,7 @@ describe("Test CompletionProvider", () => {
           context,
         )
       ).length,
-    ).toBe(521);
+    ).toBe(517);
   });
 
   test(" Test number of suggestions for COPY when no dialect is selected", async () => {
