49a9af869d7c5b671b3c9551d3bd25b975f192c0
Michael Messer <michael.messer@broadcom.com>
2025-06-10 07:49:36 -0400

    Removed QUEUE

--- clients/cobol-lsp-vscode-extension/src/__tests__/services/snippets/SnippetTest.spec.ts
+++ clients/cobol-lsp-vscode-extension/src/__tests__/services/snippets/SnippetTest.spec.ts
@@ -78,7 +78,7 @@ describe("Test CompletionProvider", () => {
           context,
         )
       ).length,
-    ).toBe(506);
+    ).toBe(505);
   });
   test("Suggest all IDMS Snippets", async () => {
     const doc = {
@@ -104,7 +104,7 @@ describe("Test CompletionProvider", () => {
           context,
         )
       ).length,
-    ).toBe(437);
+    ).toBe(436);
   });
 
   test("Suggest all Cobol only Snippets", async () => {
@@ -131,7 +131,7 @@ describe("Test CompletionProvider", () => {
           context,
         )
       ).length,
-    ).toBe(427);
+    ).toBe(426);
   });
 
   test("Suggest Snippets when both IDMS and DaCo is set", async () => {
@@ -158,7 +158,7 @@ describe("Test CompletionProvider", () => {
           context,
         )
       ).length,
-    ).toBe(516);
+    ).toBe(515);
   });
 
   test(" Test number of suggestions for COPY when no dialect is selected", async () => {
