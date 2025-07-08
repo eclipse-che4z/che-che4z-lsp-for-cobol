7aa2f50e8204ca52789257cf066798110b099946
Kutlu <kutlu.ozel@broadcom.com>
2025-07-08 16:46:17 +0200

    revert check for map has literal suboperand & fix condition

--- server/engine/src/test/java/org/eclipse/lsp/cobol/usecases/TestCicsReceive.java
+++ server/engine/src/test/java/org/eclipse/lsp/cobol/usecases/TestCicsReceive.java
@@ -344,6 +344,21 @@ public class TestCicsReceive {
   }
 
   @Test
+  void testReceiveInvalidTwo() {
+    UseCaseEngine.runTest(
+        getTestString(RECEIVE_INVALID_TWO),
+        ImmutableList.of(),
+        ImmutableMap.of(
+            "errorOne",
+            new Diagnostic(
+                new Range(),
+                "Exactly one option required, none provided: INTO or SET when specifying MAP param without literal",
+                DiagnosticSeverity.Error,
+                ErrorSource.PARSING.getText())),
+        ImmutableList.of());
+  }
+
+  @Test
   void testReceiveInvalidThree() {
     UseCaseEngine.runTest(
         getTestString(RECEIVE_INVALID_THREE),
@@ -359,6 +374,21 @@ public class TestCicsReceive {
             new Diagnostic(
                 new Range(),
                 "Exactly one option required, options are mutually exclusive: TERMINAL or FROM",
+                DiagnosticSeverity.Error,
+                ErrorSource.PARSING.getText())),
+        ImmutableList.of());
+  }
+
+  @Test
+  void testReceiveInvalidFour() {
+    UseCaseEngine.runTest(
+        getTestString(RECEIVE_INVALID_FOUR),
+        ImmutableList.of(),
+        ImmutableMap.of(
+            "errorOne",
+            new Diagnostic(
+                new Range(),
+                "Exactly one option required, none provided: INTO or SET when specifying MAP param without literal",
                 DiagnosticSeverity.Error,
                 ErrorSource.PARSING.getText())),
         ImmutableList.of());
