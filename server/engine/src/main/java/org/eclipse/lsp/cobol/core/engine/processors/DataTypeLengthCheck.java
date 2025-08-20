4a3462364e29bdb6c35d7f293c8a0fa3f3fdca41
Ilidio Lopes <ilidio.depinalopes@broadcom.com>
2025-08-20 14:57:18 +0200

    fix decimal (P)

--- server/engine/src/main/java/org/eclipse/lsp/cobol/core/engine/processors/DataTypeLengthCheck.java
+++ server/engine/src/main/java/org/eclipse/lsp/cobol/core/engine/processors/DataTypeLengthCheck.java
@@ -255,22 +255,19 @@ public class DataTypeLengthCheck implements Processor<VariableWithLevelNode> {
   private boolean checkSimpleNumericWithDecimal(
       VariableWithLevelNode node, String pictureClause, ProcessingContext context) {
 
-    if (!pictureClause.matches("(?i)S?[09]*(V[09]*)?")) {
+    if (pictureClause.matches("(?i)([0]*9[09]*P[09]+)")) {
+      showError(context, node, "dataTypeLengthCheck.invalidPictureString", "", 0);
+      return true;
+    }
+    if (!pictureClause.matches("(?i)S?[09]*(V[09]*)?")
+        && !pictureClause.matches("(?i)([0]*P[09]*|[09]*P)")) {
       return false;
     }
 
     String numericPart =
         pictureClause.toUpperCase().startsWith("S") ? pictureClause.substring(1) : pictureClause;
 
-    String[] parts = numericPart.replace("0", "").split("V");
-    if (parts.length == 0) {
-      return false;
-    }
-
-    int totalLength = parts[0].length();
-    if (parts.length == 2) {
-      totalLength += parts[1].length();
-    }
+    int totalLength = numericPart.replace("0", "").replace("V", "").length();
 
     if (totalLength > MAX_NUMERIC_LENGTH) {
       showError(
