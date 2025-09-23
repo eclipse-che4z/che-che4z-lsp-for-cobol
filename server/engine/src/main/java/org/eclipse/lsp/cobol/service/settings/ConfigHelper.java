be35eac0b6c2985b7db80b235689a29d842fed30
Michael Messer <michael.messer@broadcom.com>
2025-09-23 08:25:27 -0400

    Ran formatter

--- server/engine/src/main/java/org/eclipse/lsp/cobol/service/settings/ConfigHelper.java
+++ server/engine/src/main/java/org/eclipse/lsp/cobol/service/settings/ConfigHelper.java
@@ -125,14 +125,14 @@ public class ConfigHelper {
    * @return Enabled if checked or Disabled otherwise, Enabled in the case of an invalid state
    */
   public SqlProcessing parseSQLProcessingEnabled(JsonElement options) {
-      if (!(options instanceof JsonNull)) {
-          if (options.isJsonPrimitive()) {
-              if (options.getAsJsonPrimitive().isBoolean()) {
-                  return options.getAsBoolean() ? SqlProcessing.ENABLED : SqlProcessing.DISABLED;
-              }
-          }
+    if (!(options instanceof JsonNull)) {
+      if (options.isJsonPrimitive()) {
+        if (options.getAsJsonPrimitive().isBoolean()) {
+          return options.getAsBoolean() ? SqlProcessing.ENABLED : SqlProcessing.DISABLED;
+        }
       }
-      return SqlProcessing.ENABLED;
+    }
+    return SqlProcessing.ENABLED;
   }
 
   /**
