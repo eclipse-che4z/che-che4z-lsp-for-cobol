2a5ff9092789667be51b734835b3ef6fb80b7d25
Leonid Baranov <leonid.baranov@broadcom.com>
2025-11-24 12:04:12 +0100

    Reorder tests

--- clients/cobol-lsp-vscode-extension/src/__tests__/services/JavaCheckTest.spec.ts
+++ clients/cobol-lsp-vscode-extension/src/__tests__/services/JavaCheckTest.spec.ts
@@ -19,29 +19,29 @@ jest.mock("../../services/reporter");
 describe("Checks Java version", () => {
   it("If Java version is supported", () => {
     expect(
+      JavaCheck.isJavaVersionSupported('openjdk version "1.8.0-internal"'),
+    ).toBeTruthy();
+    expect(
+      JavaCheck.isJavaVersionSupported('java version "1.8.0_181"'),
+    ).toBeTruthy();
+    expect(
       JavaCheck.isJavaVersionSupported('openjdk version "1.11.0-internal"'),
     ).toBeTruthy();
     expect(
       JavaCheck.isJavaVersionSupported('java version "1.11.0_181"'),
     ).toBeTruthy();
     expect(
-      JavaCheck.isJavaVersionSupported('openjdk version "11.0.1" 2018-10-16'),
+      JavaCheck.isJavaVersionSupported('java version "10.0.1" 2018-04-17'),
     ).toBeTruthy();
-    expect(
-      JavaCheck.isJavaVersionSupported('openjdk version "12.0.1" 2018-10-16'),
-    ).toBeTruthy();
+    expect(JavaCheck.isJavaVersionSupported('java version "9"')).toBeTruthy();
     expect(
       JavaCheck.isJavaVersionSupported('java version "1.8.0_131"'),
     ).toBeTruthy();
     expect(
-      JavaCheck.isJavaVersionSupported('openjdk version "1.8.0-internal"'),
+      JavaCheck.isJavaVersionSupported('openjdk version "11.0.1" 2018-10-16'),
     ).toBeTruthy();
     expect(
-      JavaCheck.isJavaVersionSupported('java version "1.8.0_181"'),
-    ).toBeTruthy();
-    expect(JavaCheck.isJavaVersionSupported('java version "9"')).toBeTruthy();
-    expect(
-      JavaCheck.isJavaVersionSupported('java version "10.0.1" 2018-04-17'),
+      JavaCheck.isJavaVersionSupported('openjdk version "12.0.1" 2018-10-16'),
     ).toBeTruthy();
     expect(JavaCheck.isJavaVersionSupported("java 11 2018-09-25")).toBeTruthy();
     expect(JavaCheck.isJavaVersionSupported("java 12 2019-03-19")).toBeTruthy();
