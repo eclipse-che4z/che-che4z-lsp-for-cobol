218d1d500ede8fc7fcccb439da5ce9e852cda916
hb891057 <hizir-cagin.bektas@broadcom.com>
2025-12-05 16:05:28 +0100

    improve picture clause retrival

--- server/engine/src/main/java/org/eclipse/lsp/cobol/core/visitor/VisitorHelper.java
+++ server/engine/src/main/java/org/eclipse/lsp/cobol/core/visitor/VisitorHelper.java
@@ -105,7 +105,9 @@ public class VisitorHelper {
       List<org.eclipse.lsp.cobol.core.CobolParser.DataPictureClauseContext> clauses) {
     List<String> list = new ArrayList<>(clauses.size());
     for (CobolParser.DataPictureClauseContext clause : clauses) {
-      list.add(clause.pictureString().getText());
+      if (clause.exception == null) {
+        list.add(clause.pictureString().getText());
+      }
     }
     return list;
   }
