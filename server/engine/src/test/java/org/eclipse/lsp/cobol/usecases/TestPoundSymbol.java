dbb1bc2ab7b11ab3df39f0d80568aac6f185cad9
hb891057 <hizir-cagin.bektas@broadcom.com>
2025-12-08 13:44:16 +0100

    test case added

---
+++ server/engine/src/test/java/org/eclipse/lsp/cobol/usecases/TestPoundSymbol.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2021 Broadcom.
+ * The term "Broadcom" refers to Broadcom Inc. and/or its subsidiaries.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Broadcom, Inc. - initial API and implementation
+ *
+ */
+package org.eclipse.lsp.cobol.usecases;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import org.eclipse.lsp.cobol.common.error.ErrorSource;
+import org.eclipse.lsp.cobol.test.engine.UseCaseEngine;
+import org.eclipse.lsp4j.Diagnostic;
+import org.eclipse.lsp4j.DiagnosticSeverity;
+import org.eclipse.lsp4j.Range;
+import org.junit.jupiter.api.Test;
+
+/**
+ * This test verifies that unknown currency symbols trigger diagnostic messages rather than causing
+ * a null pointer exception.
+ */
+class TestPoundSymbol {
+
+  private static final String TEXT =
+      "       IDENTIFICATION DIVISION.\n"
+          + "       PROGRAM-ID. TEST1.\n"
+          + "       DATA DIVISION.\n"
+          + "       WORKING-STORAGE SECTION.\n"
+          + "       01  {$*VRB|1} PIC {£|2}(10)9.99-.\n"
+          + "       PROCEDURE DIVISION.\n"
+          + "           STOP RUN.";
+
+  private static final String MESSAGE1 =
+      "A \"PICTURE\" or \"USAGE INDEX\" clause was not found for elementary item VRB";
+  private static final String MESSAGE2 = "Syntax error on '£'";
+
+  @Test
+  void test() {
+    UseCaseEngine.runTest(
+        TEXT,
+        ImmutableList.of(),
+        ImmutableMap.of(
+            "1",
+            new Diagnostic(
+                new Range(), MESSAGE1, DiagnosticSeverity.Error, ErrorSource.PARSING.getText()),
+            "2",
+            new Diagnostic(
+                new Range(), MESSAGE2, DiagnosticSeverity.Error, ErrorSource.PARSING.getText())));
+  }
+}
