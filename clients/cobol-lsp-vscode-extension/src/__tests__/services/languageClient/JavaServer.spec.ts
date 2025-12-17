7cb4bf4261a0eedacfa7d9035cf28f4a7fa8b7a9
Vit Gottwald <vit.gottwald@broadcom.com>
2025-12-17 13:10:31 +0100

    wip: add windows specific test

--- clients/cobol-lsp-vscode-extension/src/__tests__/services/languageClient/JavaServer.spec.ts
+++ clients/cobol-lsp-vscode-extension/src/__tests__/services/languageClient/JavaServer.spec.ts
@@ -7,31 +7,64 @@ jest.mock("../../../services/JavaCheck", () => ({
   getJavaVersion: jest.fn().mockResolvedValue(17),
 }));
 
-test("Starts java language server", async () => {
-  const javaServer: JavaServer = {
-    kind: "JAVA",
-    command: "/test/bin/java",
-    dialects: vscode.Uri.parse("file:///test/dialectsFolder"),
-    jar: vscode.Uri.parse("file:///test/server/server.jar"),
-  };
+const windows = process.platform === "win32";
 
-  await startJavaServer(javaServer, {});
-  expect(LanguageClient).toHaveBeenCalledTimes(1);
-  expect(LanguageClient).toHaveBeenCalledWith(
-    "cobol",
-    "COBOL Language Support",
-    {
-      command: "/test/bin/java",
-      args: [
-        "-Dline.separator=\r\n",
-        "-Ddialect.path=/test/dialectsFolder",
-        "-Xmx768M",
-        "-jar",
-        "/test/server/server.jar",
-        "pipeEnabled",
-      ],
-      options: { detached: false },
-    },
-    { errorHandler: expect.any(Function) as any },
-  );
-});
+if (windows) {
+  test("Starts java language server", async () => {
+    const javaServer: JavaServer = {
+      kind: "JAVA",
+      command: "C:\\t e s t\\bin\\java",
+      dialects: vscode.Uri.file("C:\\t e s t\\dialects"),
+      jar: vscode.Uri.file("C:\\t e s t\\server\\server.jar"),
+    };
+
+    await startJavaServer(javaServer, {});
+    expect(LanguageClient).toHaveBeenCalledTimes(1);
+    expect(LanguageClient).toHaveBeenCalledWith(
+      "cobol",
+      "COBOL Language Support",
+      {
+        command: "C:\\t e s t\\bin\\java",
+        args: [
+          "-Dline.separator=\r\n",
+          "-Ddialect.path=C:\\t e s t\\dialects",
+          "-Xmx768M",
+          "-jar",
+          "C:\\t e s t\\server\\server.jar",
+          "pipeEnabled",
+        ],
+        options: { detached: false },
+      },
+      { errorHandler: expect.any(Function) as any },
+    );
+  });
+} else {
+  test("Starts java language server", async () => {
+    const javaServer: JavaServer = {
+      kind: "JAVA",
+      command: "/t e s t/bin/java",
+      dialects: vscode.Uri.file("/t e s t/dialectsFolder"),
+      jar: vscode.Uri.file("/t e s t/server/server.jar"),
+    };
+
+    await startJavaServer(javaServer, {});
+    expect(LanguageClient).toHaveBeenCalledTimes(1);
+    expect(LanguageClient).toHaveBeenCalledWith(
+      "cobol",
+      "COBOL Language Support",
+      {
+        command: "/t e s t/bin/java",
+        args: [
+          "-Dline.separator=\r\n",
+          "-Ddialect.path=/t e s t/dialectsFolder",
+          "-Xmx768M",
+          "-jar",
+          "/t e s t/server/server.jar",
+          "pipeEnabled",
+        ],
+        options: { detached: false },
+      },
+      { errorHandler: expect.any(Function) as any },
+    );
+  });
+}
