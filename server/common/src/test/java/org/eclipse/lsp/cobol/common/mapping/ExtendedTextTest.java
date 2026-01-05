0e326bf637759abd213ad7e4e796acc3977ec689
Leonid Baranov <leonid.baranov@broadcom.com>
2026-01-05 13:59:52 +0100

    Update test

--- server/common/src/test/java/org/eclipse/lsp/cobol/common/mapping/ExtendedTextTest.java
+++ server/common/src/test/java/org/eclipse/lsp/cobol/common/mapping/ExtendedTextTest.java
@@ -690,6 +690,11 @@ class ExtendedTextTest {
     Range statementRange = new Range(new Position(0, 0), new Position(0, 9));
     extendedText.replace(range, statementRange, statementMap, "STATEMENT {A}\nSTATEMENT {B}");
 
+    Location location =
+        extendedText.mapLocation(new Range(new Position(1, 10), new Position(1, 11)));
+
     assertEquals("STATEMENT A\n" + "STATEMENT B", extendedText.toString());
+    assertEquals(
+        new Location("uri", new Range(new Position(0, 11), new Position(0, 12))), location);
   }
 }
