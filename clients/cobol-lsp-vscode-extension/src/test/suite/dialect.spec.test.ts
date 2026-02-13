b211b1d08863fdb005d60e661de373e0c3707b62
Leonid Baranov <leonid.baranov@broadcom.com>
2026-02-13 14:22:56 +0100

    Update integration test to handle copybook diagnostics

--- clients/cobol-lsp-vscode-extension/src/test/suite/dialect.spec.test.ts
+++ clients/cobol-lsp-vscode-extension/src/test/suite/dialect.spec.test.ts
@@ -35,29 +35,87 @@ suite("Integration Test Suite: Dialect specific tests", function () {
 
     const diagnostics = await helper.waitForDiagnostics(editor.document.uri);
 
-    console.warn("Diagnostics:", diagnostics);
-    assert.strictEqual(diagnostics.length, 2);
+    helper.printAllDiagnostics(diagnostics);
+    assert.strictEqual(diagnostics.length, 4);
 
-    const d0 = diagnostics[0];
-    assert.strictEqual(d0.severity, vscode.DiagnosticSeverity.Error);
-    assert.strictEqual(
+    checkDiagnostic(
+      diagnostics,
+      "Errors inside the copybook",
+      24,
+      11,
+      24,
+      33,
+      "COBOL Language Support (copybook)",
+    );
+
+    checkDiagnostic(
+      diagnostics,
       "Variable V2 does not exist in structure G2",
-      d0.message,
+      25,
+      28,
+      25,
+      30,
+      "COBOL Language Support (parsing)",
     );
-    assert.strictEqual(d0.range.start.line, 25);
-    assert.strictEqual(d0.range.start.character, 28);
-    assert.strictEqual(d0.range.end.line, 25);
-    assert.strictEqual(d0.range.end.character, 30);
 
-    const d1 = diagnostics[1];
-    assert.strictEqual(d1.severity, vscode.DiagnosticSeverity.Error);
-    assert.strictEqual(
+    checkDiagnostic(
+      diagnostics,
       "The following paragraph is not defined: P1",
-      d1.message,
+      25,
+      48,
+      25,
+      50,
+      "COBOL Language Support (parsing)",
     );
-    assert.strictEqual(d1.range.start.line, 25);
-    assert.strictEqual(d1.range.start.character, 48);
-    assert.strictEqual(d1.range.end.line, 25);
-    assert.strictEqual(d1.range.end.character, 50);
+
+    checkDiagnostic(
+      diagnostics,
+      "Sample dialect diagnostic (MAKEDIAG)",
+      26,
+      11,
+      26,
+      19,
+      "COBOL Language Support (dialect)",
+    );
+
+    const extCopybookSrcPath = path.join("copy-sample", "COPY1.cpy");
+    const copybookEditor = await helper.showDocument(extCopybookSrcPath);
+    const copybookDiagnostics = await helper.waitForDiagnostics(
+      copybookEditor.document.uri,
+    );
+    helper.printAllDiagnostics(copybookDiagnostics);
+
+    assert.strictEqual(copybookDiagnostics.length, 1);
+
+    checkDiagnostic(
+      copybookDiagnostics,
+      "Sample dialect diagnostic (MAKEDIAG)",
+      1,
+      11,
+      1,
+      19,
+      "COBOL Language Support (dialect)",
+    );
   });
 });
+
+function checkDiagnostic(
+  diagnostics: vscode.Diagnostic[],
+  message: string,
+  startLine: number,
+  startCharacter: number,
+  endLine: number,
+  endCharacter: number,
+  source: string,
+) {
+  const diagnostic = diagnostics.find(
+    (diag) =>
+      diag.message === message &&
+      diag.source === source &&
+      diag.range.start.line === startLine &&
+      diag.range.start.character === startCharacter &&
+      diag.range.end.line === endLine &&
+      diag.range.end.character === endCharacter,
+  );
+  assert.ok(diagnostic, `Diagnostic with message "${message}" not found.`);
+}
--- tests/test_files/project/copy-sample/COPY1.cpy
+++ tests/test_files/project/copy-sample/COPY1.cpy
@@ -1,1 +1,2 @@
            ALT STATE P2 OF S2.
+           MAKEDIAG.
