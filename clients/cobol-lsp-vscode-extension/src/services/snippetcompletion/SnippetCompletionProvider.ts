e488ae1b8ba218afc55f8ad7284bd1e6532d3830
Hizir <86234686+hizir-bc@users.noreply.github.com>
2026-03-10 12:59:47 +0100

    Update clients/cobol-lsp-vscode-extension/src/services/snippetcompletion/SnippetCompletionProvider.ts
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>

--- clients/cobol-lsp-vscode-extension/src/services/snippetcompletion/SnippetCompletionProvider.ts
+++ clients/cobol-lsp-vscode-extension/src/services/snippetcompletion/SnippetCompletionProvider.ts
@@ -129,8 +129,8 @@ function createCompletionItem(
  * Return map of all predefined snippets and snippets
  * for all active dialects from JSON files provided by dialects
  */
-async function loadSnippets(entries = {}): Promise<Map<string, Snippet>> {
-  const loadedSnippets: Map<string, Snippet> = new Map(Object.entries(entries));
+async function loadSnippets(entries: Record<string, Snippet> = {}): Promise<Map<string, Snippet>> {
+  const loadedSnippets = new Map(Object.entries(entries));
   const activeDialects = DialectRegistry.getActiveDialects();
 
   await Promise.all(
