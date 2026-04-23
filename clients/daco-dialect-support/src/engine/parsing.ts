98342ada7b2dc560f4e50b68a226a5ecac5fbb9a
Leonid Baranov <leonid.baranov@broadcom.com>
2026-04-23 11:07:56 +0200

    Update clients/daco-dialect-support/src/engine/parsing.ts
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>

--- clients/daco-dialect-support/src/engine/parsing.ts
+++ clients/daco-dialect-support/src/engine/parsing.ts
@@ -92,6 +92,10 @@ export class CollectingErrorListener extends BaseErrorListener {
   }
 }
 
+function concatResults<T>(r1: T[] | null, r2: T[] | null): T[] {
+    return [...(r1 ?? []), ...(r2 ?? [])];
+}
+
 export class CopybookVisitor extends CopybookParserVisitor<
   CopybookDescriptor[]
 > {
