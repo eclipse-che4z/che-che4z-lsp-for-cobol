a5896da39e4f21e75991b9db36f4c9487656b352
Leonid Baranov <leonid.baranov@broadcom.com>
2026-04-23 12:17:48 +0200

    Insert MAID copybook (very simple case) (#2989)
    * Initial parser
    
    * Draft
    
    * Draft
    
    * Add copybook content parser and lexer
    
    * Fix vscode dependencies
    
    * Minor
    
    * Fix
    
    * Fix CI/CD
    
    * Remove non needed test
    
    * Remove DaCo from main extension integration tests
    
    * Minor
    
    * Draft integration tests
    
    * Revert esm workaround
    
    * Prettier
    
    * Update clients/daco-dialect-support/src/test/suite/testHelper.ts
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
    
    * Update clients/daco-dialect-support/src/test/suite/testHelper.ts
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
    
    * Update clients/daco-dialect-support/src/test/suite/testHelper.ts
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
    
    * Update clients/daco-dialect-support/src/test/suite/testHelper.ts
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
    
    * Address comments
    
    * Update clients/daco-dialect-support/src/antlr/CopybookParser.g4
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
    
    * Update clients/daco-dialect-support/src/antlr/CopybookParser.g4
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
    
    * Update clients/daco-dialect-support/src/antlr/CopybookParser.g4
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
    
    * Update clients/daco-dialect-support/src/engine/parsing.ts
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
    
    * Address comments
    
    * Update clients/daco-dialect-support/src/engine/parsing.ts
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
    
    * Update clients/daco-dialect-support/src/engine/parsing.ts
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
    
    * Address comments
    
    * Update clients/daco-dialect-support/src/engine/parsing.ts
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
    
    * Address comments
    
    * Address comments
    
    * Address comments
    
    * Address comments
    
    * Address comments
    
    * Update clients/daco-dialect-support/src/engine/parsing.ts
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
    
    * Address comments
    
    * Update clients/daco-dialect-support/src/engine/parsing.ts
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
    
    * Update clients/daco-dialect-support/src/engine/parsing.ts
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
    
    * Update clients/daco-dialect-support/src/engine/parsing.ts
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
    
    * Update clients/daco-dialect-support/src/engine/parsing.ts
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
    
    * Prettier
    
    ---------
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>

---
+++ clients/daco-dialect-support/src/test/unit/engine/__mocks__/vscode.ts
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2026 Broadcom.
+ * The term "Broadcom" refers to Broadcom Inc. and/or its subsidiaries.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Broadcom, Inc. - initial API and implementation
+ */
+import { URI, Utils } from "vscode-uri";
+
+export const Uri = URI;
+Object.assign(Uri, Utils);
+
+export class Range {
+  public start: Position;
+  public end: Position;
+
+  constructor(
+    par0: number | Position,
+    par1: number | Position,
+    endLine?: number,
+    endCharacter?: number,
+  ) {
+    if (endLine !== undefined && endCharacter !== undefined) {
+      this.start = new Position(par0 as number, par1 as number);
+      this.end = new Position(endLine, endCharacter);
+    } else {
+      this.start = par0 as Position;
+      this.end = par1 as Position;
+    }
+  }
+}
+
+export class Position {
+  constructor(public line: number, public character: number) {}
+}
+
+export enum DiagnosticSeverity {
+  Error = 0,
+  Warning = 1,
+  Information = 2,
+  Hint = 3,
+}
