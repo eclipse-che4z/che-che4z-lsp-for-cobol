22f8b5b15b62d571986e645bc9f11e6c98286628
Leonid Baranov <leonid.baranov@broadcom.com>
2026-04-23 12:17:48 +0200

    Insert MAID copybook (very simple case) (#2989)
    * Initial parser
    
    * Draft
    
    * Draft
    
    * Add copybook content parser and lexer
    
    * Fix vscode dependencies
    
    * Minor
    
    * Fix
    
    * Fix CI/CD
    
    * Remove non needed test
    
    * Remove DaCo from main extension integration tests
    
    * Minor
    
    * Draft integration tests
    
    * Revert esm workaround
    
    * Prettier
    
    * Update clients/daco-dialect-support/src/test/suite/testHelper.ts
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
    
    * Update clients/daco-dialect-support/src/test/suite/testHelper.ts
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
    
    * Update clients/daco-dialect-support/src/test/suite/testHelper.ts
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
    
    * Update clients/daco-dialect-support/src/test/suite/testHelper.ts
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
    
    * Address comments
    
    * Update clients/daco-dialect-support/src/antlr/CopybookParser.g4
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
    
    * Update clients/daco-dialect-support/src/antlr/CopybookParser.g4
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
    
    * Update clients/daco-dialect-support/src/antlr/CopybookParser.g4
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
    
    * Update clients/daco-dialect-support/src/engine/parsing.ts
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
    
    * Address comments
    
    * Update clients/daco-dialect-support/src/engine/parsing.ts
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
    
    * Update clients/daco-dialect-support/src/engine/parsing.ts
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
    
    * Address comments
    
    * Update clients/daco-dialect-support/src/engine/parsing.ts
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
    
    * Address comments
    
    * Address comments
    
    * Address comments
    
    * Address comments
    
    * Address comments
    
    * Update clients/daco-dialect-support/src/engine/parsing.ts
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
    
    * Address comments
    
    * Update clients/daco-dialect-support/src/engine/parsing.ts
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
    
    * Update clients/daco-dialect-support/src/engine/parsing.ts
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
    
    * Update clients/daco-dialect-support/src/engine/parsing.ts
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
    
    * Update clients/daco-dialect-support/src/engine/parsing.ts
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
    
    * Prettier
    
    ---------
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>

--- clients/daco-dialect-support/src/engine/preprocessor.ts
+++ clients/daco-dialect-support/src/engine/preprocessor.ts
@@ -14,14 +14,182 @@
 
 import * as vscode from "vscode";
 import { IDocumentProcessingContext } from "@code4z/cobol-dialect-api";
+import { CopybookParser } from "../generated/CopybookParser";
+import { CopybookLexer } from "../generated/CopybookLexer";
+import * as antlr from "antlr4ng";
+import {
+  CollectingErrorListener,
+  CopybookContentVisitor,
+  CopybookVisitor,
+  ParseError,
+  VariableDescriptor,
+} from "./parsing";
+import { VariableLexer } from "../generated/VariableLexer";
+import { VariableParser } from "../generated/VariableParser";
 
 export class DaCoPreprocessor {
+  private firstCopybookLevel: number = 0;
+
   public async execute(
     context: IDocumentProcessingContext,
-    programUri: vscode.Uri,
+    _programUri: vscode.Uri,
     text: string,
     outputChannel: vscode.OutputChannel,
   ) {
-    throw new Error("Preprocessor is not implemented yet");
+    const procMatch = new RegExp(/PROCEDURE\s+DIVISION\.?/i).exec(text);
+
+    const end =
+      procMatch?.index !== undefined && procMatch.index > 0
+        ? procMatch.index
+        : text.length;
+
+    const sliced = text.slice(0, end);
+    const charStream = antlr.CharStream.fromString(sliced);
+
+    const lexer = new CopybookLexer(charStream);
+    const tokenStream = new antlr.CommonTokenStream(lexer);
+    const parser = new CopybookParser(tokenStream);
+
+    tokenStream.fill();
+    for (const t of tokenStream.getTokens()) {
+      console.log(
+        JSON.stringify(t.text),
+        parser.vocabulary.getSymbolicName(t.type),
+      );
+    }
+
+    lexer.removeErrorListeners();
+    parser.removeErrorListeners();
+
+    const lexerErrors = new CollectingErrorListener();
+    const parserErrors = new CollectingErrorListener();
+
+    lexer.addErrorListener(lexerErrors);
+    parser.addErrorListener(parserErrors);
+
+    const tree = parser.startRule();
+    console.log(tree.toStringTree(parser));
+
+    const descriptors = new CopybookVisitor().visit(tree) || [];
+
+    this.addParsingErrors(context, [
+      ...lexerErrors.errors,
+      ...parserErrors.errors,
+    ]);
+
+    await Promise.all(
+      descriptors.map(async (descriptor) => {
+        outputChannel.appendLine(`Descriptor: ${JSON.stringify(descriptor)}`);
+        const copybookName =
+          descriptor.name + (descriptor.suffix ? `_${descriptor.suffix}` : "");
+
+        const copybook = await context.resolveCopybook(
+          copybookName,
+          descriptor.statementRange,
+          descriptor.nameRange,
+        );
+
+        if (copybook) {
+          outputChannel.appendLine(
+            `Resolved copybook '${copybookName}' at ${copybook.uri.toString()}`,
+          );
+          this.insertCopybookContent(
+            context,
+            copybook,
+            descriptor.level,
+            descriptor.suffix,
+          );
+        }
+      }),
+    );
+  }
+
+  private addParsingErrors(
+    context: IDocumentProcessingContext,
+    errors: ParseError[],
+  ) {
+    errors.forEach((error) => {
+      context.addDiagnostic({
+        severity: vscode.DiagnosticSeverity.Error,
+        message: error.message,
+        range: error.range,
+      });
+    });
+  }
+
+  private insertCopybookContent(
+    context: IDocumentProcessingContext,
+    copybook: {
+      context: IDocumentProcessingContext;
+      uri: vscode.Uri;
+      text: string;
+    },
+    copybookLevel: number,
+    layoutUsage?: string,
+  ) {
+    const charStream = antlr.CharStream.fromString(copybook.text);
+    const lexer = new VariableLexer(charStream);
+    const tokenStream = new antlr.CommonTokenStream(lexer);
+    const parser = new VariableParser(tokenStream);
+
+    lexer.removeErrorListeners();
+    parser.removeErrorListeners();
+
+    const lexerErrors = new CollectingErrorListener();
+    const parserErrors = new CollectingErrorListener();
+
+    lexer.addErrorListener(lexerErrors);
+    parser.addErrorListener(parserErrors);
+
+    const tree = parser.startRule();
+    console.log(tree.toStringTree(parser));
+
+    this.addParsingErrors(context, [
+      ...lexerErrors.errors,
+      ...parserErrors.errors,
+    ]);
+
+    const descriptors = new CopybookContentVisitor().visit(tree) || [];
+    descriptors.forEach((descriptor) => {
+      this.processVariableDescriptor(
+        copybook.context,
+        descriptor,
+        copybookLevel,
+        layoutUsage,
+      );
+    });
+  }
+
+  private processVariableDescriptor(
+    context: IDocumentProcessingContext,
+    descriptor: VariableDescriptor,
+    copybookLevel: number,
+    layoutUsage: string | undefined,
+  ) {
+    if (layoutUsage) {
+      const updatedName = this.updateVariableName(descriptor.name, layoutUsage);
+      context.replace(descriptor.nameRange, updatedName);
+    }
+
+    if (copybookLevel != descriptor.level) {
+      const updatedLevel = this.calculateLevel(copybookLevel, descriptor.level);
+      const updatedLevelStr = updatedLevel.toString().padStart(2, "0");
+      context.replace(descriptor.levelRange, updatedLevelStr);
+    }
+  }
+
+  private updateVariableName(name: string, suffix: string) {
+    return name;
+  }
+
+  private calculateLevel(copybookLevel: number, level: number): number {
+    if (copybookLevel != 0) {
+      if (this.firstCopybookLevel == 0) {
+        this.firstCopybookLevel = level;
+        return copybookLevel;
+      }
+      return level - this.firstCopybookLevel + copybookLevel;
+    }
+    return level;
   }
 }
