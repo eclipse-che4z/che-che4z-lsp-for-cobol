30f1437bedcf53bd2e533233ecee29ce5cd34786
Leonid Baranov <leonid.baranov@broadcom.com>
2026-05-13 15:32:30 +0200

    Insert DaCo copybook with WRK suffix (#3008)
    * DaCo copybook WRK suffix usage
    
    * Increase test coverage
    
    * Simplify logic
    
    * Update clients/daco-dialect-support/src/engine/parsing.ts
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
    
    * Prettier
    
    * Cleanup
    
    * Update DaCo according to new requirements
    
    * Update clients/daco-dialect-support/src/engine/parsing.ts
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
    
    * Fix race condition
    
    ---------
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>

---
+++ clients/daco-dialect-support/test_files/DaCo08.cbl
@@ -0,0 +1,17 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID.    TEST1.
+       ENVIRONMENT DIVISION.
+       IDMS-CONTROL SECTION.
+           PROTOCOL. MODE ABC.
+           IDMS-RECORDS MANUAL
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 PARENT-XLO.
+           05 COPY MAID DACO08-XBG WRK.
+       PROCEDURE DIVISION.
+            DISPLAY F91-XLO.
+            DISPLAY NOT_EXISTING.
+      *https://github.com/eclipse-che4z/che-che4z-lsp-for-cobol
+      */blob/40bec9e7654f1e5d8c086e188a170dc4205fdc1f/server
+      */dialect-daco/src/test/java/org/eclipse/lsp/cobol/dialects/daco
+      */usecases/TestCopyMaidWithWrkRedefines.java
