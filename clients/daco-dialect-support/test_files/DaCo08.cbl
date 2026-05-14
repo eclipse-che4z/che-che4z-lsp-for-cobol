6441c6b6b62ae589867ce790bbbb5656c0586ba3
Leonid Baranov <leonid.baranov@broadcom.com>
2026-05-14 12:55:36 +0200

    DaCo copybook: Cover REDEFINE use case (#3010)

--- clients/daco-dialect-support/test_files/DaCo08.cbl
+++ clients/daco-dialect-support/test_files/DaCo08.cbl
@@ -9,8 +9,8 @@
        01 PARENT-XLO.
            05 COPY MAID DACO08-XBG WRK.
        PROCEDURE DIVISION.
-            DISPLAY F91-XLO.
-            DISPLAY NOT_EXISTING.
+           DISPLAY F91-XLO.
+           DISPLAY NOT_EXISTING.
       *https://github.com/eclipse-che4z/che-che4z-lsp-for-cobol
       */blob/40bec9e7654f1e5d8c086e188a170dc4205fdc1f/server
       */dialect-daco/src/test/java/org/eclipse/lsp/cobol/dialects/daco
