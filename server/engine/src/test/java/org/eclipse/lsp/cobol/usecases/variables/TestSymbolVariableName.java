4a144406e5bbb440aeee419d45a7abdbf4b61a71
Michael Messer <michael.messer@broadcom.com>
2026-05-22 12:11:06 -0400

    Add date symbol variable name test

--- server/engine/src/test/java/org/eclipse/lsp/cobol/usecases/variables/TestSymbolVariableName.java
+++ server/engine/src/test/java/org/eclipse/lsp/cobol/usecases/variables/TestSymbolVariableName.java
@@ -72,6 +72,7 @@ public class TestSymbolVariableName {
         ImmutablePair.of("count", ""),
         ImmutablePair.of("currency", ""),
         ImmutablePair.of("data", ""),
+        ImmutablePair.of("date", ""),
         ImmutablePair.of("day", ""),
         ImmutablePair.of("dbcs", ""),
         ImmutablePair.of("exit", ""),
