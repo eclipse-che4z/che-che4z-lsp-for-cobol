cc5cd7e664df00c3d7cb64f633d826e84a5c3fe3
Leonid Baranov <leonid.baranov@broadcom.com>
2026-05-25 16:27:29 +0200

    Add OPEN PACKET and USE statements to the DaCo processing (#3029)

---
+++ clients/daco-dialect-support/test_files/DaCo24.cbl
@@ -0,0 +1,22 @@
+       IDENTIFICATION DIVISION. 
+       PROGRAM-ID. test1. 
+       ENVIRONMENT DIVISION.
+       IDMS-CONTROL SECTION.
+           PROTOCOL. MODE ABC.
+           IDMS-RECORDS MANUAL
+       DATA DIVISION. 
+       WORKING-STORAGE SECTION. 
+       01 WS-AREA. 
+          03 AREA-XW1. 
+            05 DET001-XW1. 
+              07 FILLER               PIC X(5)    VALUE 'REMBD'. 
+       PROCEDURE DIVISION. 
+           GET TASK 'TAKD'. 
+           GET TASK DET001-XW1.
+      * Negative tests     
+           GET TASK 'SDREE'. 
+           GET TASK SDFRE. 
+      *https://github.com/eclipse-che4z/che-che4z-lsp-for-cobol/blob
+      */40bec9e7654f1e5d8c086e188a170dc4205fdc1f/server/dialect-daco/src
+      */test/java/org/eclipse/lsp/cobol/dialects/daco/usecases
+      */TestDaCoGetTaskStatement.java
