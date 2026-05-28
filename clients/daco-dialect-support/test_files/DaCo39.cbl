db6effac76576b4e0ccd82e3a63adb4593378d1a
Leonid Baranov <leonid.baranov@broadcom.com>
2026-05-28 10:11:54 +0200

    Add table ROW statements to the DaCo processing (#3032)

---
+++ clients/daco-dialect-support/test_files/DaCo39.cbl
@@ -0,0 +1,28 @@
+       IDENTIFICATION DIVISION. 
+       PROGRAM-ID. test1. 
+       ENVIRONMENT DIVISION.
+       IDMS-CONTROL SECTION.
+           PROTOCOL. MODE ABC.
+           IDMS-RECORDS MANUAL
+       DATA DIVISION. 
+       WORKING-STORAGE SECTION. 
+       01 WS-AREA. 
+          03 AREA-XW1. 
+            05 TBLPRO-XL1. 
+              07 FILLER               PIC X(5)    VALUE 'REMBD'. 
+            05 TBFPRO-XL1. 
+              07 FILLER               PIC X(5)    VALUE 'REMBD'. 
+            05 DSAPRO-XL1. 
+              07 FILLER               PIC X(5)    VALUE 'REMBD'. 
+       PROCEDURE DIVISION. 
+           ROW INSERT TBLPRO-XL1 WITH DSAPRO-XL1. 
+           ROW INSERT TBFPRO-XL1 WITH DSAPRO-XL1. 
+           ROW INSERT TBFPRO-XL1.
+      * Negative tests
+           ROW INSERT DSAPRO-XL1 WITH DSAPRO-XL1. 
+           ROW INSERT GBR4 WITH DSAPRO-XL1. 
+           ROW INSERT TBFPRO-XL1 WITH GBR4. 
+      * https://github.com/eclipse-che4z/che-che4z-lsp-for-cobol/blob
+      */ec60b8b1d88da1ebf8a3fca8cc2efd68a572441b/server/dialect-daco/src/test
+      */java/org/eclipse/lsp/cobol/dialects/daco/usecases
+      */TestDaCoTableRowInsertStatement.java
