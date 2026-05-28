1c6f54fdadfa921d3fb4f0d70c903bbed6093b2a
Leonid Baranov <leonid.baranov@broadcom.com>
2026-05-28 10:11:54 +0200

    Add table ROW statements to the DaCo processing (#3032)

--- clients/daco-dialect-support/src/test/suite/misc.test.ts
+++ clients/daco-dialect-support/src/test/suite/misc.test.ts
@@ -876,4 +876,502 @@ suite("Miscellaneous statements Test Suite", function () {
       range(pos(35, 28), pos(35, 31)),
     );
   });
+
+  test("Process ROW ADD statement successfully", async () => {
+    const editor = await helper.showDocument("DaCo32.cbl");
+    const diagnostics = await helper.waitForDiagnostics(editor.document.uri);
+    helper.printAllDiagnostics(diagnostics);
+    assert.strictEqual(diagnostics.length, 4);
+
+    checkDiagnostic(
+      diagnostics,
+      "String must starts with TBL or TBF values",
+      range(pos(23, 19), pos(23, 29)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "String must starts with TBL or TBF values",
+      range(pos(24, 19), pos(24, 23)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "Variable GBR4 is not defined",
+      range(pos(24, 19), pos(24, 23)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "Variable GBR4 is not defined",
+      range(pos(25, 35), pos(25, 39)),
+    );
+  });
+
+  test("Process ROW ANY statement successfully", async () => {
+    const editor = await helper.showDocument("DaCo33.cbl");
+    const diagnostics = await helper.waitForDiagnostics(editor.document.uri);
+    helper.printAllDiagnostics(diagnostics);
+    assert.strictEqual(diagnostics.length, 2);
+
+    checkDiagnostic(
+      diagnostics,
+      "String must starts with TBL or TBF values",
+      range(pos(20, 19), pos(20, 29)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "Variable GBR4 is not defined",
+      range(pos(21, 36), pos(21, 40)),
+    );
+  });
+
+  test("Process ROW BUFFER statement successfully", async () => {
+    const editor = await helper.showDocument("DaCo34.cbl");
+    const diagnostics = await helper.waitForDiagnostics(editor.document.uri);
+    helper.printAllDiagnostics(diagnostics);
+    assert.strictEqual(diagnostics.length, 3);
+
+    checkDiagnostic(
+      diagnostics,
+      "String must starts with TBL or TBF values",
+      range(pos(17, 22), pos(17, 32)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "String must starts with TBL or TBF values",
+      range(pos(18, 22), pos(18, 26)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "Variable GBR4 is not defined",
+      range(pos(18, 22), pos(18, 26)),
+    );
+  });
+
+  test("Process ROW DELETE statement successfully", async () => {
+    const editor = await helper.showDocument("DaCo35.cbl");
+    const diagnostics = await helper.waitForDiagnostics(editor.document.uri);
+    helper.printAllDiagnostics(diagnostics);
+    assert.strictEqual(diagnostics.length, 3);
+
+    checkDiagnostic(
+      diagnostics,
+      "String must starts with TBL or TBF values",
+      range(pos(20, 22), pos(20, 32)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "String must starts with TBL or TBF values",
+      range(pos(21, 22), pos(21, 26)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "Variable GBR4 is not defined",
+      range(pos(21, 22), pos(21, 26)),
+    );
+  });
+
+  test("Process ROW DUPLICATE statement successfully", async () => {
+    const editor = await helper.showDocument("DaCo36.cbl");
+    const diagnostics = await helper.waitForDiagnostics(editor.document.uri);
+    helper.printAllDiagnostics(diagnostics);
+    assert.strictEqual(diagnostics.length, 4);
+
+    checkDiagnostic(
+      diagnostics,
+      "String must starts with TBL or TBF values",
+      range(pos(20, 25), pos(20, 35)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "String must starts with TBL or TBF values",
+      range(pos(21, 25), pos(21, 29)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "Variable GBR4 is not defined",
+      range(pos(21, 25), pos(21, 29)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "Variable GBR4 is not defined",
+      range(pos(22, 39), pos(22, 43)),
+    );
+  });
+
+  test("Process ROW GET statement successfully", async () => {
+    const editor = await helper.showDocument("DaCo37.cbl");
+    const diagnostics = await helper.waitForDiagnostics(editor.document.uri);
+    helper.printAllDiagnostics(diagnostics);
+    assert.strictEqual(diagnostics.length, 6);
+
+    checkDiagnostic(
+      diagnostics,
+      "String must starts with TBL or TBF values",
+      range(pos(30, 19), pos(30, 29)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "String must starts with TBL or TBF values",
+      range(pos(31, 19), pos(31, 23)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "Variable GBR4 is not defined",
+      range(pos(31, 19), pos(31, 23)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "Variable GBR4 is not defined",
+      range(pos(32, 33), pos(32, 37)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "Variable GBR4 is not defined",
+      range(pos(33, 47), pos(33, 51)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "Variable GBR4 is not defined",
+      range(pos(34, 33), pos(34, 37)),
+    );
+  });
+
+  test("Process ROW INITIALIZE statement successfully", async () => {
+    const editor = await helper.showDocument("DaCo38.cbl");
+    const diagnostics = await helper.waitForDiagnostics(editor.document.uri);
+    helper.printAllDiagnostics(diagnostics);
+    assert.strictEqual(diagnostics.length, 3);
+
+    checkDiagnostic(
+      diagnostics,
+      "String must starts with TBL or TBF values",
+      range(pos(20, 26), pos(20, 36)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "String must starts with TBL or TBF values",
+      range(pos(21, 26), pos(21, 30)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "Variable GBR4 is not defined",
+      range(pos(21, 26), pos(21, 30)),
+    );
+  });
+
+  test("Process ROW INSERT statement successfully", async () => {
+    const editor = await helper.showDocument("DaCo39.cbl");
+    const diagnostics = await helper.waitForDiagnostics(editor.document.uri);
+    helper.printAllDiagnostics(diagnostics);
+    assert.strictEqual(diagnostics.length, 4);
+
+    checkDiagnostic(
+      diagnostics,
+      "String must starts with TBL or TBF values",
+      range(pos(21, 22), pos(21, 32)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "String must starts with TBL or TBF values",
+      range(pos(22, 22), pos(22, 26)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "Variable GBR4 is not defined",
+      range(pos(22, 22), pos(22, 26)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "Variable GBR4 is not defined",
+      range(pos(23, 38), pos(23, 42)),
+    );
+  });
+
+  test("Process ROW INVERT statement successfully", async () => {
+    const editor = await helper.showDocument("DaCo40.cbl");
+    const diagnostics = await helper.waitForDiagnostics(editor.document.uri);
+    helper.printAllDiagnostics(diagnostics);
+    assert.strictEqual(diagnostics.length, 3);
+
+    checkDiagnostic(
+      diagnostics,
+      "String must starts with TBL or TBF values",
+      range(pos(20, 22), pos(20, 32)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "String must starts with TBL or TBF values",
+      range(pos(21, 22), pos(21, 26)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "Variable GBR4 is not defined",
+      range(pos(21, 22), pos(21, 26)),
+    );
+  });
+
+  test("Process ROW MATCH statement successfully", async () => {
+    const editor = await helper.showDocument("DaCo41.cbl");
+    const diagnostics = await helper.waitForDiagnostics(editor.document.uri);
+    helper.printAllDiagnostics(diagnostics);
+    assert.strictEqual(diagnostics.length, 2);
+
+    checkDiagnostic(
+      diagnostics,
+      "String must starts with TBL or TBF values",
+      range(pos(20, 21), pos(20, 31)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "Variable GBR4 is not defined",
+      range(pos(21, 38), pos(21, 42)),
+    );
+  });
+
+  test("Process ROW MODIFY statement successfully", async () => {
+    const editor = await helper.showDocument("DaCo42.cbl");
+    const diagnostics = await helper.waitForDiagnostics(editor.document.uri);
+    helper.printAllDiagnostics(diagnostics);
+    assert.strictEqual(diagnostics.length, 5);
+
+    checkDiagnostic(
+      diagnostics,
+      "String must starts with TBL or TBF values",
+      range(pos(26, 22), pos(26, 32)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "String must starts with TBL or TBF values",
+      range(pos(27, 22), pos(27, 26)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "Variable GBR4 is not defined",
+      range(pos(27, 22), pos(27, 26)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "Variable GBR4 is not defined",
+      range(pos(28, 36), pos(28, 40)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "Variable GBR4 is not defined",
+      range(pos(29, 52), pos(29, 56)),
+    );
+  });
+
+  test("Process ROW NEXT statement successfully", async () => {
+    const editor = await helper.showDocument("DaCo43.cbl");
+    const diagnostics = await helper.waitForDiagnostics(editor.document.uri);
+    helper.printAllDiagnostics(diagnostics);
+    assert.strictEqual(diagnostics.length, 2);
+
+    checkDiagnostic(
+      diagnostics,
+      "String must starts with TBL or TBF values",
+      range(pos(22, 20), pos(22, 30)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "Variable GBR4 is not defined",
+      range(pos(23, 34), pos(23, 38)),
+    );
+  });
+
+  test("Process ROW PRIOR statement successfully", async () => {
+    const editor = await helper.showDocument("DaCo44.cbl");
+    const diagnostics = await helper.waitForDiagnostics(editor.document.uri);
+    helper.printAllDiagnostics(diagnostics);
+    assert.strictEqual(diagnostics.length, 2);
+
+    checkDiagnostic(
+      diagnostics,
+      "String must starts with TBL or TBF values",
+      range(pos(22, 21), pos(22, 31)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "Variable GBR4 is not defined",
+      range(pos(23, 35), pos(23, 39)),
+    );
+  });
+
+  test("Process ROW RESTORE statement successfully", async () => {
+    const editor = await helper.showDocument("DaCo45.cbl");
+    const diagnostics = await helper.waitForDiagnostics(editor.document.uri);
+    helper.printAllDiagnostics(diagnostics);
+    assert.strictEqual(diagnostics.length, 4);
+
+    checkDiagnostic(
+      diagnostics,
+      "String must starts with TBL or TBF values",
+      range(pos(21, 23), pos(21, 33)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "String must starts with TBL or TBF values",
+      range(pos(22, 23), pos(22, 27)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "Variable GBR4 is not defined",
+      range(pos(22, 23), pos(22, 27)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "Variable GBR4 is not defined",
+      range(pos(23, 39), pos(23, 43)),
+    );
+  });
+
+  test("Process ROW SAVE statement successfully", async () => {
+    const editor = await helper.showDocument("DaCo46.cbl");
+    const diagnostics = await helper.waitForDiagnostics(editor.document.uri);
+    helper.printAllDiagnostics(diagnostics);
+    assert.strictEqual(diagnostics.length, 4);
+
+    checkDiagnostic(
+      diagnostics,
+      "String must starts with TBL or TBF values",
+      range(pos(21, 20), pos(21, 30)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "String must starts with TBL or TBF values",
+      range(pos(22, 20), pos(22, 24)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "Variable GBR4 is not defined",
+      range(pos(22, 20), pos(22, 24)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "Variable GBR4 is not defined",
+      range(pos(23, 34), pos(23, 38)),
+    );
+  });
+
+  test("Process ROW SINGLE statement successfully", async () => {
+    const editor = await helper.showDocument("DaCo47.cbl");
+    const diagnostics = await helper.waitForDiagnostics(editor.document.uri);
+    helper.printAllDiagnostics(diagnostics);
+    assert.strictEqual(diagnostics.length, 4);
+
+    checkDiagnostic(
+      diagnostics,
+      "String must starts with TBL or TBF values",
+      range(pos(20, 22), pos(20, 32)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "String must starts with TBL or TBF values",
+      range(pos(21, 22), pos(21, 26)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "Variable GBR4 is not defined",
+      range(pos(21, 22), pos(21, 26)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "Variable GBR4 is not defined",
+      range(pos(22, 36), pos(22, 40)),
+    );
+  });
+
+  test("Process ROW SORT statement successfully", async () => {
+    const editor = await helper.showDocument("DaCo48.cbl");
+    const diagnostics = await helper.waitForDiagnostics(editor.document.uri);
+    helper.printAllDiagnostics(diagnostics);
+    assert.strictEqual(diagnostics.length, 4);
+
+    checkDiagnostic(
+      diagnostics,
+      "String must starts with TBL or TBF values",
+      range(pos(22, 20), pos(22, 30)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "String must starts with TBL or TBF values",
+      range(pos(23, 20), pos(23, 24)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "Variable GBR4 is not defined",
+      range(pos(23, 20), pos(23, 24)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "Variable GBR4 is not defined",
+      range(pos(24, 38), pos(24, 42)),
+    );
+  });
+
+  test("Process ROW START statement successfully", async () => {
+    const editor = await helper.showDocument("DaCo49.cbl");
+    const diagnostics = await helper.waitForDiagnostics(editor.document.uri);
+    helper.printAllDiagnostics(diagnostics);
+    assert.strictEqual(diagnostics.length, 3);
+
+    checkDiagnostic(
+      diagnostics,
+      "String must starts with TBL or TBF values",
+      range(pos(20, 21), pos(20, 31)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "String must starts with TBL or TBF values",
+      range(pos(21, 21), pos(21, 25)),
+    );
+
+    checkDiagnostic(
+      diagnostics,
+      "Variable GBR4 is not defined",
+      range(pos(21, 21), pos(21, 25)),
+    );
+  });
 });
