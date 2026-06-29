d22b7be6c2623c00c55d85bfc66c7f9fb0bfb650
Leonid Baranov <leonid.baranov@broadcom.com>
2026-06-29 16:48:38 +0200

    Add COPY-FROM to the DaCo processing (#3043)
    * Add COPY-FROM to the DaCo processing
    
    * Update clients/daco-dialect-support/src/test/unit/engine/parsing.spec.ts
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
    
    * Address comments
    
    * Optimize
    
    * Cleanup
    
    * Update clients/daco-dialect-support/resources/en.json
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
    
    * Update test
    
    ---------
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>

---
+++ clients/daco-dialect-support/test_files/DaCo74.cbl
@@ -0,0 +1,18 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID.    CPYFROM.
+       ENVIRONMENT DIVISION.
+       IDMS-CONTROL SECTION.
+            PROTOCOL. MODE ABC.
+            IDMS-RECORDS MANUAL
+       DATA   DIVISION.
+       WORKING-STORAGE SECTION.
+       01  ROOT.
+           03 AREA-XXX.
+             05 PROTO-XXX COPY-FROM DY.
+       01  COPY MAID PROTOCB.
+       PROCEDURE DIVISION.
+           DISPLAY PR1-XXX.
+           DISPLAY NOT_EXISTING.
+      * https://github.com/eclipse-che4z/che-che4z-lsp-for-cobol/blob
+      */40bec9e7654f1e5d8c086e188a170dc4205fdc1f/server/dialect-daco/src/test
+      */java/org/eclipse/lsp/cobol/dialects/daco/usecases/TestDaCoCopyFrom.java 
---
+++ clients/daco-dialect-support/test_files/DaCo78.cbl
@@ -0,0 +1,13 @@
+1      IDENTIFICATION DIVISION.
+2      PROGRAM-ID.    CPFTC.
+       ENVIRONMENT DIVISION.
+       IDMS-CONTROL SECTION.
+            PROTOCOL. MODE ABC.
+            IDMS-RECORDS MANUAL
+3      DATA   DIVISION.
+4      WORKING-STORAGE SECTION.
+5      01 ROOT.
+6          05 NODE1 COPY-FROM W4.
+      * https://github.com/eclipse-che4z/che-che4z-lsp-for-cobol/blob
+      */8f42f475c3824e9eaf1fdd0ecd5b133aada8c304/server/dialect-daco/src/test
+      */java/org/eclipse/lsp/cobol/dialects/daco/usecases/TestDaCoCopyFrom5.java
