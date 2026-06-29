ec0296cce00c75cfb85eb4f857cef2078c71a0f9
Leonid Baranov <leonid.baranov@broadcom.com>
2026-06-29 16:48:38 +0200

    Add COPY-FROM to the DaCo processing (#3043)
    * Add COPY-FROM to the DaCo processing
    
    * Update clients/daco-dialect-support/src/test/unit/engine/parsing.spec.ts
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
    
    * Address comments
    
    * Optimize
    
    * Cleanup
    
    * Update clients/daco-dialect-support/resources/en.json
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
    
    * Update test
    
    ---------
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>

---
+++ clients/daco-dialect-support/src/test/suite/copy-from.test.ts
@@ -0,0 +1,170 @@
+/*
+ * Copyright (c) 2026 Broadcom.
+ * The term "Broadcom" refers to Broadcom Inc. and/or its subsidiaries.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Broadcom - initial API and implementation
+ */
+import * as assert from "node:assert";
+import * as helper from "./testHelper";
+import { pos, range } from "./testHelper";
+
+suite("COPY-FROM statement Test Suite", function () {
+  this.timeout(helper.TEST_TIMEOUT);
+
+  suiteSetup(async function () {
+    await helper.activate();
+  });
+
+  this.afterEach(async function () {
+    this.timeout(helper.TEST_TIMEOUT);
+    await helper.closeAllEditors();
+  });
+
+  this.afterAll(async function () {
+    this.timeout(helper.TEST_TIMEOUT);
+    await helper.closeAllEditors();
+  });
+
+  test("Process COPY-FROM statement successfully (case 1)", async () => {
+    const editor = await helper.showDocument("DaCo74.cbl");
+    const diagnostics = await helper.waitForDiagnostics(editor.document.uri);
+    helper.printAllDiagnostics(diagnostics);
+    assert.strictEqual(diagnostics.length, 1);
+
+    helper.checkDiagnostic(
+      diagnostics,
+      "Variable NOT_EXISTING is not defined",
+      range(pos(14, 19), pos(14, 31)),
+    );
+  });
+
+  test("Process COPY-FROM statement successfully (case 2)", async () => {
+    const editor = await helper.showDocument("DaCo75.cbl");
+    const diagnostics = await helper.waitForDiagnostics(editor.document.uri);
+    helper.printAllDiagnostics(diagnostics);
+    assert.strictEqual(diagnostics.length, 1);
+
+    helper.checkDiagnostic(
+      diagnostics,
+      "Variable NOT_EXISTING is not defined",
+      range(pos(13, 19), pos(13, 31)),
+    );
+  });
+
+  test("Process COPY-FROM statement successfully (case 3)", async () => {
+    const editor = await helper.showDocument("DaCo76.cbl");
+    const diagnostics = await helper.waitForDiagnostics(editor.document.uri);
+    helper.printAllDiagnostics(diagnostics);
+    assert.strictEqual(diagnostics.length, 1);
+
+    helper.checkDiagnostic(
+      diagnostics,
+      "Variable NOT_EXISTING is not defined",
+      range(pos(13, 19), pos(13, 31)),
+    );
+  });
+
+  test("Process COPY-FROM statement successfully (line continuation)", async () => {
+    const editor = await helper.showDocument("DaCo77.cbl");
+    const diagnostics = await helper.waitForDiagnostics(editor.document.uri);
+    helper.printAllDiagnostics(diagnostics);
+    assert.strictEqual(diagnostics.length, 1);
+
+    helper.checkDiagnostic(
+      diagnostics,
+      "Variable NOT_EXISTING is not defined",
+      range(pos(15, 19), pos(15, 31)),
+    );
+  });
+
+  test("Process COPY-FROM statement successfully (no source)", async () => {
+    const editor = await helper.showDocument("DaCo78.cbl");
+    const diagnostics = await helper.waitForDiagnostics(editor.document.uri);
+    helper.printAllDiagnostics(diagnostics);
+    assert.strictEqual(diagnostics.length, 2);
+
+    helper.checkDiagnostic(
+      diagnostics,
+      "Source for NODE1 not found",
+      range(pos(9, 20), pos(9, 32)),
+    );
+
+    helper.checkDiagnostic(
+      diagnostics,
+      'A "PICTURE" or "USAGE INDEX" clause was not found for elementary item NODE1',
+      range(pos(9, 14), pos(9, 19)),
+    );
+  });
+
+  test("Process COPY-FROM statement successfully (case 4)", async () => {
+    const editor = await helper.showDocument("DaCo79.cbl");
+    const diagnostics = await helper.waitForDiagnostics(editor.document.uri);
+    helper.printAllDiagnostics(diagnostics);
+    assert.strictEqual(diagnostics.length, 1);
+
+    helper.checkDiagnostic(
+      diagnostics,
+      "Variable NOT_EXISTING is not defined",
+      range(pos(13, 19), pos(13, 31)),
+    );
+  });
+
+  test("Process COPY-FROM statement successfully (duplicate variables)", async () => {
+    const editor = await helper.showDocument("DaCo80.cbl");
+    const diagnostics = await helper.waitForDiagnostics(editor.document.uri);
+    helper.printAllDiagnostics(diagnostics);
+    assert.strictEqual(diagnostics.length, 1);
+
+    helper.checkDiagnostic(
+      diagnostics,
+      "Variable NOT_EXISTING is not defined",
+      range(pos(15, 19), pos(15, 31)),
+    );
+  });
+
+  test("Process COPY-FROM statement successfully (undefined variable)", async () => {
+    const editor = await helper.showDocument("DaCo81.cbl");
+    const diagnostics = await helper.waitForDiagnostics(editor.document.uri);
+    helper.printAllDiagnostics(diagnostics);
+    assert.strictEqual(diagnostics.length, 1);
+
+    helper.checkDiagnostic(
+      diagnostics,
+      "Variable NOT_EXISTING is not defined",
+      range(pos(16, 19), pos(16, 31)),
+    );
+  });
+
+  test("Process COPY-FROM statement successfully (group variable)", async () => {
+    const editor = await helper.showDocument("DaCo82.cbl");
+    const diagnostics = await helper.waitForDiagnostics(editor.document.uri);
+    helper.printAllDiagnostics(diagnostics);
+    assert.strictEqual(diagnostics.length, 1);
+
+    helper.checkDiagnostic(
+      diagnostics,
+      "Variable NOT_EXISTING is not defined",
+      range(pos(16, 19), pos(16, 31)),
+    );
+  });
+
+  test("Process COPY-FROM as a variable name successfully", async () => {
+    const editor = await helper.showDocument("DaCo83.cbl");
+    const diagnostics = await helper.waitForDiagnostics(editor.document.uri);
+    helper.printAllDiagnostics(diagnostics);
+    assert.strictEqual(diagnostics.length, 1);
+
+    helper.checkDiagnostic(
+      diagnostics,
+      "Variable NOT_EXISTING is not defined",
+      range(pos(10, 19), pos(10, 31)),
+    );
+  });
+});
