9516fc1e7114bcdc95b3e33651ca806d6daa0205
Leonid Baranov <leonid.baranov@broadcom.com>
2026-07-16 15:17:45 +0200

    Support for predefined variables (#3068)
    * Support for predefined variables
    
    * Update clients/daco-dialect-support/src/antlr/ProgramParser.g4
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>

---
+++ clients/daco-dialect-support/test_files/DaCo99.cbl
@@ -0,0 +1,15 @@
+       IDENTIFICATION DIVISION. 
+       PROGRAM-ID. test1. 
+       ENVIRONMENT DIVISION.
+       IDMS-CONTROL SECTION.
+           PROTOCOL. MODE ABC.
+           IDMS-RECORDS MANUAL
+       DATA DIVISION. 
+       WORKING-STORAGE SECTION. 
+       PROCEDURE DIVISION. 
+           MOVE 6 to USIzXSH.
+           DISPLAY NOT_EXISTING.
+      *https://github.com/eclipse-che4z/che-che4z-lsp-for-cobol/blob
+      */87cfdc478079c3cb02e549ca75095a6f6f85122a/server/dialect-daco/src/test
+      */java/org/eclipse/lsp/cobol/dialects/daco/usecases
+      */TestDaCoImplicitCode.java
