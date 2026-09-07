8654e07b3cd04cd04a1c958cabc47777df073017
Leonid Baranov <leonid.baranov@broadcom.com>
2026-09-07 18:08:59 +0200

    Setting up new IDMS (#3107)
    * Initial commit
    
    * Update CI
    
    * Fix build scripts
    
    * Prettier
    
    * Set up eslint
    
    * Update eslint
    
    * Update clients/idms-dialect-support/eslint.config.mjs
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
    
    * Fix build
    
    * Fix lint
    
    ---------
    
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>

---
+++ clients/idms-dialect-support/src/test/unit/scaffolding.spec.ts
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2026 Broadcom.
+ * The term "Broadcom" refers to Broadcom Inc. and/or its subsidiaries.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Broadcom - initial API and implementation
+ */
+
+// Placeholder spec proving the Jest/ts-jest toolchain runs end to end.
+describe("v2 engine scaffolding", () => {
+  it("has a working unit test toolchain", () => {
+    expect(true).toBe(true);
+  });
+});
