c49a7aac779a1f068e99db52689eaa45b3adb8f8
Leonid Baranov <leonid.baranov@broadcom.com>
2026-09-10 13:23:10 +0200

    Set up ANTLR

---
+++ clients/idms-dialect-support/src/engine/preprocessor.ts
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2026 Broadcom.
+ * The term "Broadcom" refers to Broadcom Inc. and/or its subsidiaries.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Broadcom - initial API and implementation
+ */
+import * as vscode from "vscode";
+import { IDocumentProcessingContext } from "@code4z/cobol-dialect-api";
+import { MessageService } from "./services/MessageService";
+
+export class IdmsPreprocessor {
+  constructor(
+    private readonly outputChannel: vscode.OutputChannel,
+    private readonly messageService: MessageService,
+  ) {}
+
+  public async execute(context: IDocumentProcessingContext, text: string) {}
+}
