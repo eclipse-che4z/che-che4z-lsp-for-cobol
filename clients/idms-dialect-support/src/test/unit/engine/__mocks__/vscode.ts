88f2854f0cc301866eaa9d23e257275897afa7c2
Leonid Baranov <leonid.baranov@broadcom.com>
2026-09-10 13:41:21 +0200

    Increase test coverage

--- clients/idms-dialect-support/src/test/unit/engine/__mocks__/vscode.ts
+++ clients/idms-dialect-support/src/test/unit/engine/__mocks__/vscode.ts
@@ -16,8 +16,36 @@ export const workspace = {
   getConfiguration: jest.fn().mockReturnValue({
     get: jest.fn(),
   }),
+  fs: {
+    readFile: jest.fn(),
+  },
 };
 
 export const window = {
   showErrorMessage: jest.fn(),
+};
+
+export class Position {
+  constructor(
+    public readonly line: number,
+    public readonly character: number,
+  ) {}
+}
+
+export class Range {
+  constructor(public readonly start: Position, public readonly end: Position) {}
+}
+
+export enum DiagnosticSeverity {
+  Error = 0,
+  Warning = 1,
+  Information = 2,
+  Hint = 3,
+}
+
+export const Uri = {
+  joinPath: jest.fn((base: unknown, ...paths: string[]) => ({
+    base,
+    paths,
+  })),
 };
