ebcb0e2221c7647c7b98d1ce73d6d09b9780d9f4
Leonid Baranov <leonid.baranov@broadcom.com>
2026-09-18 16:35:38 +0200

    Update clients/idms-dialect-support/src/engine/statementsts.ts
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>

--- clients/idms-dialect-support/src/engine/statementsts.ts
+++ clients/idms-dialect-support/src/engine/statementsts.ts
@@ -24,7 +24,7 @@ import { StatementDescriptor } from "./model";
  * preserving variable usages so that the underlying COBOL parser can still
  * resolve them.
  */
-export function execute(
+export function processStatement(
   context: IDocumentProcessingContext,
   descriptors: StatementDescriptor[],
 ): void {
